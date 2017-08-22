99c511786837016d86d219dbb55c9669888e594e
kyraNET <kyradiscord@gmail.com>
2017-08-22 20:15:25 +0200

    Updated packagelocks, added hastebin.js

---
+++ utils/hastebin.js
@@ -0,0 +1,17 @@
+const snekfetch = require('snekfetch');
+
+class Hastebin {
+
+    static async upload(text) {
+        const res = await snekfetch.post('https://hastebin.com/documents').send(text).catch((err) => { throw err; });
+        return `https://hastebin.com/${res.body.key}.js`;
+    }
+
+    static async download(url) {
+        const res = await snekfetch.get(url).catch((err) => { throw err; });
+        return res.text;
+    }
+
+}
+
+module.exports = Hastebin;

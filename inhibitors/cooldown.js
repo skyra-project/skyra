fba1b33c3d23816a637d16f5e2f0cdca815f609b
kyraNET <kyradiscord@gmail.com>
2017-08-25 14:12:46 +0200

    Better cooldown, spam fix

--- inhibitors/cooldown.js
+++ inhibitors/cooldown.js
@@ -4,6 +4,7 @@ module.exports = class extends Inhibitor {
 
     constructor(...args) {
         super(...args, { spamProtection: true });
+        this.cooldowns = new Set();
     }
 
     async run(msg, cmd) {
@@ -11,15 +12,18 @@ module.exports = class extends Inhibitor {
         if (!cmd.cooldown || cmd.cooldown <= 0) return;
 
         const instance = cmd.cooldowns.get(msg.author.id);
-
         if (!instance) return;
 
         const remaining = ((cmd.cooldown * 1000) - (Date.now() - instance)) / 1000;
-
         if (remaining < 0) {
             cmd.cooldowns.delete(msg.author.id);
             return;
         }
+
+        if (this.cooldowns.has(msg.channel.id)) throw true;
+
+        this.cooldowns.add(msg.channel.id);
+        setTimeout(() => this.cooldowns.delete(msg.channel.id), 30000);
 
         throw msg.language.get('INHIBITOR_COOLDOWN', Math.ceil(remaining));
     }
--- inhibitors/spam.js
+++ inhibitors/spam.js
@@ -1,18 +1,17 @@
 const { Inhibitor } = require('../index');
 
-const cooldowns = new Set();
-
 module.exports = class extends Inhibitor {
 
     constructor(...args) {
         super(...args, { spamProtection: true });
+        this.cooldowns = new Set();
     }
 
     async run(msg, cmd, settings) {
-        if (msg.author.id === this.client.config.ownerID || cmd.spam !== true) return;
+        if (msg.author.id === this.client.config.ownerID || cmd.spam !== true || msg.channel.type !== 'text') return;
         if (settings.channels.spam === msg.channel.id) return;
 
-        if (cooldowns.has(msg.guild.id)) throw true;
+        if (this.cooldowns.has(msg.channel.id)) throw true;
 
         const channel = msg.guild.channels.get(settings.channels.spam);
         if (!channel) {
@@ -20,8 +19,8 @@ module.exports = class extends Inhibitor {
             return;
         }
 
-        cooldowns.add(msg.guild.id);
-        setTimeout(() => cooldowns.delete(msg.guild.id), 30000);
+        this.cooldowns.add(msg.channel.id);
+        setTimeout(() => this.cooldowns.delete(msg.channel.id), 30000);
 
         throw msg.language.get('INHIBITOR_SPAM', channel);
     }

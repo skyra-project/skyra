7d5a418bedfc026fefb2062774e284d0e79a423a
kyraNET <kyradiscord@gmail.com>
2017-11-10 12:57:58 +0100

    Two deps less (stream-to-array and any-promise)

--- commands/Misc/triggered.js
+++ commands/Misc/triggered.js
@@ -1,5 +1,4 @@
 const { structures: { Command }, util: { util } } = require('../../index');
-const streamToArray = require('stream-to-array');
 const fsn = require('fs-nextra');
 const path = require('path');
 const GIFEncoder = require('gifencoder');
@@ -66,7 +65,45 @@ module.exports = class extends Command {
 
 		encoder.finish();
 
-		return streamToArray(stream).then(Buffer.concat);
+		return this.streamToArray(stream).then(Buffer.concat);
+	}
+
+	/**
+	 * @param {ReadableStream} stream The stream.
+	 * @returns {Promise<[]>}
+	 */
+	streamToArray(stream) {
+		if (!stream.readable) return Promise.resolve([]);
+		return new Promise((resolve, reject) => {
+			const array = [];
+
+			function onData(data) {
+				array.push(data);
+			}
+
+			function onEnd(error) {
+				if (error) reject(error);
+				else resolve(array);
+				cleanup();
+			}
+
+			function onClose() {
+				resolve(array);
+				cleanup();
+			}
+
+			function cleanup() {
+				stream.removeListener('data', onData);
+				stream.removeListener('end', onEnd);
+				stream.removeListener('error', onEnd);
+				stream.removeListener('close', onClose);
+			}
+
+			stream.on('data', onData);
+			stream.on('end', onEnd);
+			stream.on('error', onEnd);
+			stream.on('close', onClose);
+		});
 	}
 
 	async init() {

7ddf1b596eb254bf43b002237219fdb6e2d7fe47
kyraNET <kyradiscord@gmail.com>
2017-11-10 23:03:23 +0100

    Removed marked

--- inhibitors/disable.js
+++ inhibitors/disable.js
@@ -3,8 +3,11 @@ const { structures: { Inhibitor } } = require('../index');
 module.exports = class extends Inhibitor {
 
 	async run(msg, cmd, settings, i18n) {
-		if (cmd.enabled || msg.channel.type !== 'text' || !settings.disable.commands.includes(cmd.name)) return;
-		throw i18n.get('INHIBITOR_DISABLED');
+		if (cmd.enabled
+			&& msg.channel.type === 'text'
+			&& settings.disable.commands.length > 0
+			&& settings.disable.commands.includes(cmd.name))
+			throw i18n.get('INHIBITOR_DISABLED');
 	}
 
 };

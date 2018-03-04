c22ac9abb9d59a0ed5d1000115c80115ce9d8596
kyraNET <kyradiscord@gmail.com>
2018-03-04 12:33:11 +0100

    Missed this bit

--- commands/Starboard/starboard.js
+++ commands/Starboard/starboard.js
@@ -21,16 +21,4 @@ module.exports = class extends Command {
 	// 	// This is where you place the code you want to run for your command
 	// }
 
-	async init() {
-		const r = this.client.providers.default.db;
-		const TABLENAME = 'starboard', INDEXNAME = 'guild_message';
-
-		await r.branch(r.tableList().contains(TABLENAME), null,
-			r.tableCreate(TABLENAME));
-		await r.branch(r.table(TABLENAME).indexList().contains(INDEXNAME), null,
-			r.table(TABLENAME).indexCreate(INDEXNAME, [r.row('guildID'), r.row('messageID')]));
-		await r.branch(r.table(TABLENAME).indexStatus(INDEXNAME).nth(0)('ready'), null,
-			r.table(TABLENAME).indexWait(INDEXNAME));
-	}
-
 };

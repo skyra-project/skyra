b36b4d09e187343f1152553a28fab9e9457d0451
kyranet <kyradiscord@gmail.com>
2018-05-12 11:40:31 +0200

    License MemorySweeper under MIT

--- src/ipcPieces/global/status.js
+++ src/ipcPieces/global/status.js
@@ -3,11 +3,13 @@ const { API } = require('../../index');
 module.exports = class extends API {
 
 	run() {
-		return this.client.ws.shards
-			// Internal Sharding Support
-			? this.client.ws.shards.map(shard => shard.status)
-			// Without Sharding
-			: [this.client.ws.connection.status];
+		return {
+			response: this.client.ws.shards
+				// Internal Sharding Support
+				? this.client.ws.shards.map(shard => shard.status)
+				// Without Sharding
+				: [this.client.ws.connection.status]
+		};
 	}
 
 };

5c81def3f97bb5568d2e0feb065d9f38f19dd566
kyraNET <kyradiscord@gmail.com>
2018-07-14 10:05:51 +0200

    fix: Headers and body for postStats

--- src/tasks/poststats.js
+++ src/tasks/poststats.js
@@ -5,30 +5,30 @@ module.exports = class extends Task {
 	run() {
 		if (this.client.options.dev) return;
 
-		/* eslint-disable camelcase */
+		const body = `{"server_count":${this.client.guilds.size}}`;
 		if (config.tokens.discordBotOrg) {
 			fetch(`https://discordbots.org/api/bots/${this.client.user.id}/stats`, {
-				headers: { Authorization: config.tokens.discordBotOrg },
-				body: { server_count: this.client.guilds.size },
-				method: 'POST'
+				headers: { 'Content-Type': 'application/json', Authorization: config.tokens.discordBotOrg },
+				method: 'POST',
+				body
 			}, 'result')
 				.then(() => this.client.emit('verbose', `POST [discordbots.org]: ${this.client.guilds.size}`))
 				.catch(err => this.client.emit('error', `ERROR [discordbots.org]:\nError: ${(err && err.stack) || err}`));
 		}
 		if (config.tokens.discordBots) {
 			fetch(`https://bots.discord.pw/api/bots/${this.client.user.id}/stats`, {
-				headers: { Authorization: config.tokens.discordBots },
-				body: { server_count: this.client.guilds.size },
-				method: 'POST'
+				headers: { 'Content-Type': 'application/json', Authorization: config.tokens.discordBots },
+				method: 'POST',
+				body
 			}, 'result')
 				.then(() => this.client.emit('verbose', `POST [bots.discord.pw]: ${this.client.guilds.size}`))
 				.catch(err => this.client.emit('error', `ERROR [bots.discord.pw]:\nError: ${(err && err.stack) || err}`));
 		}
 		if (config.tokens.botsForDiscord) {
 			fetch(`https://botsfordiscord.com/api/v1/bots/${this.client.user.id}`, {
-				headers: { Authorization: config.tokens.botsForDiscord },
-				body: { server_count: this.client.guilds.size },
-				method: 'POST'
+				headers: { 'Content-Type': 'application/json', Authorization: config.tokens.botsForDiscord },
+				method: 'POST',
+				body
 			}, 'result')
 				.then(() => this.client.emit('verbose', `POST [botsfordiscord.com]: ${this.client.guilds.size}`))
 				.catch(err => this.client.emit('error', `ERROR [botsfordiscord.com]:\nError: ${(err && err.stack) || err}`));
@@ -39,7 +39,6 @@ module.exports = class extends Task {
 		// 	.send({ token: config.tokens.serverHound, servers: this.client.guilds.size })
 		// 	.then(() => this.client.emit('log', `POST [ServerHound]: ${this.client.guilds.size}`))
 		// 	.catch(err => this.client.emit('error', `ERROR [ServerHound]:\nError: ${(err && err.stack) || err}`));
-		/* eslint-enable camelcase */
 	}
 
 };

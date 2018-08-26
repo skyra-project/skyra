59824cb5a61c105082f303a10ef1ad17a72abdc3
kyraNET <kyradiscord@gmail.com>
2018-08-26 19:34:16 +0200

    fix: Update command sending two messages

--- src/commands/System/Admin/update.js
+++ src/commands/System/Admin/update.js
@@ -14,14 +14,15 @@ module.exports = class extends Command {
 
 	async run(message, [branch = 'master']) {
 		const pullResponse = await klasaUtil.exec(`git pull origin ${branch}`);
-		const response = await message.channel.sendCode('prolog', [pullResponse.stdout, pullResponse.stderr || '✔'].join('\n-=-=-=-\n'));
+		const shouldReboot = !pullResponse.stdout.includes('Already up-to-date.') && ('reboot' in message.flags);
+		const response = await message.sendCode('prolog', [pullResponse.stdout, pullResponse.stderr || '✔'].join('\n-=-=-=-\n'));
 		if (!await this.isCurrentBranch(branch)) {
-			const switchResponse = await message.channel.send(`Switching to ${branch}...`);
+			const switchResponse = await message.sendMessage(`Switching to ${branch}...`);
 			const checkoutResponse = await klasaUtil.exec(`git checkout ${branch}`);
 			await switchResponse.edit([checkoutResponse.stdout, checkoutResponse.stderr || '✔'].join('\n-=-=-=-\n'), { code: 'prolog' });
-			if ('reboot' in message.flags) return this.store.get('reboot').run(message);
-		} else if (!pullResponse.stdout.includes('Already up-to-date.') && ('reboot' in message.flags)) {
-			return this.store.get('reboot').run(message);
+			if (shouldReboot) return this.store.get('reboot').run(message, []);
+		} else if (shouldReboot) {
+			return this.store.get('reboot').run(message, []);
 		}
 		return response;
 	}

3a75998fc54fb0abe0f16ee0e7dbb02d1443748a
kyranet <kyradiscord@gmail.com>
2018-09-09 14:46:59 +0200

    misc: Optimise poststats task

--- src/tasks/poststats.js
+++ src/tasks/poststats.js
@@ -5,14 +5,16 @@ module.exports = class extends Task {
 	async run() {
 		if (this.client.options.dev) return;
 
-		const body = `{"server_count":${this.client.guilds.size}}`;
+		const guilds = this.client.guilds.size;
+		const users = this.client.guilds.reduce((a, b) => a + b.memberCount, 0);
+		const body = `{"server_count":${guilds}}`;
 		if (config.tokens.discordBotOrg) {
 			fetch(`https://discordbots.org/api/bots/${this.client.user.id}/stats`, {
 				headers: { 'Content-Type': 'application/json', Authorization: config.tokens.discordBotOrg },
 				method: 'POST',
 				body
 			}, 'result')
-				.then(() => this.client.emit('verbose', `POST [discordbots.org]: ${this.client.guilds.size}`))
+				.then(() => this.client.emit('verbose', `POST [discordbots.org]: ${guilds}`))
 				.catch(err => this.client.emit('error', `ERROR [discordbots.org]:\nError: ${(err && err.stack) || err}`));
 		}
 		if (config.tokens.discordBots) {
@@ -21,7 +23,7 @@ module.exports = class extends Task {
 				method: 'POST',
 				body
 			}, 'result')
-				.then(() => this.client.emit('verbose', `POST [bots.discord.pw]: ${this.client.guilds.size}`))
+				.then(() => this.client.emit('verbose', `POST [bots.discord.pw]: ${guilds}`))
 				.catch(err => this.client.emit('error', `ERROR [bots.discord.pw]:\nError: ${(err && err.stack) || err}`));
 		}
 		if (config.tokens.botsForDiscord) {
@@ -30,16 +32,16 @@ module.exports = class extends Task {
 				method: 'POST',
 				body
 			}, 'result')
-				.then(() => this.client.emit('verbose', `POST [botsfordiscord.com]: ${this.client.guilds.size}`))
+				.then(() => this.client.emit('verbose', `POST [botsfordiscord.com]: ${guilds}`))
 				.catch(err => this.client.emit('error', `ERROR [botsfordiscord.com]:\nError: ${(err && err.stack) || err}`));
 		}
 		if (config.tokens.discordBotList) {
 			fetch(`https://discordbotlist.com/api/bots/${this.client.user.id}/stats`, {
 				headers: { 'Content-Type': 'application/json', Authorization: `Bot ${config.tokens.discordBotList}` },
 				method: 'POST',
-				body: `{"guilds":${this.client.guilds.size},"users":${this.client.guilds.reduce((a, b) => a + b.memberCount, 0)}}`
+				body: `{"guilds":${guilds},"users":${users}}`
 			}, 'result')
-				.then(() => this.client.emit('verbose', `POST [discordbotlist.com]: ${this.client.guilds.size}`))
+				.then(() => this.client.emit('verbose', `POST [discordbotlist.com]: ${guilds} | ${users}`))
 				.catch(err => this.client.emit('error', `ERROR [discordbotlist.com]:\nError: ${(err && err.stack) || err}`));
 		}
 		// if (config.tokens.serverHound) request

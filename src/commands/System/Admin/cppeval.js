ddac9ef8472803d53590a9ca68739c8c3b1a00d1
kyranet <kyradiscord@gmail.com>
2018-09-18 15:57:15 +0200

    enhancement: Hide paths based on S_LOCATION

--- src/commands/System/Admin/cppeval.js
+++ src/commands/System/Admin/cppeval.js
@@ -1,12 +1,13 @@
-const { Command, Stopwatch, klasaUtil: { codeBlock, exec }, rootFolder } = require('../../../index');
+const { Command, Stopwatch, klasaUtil: { codeBlock, exec, regExpEsc }, rootFolder } = require('../../../index');
 const fsn = require('fs-nextra');
 
-const BWD_FOLDER = require('path').join(rootFolder, 'bwd', 'cs');
+const BWD_FOLDER = require('path').join(rootFolder, 'bwd', 'cpp');
 const S_LOCATION = require('path').join(BWD_FOLDER, 'eval.cpp');
 const E_LOCATION = require('path').join(BWD_FOLDER, 'eval.out');
 const GCC_COMMAND = `g++ ${S_LOCATION} -o ${E_LOCATION}`;
 const MONO_COMMAND = `${E_LOCATION}`;
 const EXEC_OPTIONS = { timeout: 30000, cwd: BWD_FOLDER };
+const REPLACER = new RegExp(regExpEsc(S_LOCATION), 'g');
 
 module.exports = class extends Command {
 
@@ -34,7 +35,8 @@ module.exports = class extends Command {
 	compile(start) {
 		return exec(GCC_COMMAND, EXEC_OPTIONS)
 			.then(() => null)
-			.catch(error => `Failed to compile (${start.stop()}). ${codeBlock('cpp', error)}`);
+			.catch(error => `Failed to compile (${start.stop()}). ${codeBlock('cs', `${error.stdout}\n${error.stderr}`
+				.replace(REPLACER, 'Failed at: '))}`);
 	}
 
 	execute() {
--- src/commands/System/Admin/cseval.js
+++ src/commands/System/Admin/cseval.js
@@ -1,4 +1,4 @@
-const { Command, Stopwatch, klasaUtil: { codeBlock, exec }, rootFolder } = require('../../../index');
+const { Command, Stopwatch, klasaUtil: { codeBlock, exec, regExpEsc }, rootFolder } = require('../../../index');
 const fsn = require('fs-nextra');
 
 const BWD_FOLDER = require('path').join(rootFolder, 'bwd', 'cs');
@@ -7,6 +7,7 @@ const E_LOCATION = require('path').join(BWD_FOLDER, 'eval.exe');
 const CSC_COMMAND = `csc ${S_LOCATION}`;
 const MONO_COMMAND = `mono ${E_LOCATION}`;
 const EXEC_OPTIONS = { timeout: 30000, cwd: BWD_FOLDER };
+const REPLACER = new RegExp(regExpEsc(S_LOCATION), 'g');
 
 module.exports = class extends Command {
 
@@ -40,7 +41,7 @@ module.exports = class extends Command {
 		return exec(CSC_COMMAND, EXEC_OPTIONS)
 			.then(() => null)
 			.catch(error => `Failed to compile (${start.stop()}). ${codeBlock('cs', `${error.stdout}\n${error.stderr}`
-				.replace(/\/bwd\/cs\/eval.cs/g, 'Failed at: '))}`);
+				.replace(REPLACER, 'Failed at: '))}`);
 	}
 
 	/**

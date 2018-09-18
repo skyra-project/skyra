eac900d3cf1d930e9984b7e2c21f9b5b92ca0c94
kyranet <kyradiscord@gmail.com>
2018-09-18 15:52:11 +0200

    fix: Update cppeval

--- src/commands/System/Admin/cppeval.js
+++ src/commands/System/Admin/cppeval.js
@@ -1,5 +1,12 @@
-const { Command, Stopwatch, util } = require('klasa');
+const { Command, Stopwatch, klasaUtil: { codeBlock, exec }, rootFolder } = require('../../../index');
 const fsn = require('fs-nextra');
+
+const BWD_FOLDER = require('path').join(rootFolder, 'bwd', 'cs');
+const S_LOCATION = require('path').join(BWD_FOLDER, 'eval.cpp');
+const E_LOCATION = require('path').join(BWD_FOLDER, 'eval.out');
+const GCC_COMMAND = `g++ ${S_LOCATION} -o ${E_LOCATION}`;
+const MONO_COMMAND = `${E_LOCATION}`;
+const EXEC_OPTIONS = { timeout: 30000, cwd: BWD_FOLDER };
 
 module.exports = class extends Command {
 
@@ -17,23 +24,23 @@ module.exports = class extends Command {
 	async run(msg, [args]) {
 		const start = new Stopwatch(5);
 		const { input } = this.parse(msg, args);
-		await fsn.outputFileAtomic('/bwd/cpp/eval.cpp', input);
+		await fsn.outputFileAtomic(S_LOCATION, input);
 		const error = await this.compile(start);
 		if (error !== null) return msg.sendMessage(error);
 		const { success, result } = await this.execute();
-		return msg.sendMessage(`${success ? '⚙ **Compiled and executed:**' : '❌ **Error:**'} Took ${start.stop()}${util.codeBlock('cpp', result)}`);
+		return msg.sendMessage(`${success ? '⚙ **Compiled and executed:**' : '❌ **Error:**'} Took ${start.stop()}${codeBlock('cs', result || 'Success! No output.')}`);
 	}
 
 	compile(start) {
-		return util.exec('g++ /bwd/cpp/eval.cpp -o /bwd/cpp/eval.out', { timeout: 30000 })
+		return exec(GCC_COMMAND, EXEC_OPTIONS)
 			.then(() => null)
-			.catch(error => `Failed to compile (${start.stop()}). ${util.codeBlock('cpp', error)}`);
+			.catch(error => `Failed to compile (${start.stop()}). ${codeBlock('cpp', error)}`);
 	}
 
 	execute() {
-		return util.exec('/bwd/cpp/eval.out', { timeout: 30000 })
-			.then(result => ({ success: true, result: result.stdout }))
-			.catch(error => ({ success: false, result: error }));
+		return exec(MONO_COMMAND, EXEC_OPTIONS)
+			.then(result => ({ success: true, result: `${result.stdout}\n${result.stderr}` }))
+			.catch(error => ({ success: false, result: `${error.stdout}\n${error.stderr}` }));
 	}
 
 	parse(msg, input) {

af61eab67c2d90e255454d85bc18759c513b5db7
Frangu Vlad <kingdgrizzle@gmail.com>
2018-10-08 20:34:12 +0300

    misc: Fix wrong if check

--- src/rawEvents/guildMemberRemove.js
+++ src/rawEvents/guildMemberRemove.js
@@ -15,7 +15,7 @@ module.exports = class extends RawEvent {
 
 	async run({ guild, user }) {
 		guild.memberSnowflakes.delete(user.id);
-		if (this.client.guilds.some(g => g.memberSnowflakes.has(user.id))) this.client.usernames.delete(user.id);
+		if (!this.client.guilds.some(g => g.memberSnowflakes.has(user.id))) this.client.usernames.delete(user.id);
 		if (guild.members.has(user.id)) guild.members.delete(user.id);
 		if (guild.security.raid.has(user.id)) guild.security.raid.delete(user.id);
 		if (guild.settings.events.memberRemove) {

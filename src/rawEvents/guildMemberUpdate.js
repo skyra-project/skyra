37fafe44c7a444ef2948b332548281b9937b4a56
kyranet <kyradiscord@gmail.com>
2018-11-02 15:43:28 +0100

    fix: guildMemberUpdate raw event accessing to wrong property name

--- src/rawEvents/guildMemberUpdate.js
+++ src/rawEvents/guildMemberUpdate.js
@@ -22,7 +22,7 @@ module.exports = class extends RawEvent {
 		const guild = this.client.guilds.get(data.guild_id);
 		if (guild) {
 			guild.memberSnowflakes.add(data.user.id);
-			this.client.usernames.set(data.user.id, `${data.user.username}#${data.user.tag}`);
+			this.client.usernames.set(data.user.id, `${data.user.username}#${data.user.discriminator}`);
 			const member = guild.members.get(data.user.id);
 			// @ts-ignore
 			if (member) member._patch(data);

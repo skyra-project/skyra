6aaf59420a02ce416958d75a09244b206c3c6978
kyranet <kyradiscord@gmail.com>
2018-11-09 22:40:29 +0100

    fix: Voice states leaking

--- src/tasks/cleanup.js
+++ src/tasks/cleanup.js
@@ -45,13 +45,17 @@ module.exports = class MemorySweeper extends Task {
 
 	async run() {
 		const OLD_SNOWFLAKE = binaryToID(((Date.now() - THRESHOLD) - EPOCH).toString(2).padStart(42, '0') + EMPTY);
-		let presences = 0, guildMembers = 0, emojis = 0, lastMessages = 0, users = 0;
+		let presences = 0, guildMembers = 0, voiceStates = 0, emojis = 0, lastMessages = 0, users = 0;
 
 		// Per-Guild sweeper
 		for (const guild of this.client.guilds.values()) {
 			// Clear presences
 			presences += guild.presences.size;
 			guild.presences.clear();
+
+			// Clear voice states
+			voiceStates += guild.voiceStates.size;
+			guild.voiceStates.clear();
 
 			// Clear members that haven't send a message in the last 30 minutes
 			const { me } = guild;
@@ -91,6 +95,7 @@ module.exports = class MemorySweeper extends Task {
 			`${HEADER} ${
 				this.setColor(presences)} [Presence]s | ${
 				this.setColor(guildMembers)} [GuildMember]s | ${
+				this.setColor(voiceStates)} [Voice States] | ${
 				this.setColor(users)} [User]s | ${
 				this.setColor(emojis)} [Emoji]s | ${
 				this.setColor(lastMessages)} [Last Message]s.`);

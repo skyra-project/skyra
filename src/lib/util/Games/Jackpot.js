02b5bcda14015de9dfc7e1a7747348b6d3a20964
Tyler Davis <34944514+Tylertron1998@users.noreply.github.com>
2018-11-12 12:19:15 +0000

    Update Jackpot.js

--- src/lib/util/Games/Jackpot.js
+++ src/lib/util/Games/Jackpot.js
@@ -5,13 +5,12 @@ class Jackpot {
 	}
 
 	async add(id, amount) {
-		const object = this.client.settings.jackpot.find(user => user.id === id);
 		const index = this.client.settings.jackpot.findIndex(user => user.id === id);
-
-		console.log(object);
-
+		
+		
 		// eslint-disable-next-line curly
-		if (object) {
+		if (index !== -1) {
+			const object = this.client.settings[index];
 			await this.client.settings.update('jackpot', { id, amount: object.amount + amount }, { arrayPosition: index, action: 'overwrite' });
 		} else await this.client.settings.update('jackpot', { id, amount });
 		return this.client.settings.jackpot.find(jackpotUser => jackpotUser.id === id).amount;
@@ -19,9 +18,8 @@ class Jackpot {
 
 	async draw() {
 		if (this.client.settings.jackpot.length === 0) return { id: null, amount: 0 };
-
-		// eslint-disable-next-line
-        const amount = this.client.settings.jackpot.reduce((a, b) => a + b.amount, 0);
+		
+        	const amount = this.client.settings.jackpot.reduce((a, b) => a + b.amount, 0);
 
 		const user = this.client.settings.jackpot[Math.floor(Math.random() * this.client.settings.jackpot)].id;
 

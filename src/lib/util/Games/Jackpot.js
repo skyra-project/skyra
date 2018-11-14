d2e4094af3231b795ae30a160a4fd8f0ddc61086
Tyler Davis <34944514+Tylertron1998@users.noreply.github.com>
2018-11-14 19:21:08 +0000

    Update Jackpot.js

--- src/lib/util/Games/Jackpot.js
+++ src/lib/util/Games/Jackpot.js
@@ -5,8 +5,7 @@ class Jackpot {
 	}
 
 	async add(id, amount) {
-		const index = this.client.settings.jackpot.findIndex(user => user.id === id);
-		
+		const index = this.client.settings.jackpot.findIndex(user => user.id === id);		
 		const element = { id, amount: index === -1 ? amount : this.client.settings.jackpot[index].amount + amount };
 		await this.client.settings.update('jackpot', element, { arrayPosition: index === -1 ? null : index });
 		return element;

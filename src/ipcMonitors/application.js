c73147fed37552ead9c188aa171db112f8128f8e
kyranet <kyradiscord@gmail.com>
2018-11-19 23:10:59 +0100

    feat: application IPCMonitor

---
+++ src/ipcMonitors/application.js
@@ -0,0 +1,19 @@
+const { IPCMonitor, Duration } = require('../index');
+
+module.exports = class extends IPCMonitor {
+
+	run() {
+		return {
+			users: this.client.users.size,
+			guilds: this.client.guilds.size,
+			channels: this.client.channels.size,
+			shards: this.client.options.shardCount,
+			uptime: Duration.toNow(Date.now() - (process.uptime() * 1000)),
+			latency: this.client.ws.ping.toFixed(0),
+			memory: process.memoryUsage().heapUsed / 1024 / 1024,
+			invite: this.client.invite,
+			...this.client.application
+		};
+	}
+
+};

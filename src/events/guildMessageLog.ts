7df8ad347b681bf0f9804eb33a51230439c25190
Kyra <kyradiscord@gmail.com>
2019-02-24 12:24:57 +0100

    misc: Print better errors in guildMessageLog

--- src/events/guildMessageLog.ts
+++ src/events/guildMessageLog.ts
@@ -1,15 +1,15 @@
 import { DiscordAPIError, Guild, HTTPError, TextChannel } from 'discord.js';
 import { Event } from 'klasa';
 import { Events } from '../lib/types/Enums';
-import { MessageLogsEnum } from '../lib/util/constants';
 import { GuildSettings } from '../lib/types/settings/GuildSettings';
+import { MessageLogsEnum } from '../lib/util/constants';
 
 export default class extends Event {
 
 	public async run(type: MessageLogsEnum, guild: Guild, makeMessage: () => string) {
 		const key = TYPES[type];
 		if (!key) {
-			this.client.emit(Events.Warn, `[EVENT] GuildMessageLog: Unknown type ${type.toString()}`);
+			this.client.emit(Events.Warn, `[EVENT] GuildMessageLog: Unknown type '${type}'`);
 			return;
 		}
 
@@ -26,8 +26,8 @@ export default class extends Event {
 			await channel.send(makeMessage());
 		} catch (error) {
 			this.client.emit(Events.Wtf, error instanceof DiscordAPIError || error instanceof HTTPError
-				? `Failed to send ${type.toString()} log for guild ${guild} in channel ${channel.name}. Error: [${error.code} - ${error.method} | ${error.path}] ${error.message}`
-				: `Failed to send ${type.toString()} log for guild ${guild} in channel ${channel.name}. Error: ${error.message}`);
+				? `Failed to send '${MessageLogsEnum[type]}' log for guild ${guild} in channel ${channel.name}. Error: [${error.code} - ${error.method} | ${error.path}] ${error.message}`
+				: `Failed to send '${MessageLogsEnum[type]}' log for guild ${guild} in channel ${channel.name}. Error: ${error.message}`);
 		}
 	}
 

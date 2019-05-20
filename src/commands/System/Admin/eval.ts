daa8540cf38b1b75338569e3361c9fdb9e61209b
kyranet <kyradiscord@gmail.com>
2019-05-20 15:20:09 +0200

    misc: Put eval on diet

--- src/commands/System/Admin/eval.ts
+++ src/commands/System/Admin/eval.ts
@@ -1,119 +1,18 @@
-import { outputFileAtomic } from 'fs-nextra';
 import { CommandStore, KlasaMessage, Stopwatch, Type, util } from 'klasa';
-import { join } from 'path';
-import { ScriptTarget, transpileModule, TranspileOptions } from 'typescript';
+import { transpileModule, TranspileOptions } from 'typescript';
 import { inspect } from 'util';
 import { SkyraCommand } from '../../../lib/structures/SkyraCommand';
 import { Events } from '../../../lib/types/Enums';
 import { fetch } from '../../../lib/util/util';
-import { rootFolder } from '../../../Skyra';
+import * as transpileOptions from '../../../../tsconfig.json';
 
-const tsTranspileOptions: TranspileOptions = { compilerOptions: { allowJs: true, checkJs: true, target: ScriptTarget.ESNext } };
-
-const BWD_FOLDER = join(rootFolder, 'bwd');
-const CS_FILE = join(BWD_FOLDER, 'cs', 'eval.cs');
-const CS_EXEC = join(BWD_FOLDER, 'cs', 'eval.exe');
-const CPP_FILE = join(BWD_FOLDER, 'cpp', 'eval.cpp');
-const CPP_EXEC = join(BWD_FOLDER, 'cpp', 'eval.exe');
+// @ts-ignore
+const tsTranspileOptions: TranspileOptions = { compilerOptions: transpileOptions.compilerOptions };
 
 enum EvalLanguage {
 	JavaScript,
-	TypeScript,
-	CSharp,
-	CPlusPlus
+	TypeScript
 }
-
-const templates = new Map([
-	[EvalLanguage.CSharp, (code: string) => `
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-		var input = Execute();
-		Console.WriteLine(input == null ? null : input.GetType().FullName);
-
-		if (input is string)
-        {
-            Console.WriteLine(input);
-        }
-        else if (input != null)
-        {
-            Console.WriteLine(input.ToString());
-        }
-    }
-
-    static object Execute()
-    {
-        ${code}
-    }
-}
-	`],
-	[EvalLanguage.CPlusPlus, (code: string) => `
-#include <iostream>
-#include <string>
-#include <typeinfo>
-#include <vector>
-#include <cxxabi.h>
-using namespace std;
-
-int main()
-{
-	${code}
-	cout << abi::__cxa_demangle(typeid(foo).name(), 0, 0, 0);
-	cout << __v;
-    return 0;
-}
-    `]
-]);
-
-const FOREIGN_CONTROLLERS: Record<EvalLanguage, ForeignController> = {
-	[EvalLanguage.JavaScript]: {
-		before(code: string) { return code; },
-		evaluate() { return null; },
-		extract() { return null; }
-	},
-	[EvalLanguage.TypeScript]: {
-		before(code: string) { return code; },
-		evaluate() { return null; },
-		extract() { return null; }
-	},
-	[EvalLanguage.CSharp]: {
-		async before(code: string) {
-			const output = templates.get(EvalLanguage.CSharp)(/return/.test(code) ? code : `${code}\n\t\treturn null;`);
-			await outputFileAtomic(CS_FILE, output);
-			await util.exec(`csc ${CS_FILE} -out:${CS_EXEC}`);
-			return output;
-		},
-		async evaluate() {
-			return (await util.exec(`mono ${CS_EXEC}`)).stdout;
-		},
-		extract(output: string) {
-			const [type, ...rest] = output.split('\n');
-			return { type, value: rest.join('\n') };
-		}
-	},
-	[EvalLanguage.CPlusPlus]: {
-		async before(code: string) {
-			const output = templates.get(EvalLanguage.CPlusPlus)(code.replace(/return (.+)/, 'auto __v = $1;'));
-			await outputFileAtomic(CPP_FILE, code);
-			await util.exec(`g++ ${CPP_FILE} -o ${CPP_EXEC} -Wall -Wextra -Werror -Wconversion -pedantic`);
-			return output;
-		},
-		async evaluate() {
-			return (await util.exec(CPP_EXEC)).stdout;
-		},
-		extract(output: string) {
-			const [type, ...rest] = output.split('\n');
-			return { type, value: rest.join('\n') };
-		}
-	}
-};
 
 export default class extends SkyraCommand {
 
@@ -149,12 +48,6 @@ export default class extends SkyraCommand {
 
 	private getLanguageType(language: string) {
 		switch (language) {
-			case 'csharp':
-			case 'c#':
-			case 'cs': return EvalLanguage.CSharp;
-			case 'cplusplus':
-			case 'c++':
-			case 'cpp': return EvalLanguage.CPlusPlus;
 			case 'typescript':
 			case 'ts': return EvalLanguage.TypeScript;
 			default: return EvalLanguage.JavaScript;
@@ -182,8 +75,6 @@ export default class extends SkyraCommand {
 				return this.nativeEval(message, transpileModule(code, tsTranspileOptions).outputText);
 			case EvalLanguage.JavaScript:
 				return this.nativeEval(message, code);
-			default:
-				return this.foreignEval(message, code, languageType);
 		}
 	}
 
@@ -233,34 +124,13 @@ export default class extends SkyraCommand {
 		return { success, type, time: this.formatTime(syncTime, asyncTime), result: util.clean(result) };
 	}
 
-	private async foreignEval(message: KlasaMessage, code: string, languageType: EvalLanguage) {
-		const controller = FOREIGN_CONTROLLERS[languageType];
-		const stopwatch = new Stopwatch();
-
-		try {
-			const preprocessed = await controller.before(code);
-
-			stopwatch.reset();
-			try {
-				const evaluated = await controller.evaluate(message, preprocessed);
-				const time = this.formatTime(stopwatch.toString(), null);
-				const { type, value } = controller.extract(evaluated);
-				return { success: true, type, time, result: value };
-			} catch (error) {
-				return { success: false, type: 'Error', time: this.formatTime(stopwatch.toString(), null), result: String(error) };
-			}
-		} catch (error) {
-			return { success: false, type: 'CompilerError', time: this.formatTime(stopwatch.toString(), null), result: String(error) };
-		}
-	}
-
 	private formatTime(syncTime: string, asyncTime: string) {
 		return asyncTime ? `⏱ ${asyncTime}<${syncTime}>` : `⏱ ${syncTime}`;
 	}
 
 	private async getHaste(evalResult: string, language: string = 'js') {
-		const { key } = await fetch('https://hastebin.com/documents', { method: 'POST', body: evalResult }, 'json');
-		return `https://hastebin.com/${key}.${language}`;
+		const { key } = await fetch('https://hasteb.in/documents', { method: 'POST', body: evalResult }, 'json');
+		return `https://hasteb.in/${key}.${language}`;
 	}
 
 	private async handleMessage(message: KlasaMessage, options: InternalEvalOptions, { success, result, time, footer, language }: InternalEvalResults) {
@@ -305,19 +175,10 @@ export default class extends SkyraCommand {
 		do {
 			_choice = await message.prompt(`Choose one of the following options: ${_options.join(', ')}`).catch(() => ({ content: 'none' }));
 		}
-		while (!['file', 'haste', 'hastebin', 'console', 'log', 'default', 'none', 'abort', null].includes(_choice.content));
+		while (!_options.concat('none', 'abort').includes(_choice.content));
 		options.sendAs = _choice.content.toLowerCase();
 	}
 
-}
-
-interface ForeignController {
-	before(code: string): string | Promise<string>;
-	evaluate(message: KlasaMessage, code: string): unknown;
-	extract(output: unknown): {
-		type: string;
-		value: string;
-	};
 }
 
 interface InternalEvalResults {

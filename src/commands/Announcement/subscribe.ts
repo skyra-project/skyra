f5b7a005c780d66ba9eff5f81eebb66261a0de19
Skillz <gankstarsleague@gmail.com>
2019-05-31 11:37:22 -0400

    add unique role sets to subscribe command

--- src/commands/Announcement/subscribe.ts
+++ src/commands/Announcement/subscribe.ts
@@ -1,6 +1,7 @@
 import { CommandStore, KlasaMessage } from 'klasa';
 import { SkyraCommand } from '../../lib/structures/SkyraCommand';
 import { announcementCheck } from '../../lib/util/util';
+import { GuildSettings } from '../../lib/types/settings/GuildSettings';
 
 export default class extends SkyraCommand {
 
@@ -16,7 +17,18 @@ export default class extends SkyraCommand {
 
 	public async run(message: KlasaMessage) {
 		const role = announcementCheck(message);
-		await message.member.roles.add(role);
+		const allRoleSets = message.guild.settings.get(GuildSettings.Roles.UniqueRoleSets) as GuildSettings.Roles.UniqueRoleSets;
+
+		const memberRoles = message.member.roles.map(r => r.id);
+
+		for (const set of allRoleSets) {
+			if (!set.roles.includes(role.id)) continue;
+			memberRoles.filter(id => !set.roles.includes(id));
+		}
+
+		memberRoles.push(role.id);
+
+		await message.member.roles.set(memberRoles);
 		return message.sendLocale('COMMAND_SUBSCRIBE_SUCCESS', [role.name]);
 	}
 

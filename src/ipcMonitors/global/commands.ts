c8744364da5f56024e43f2884fa07dbab819092d
kyranet <kyradiscord@gmail.com>
2019-06-01 11:43:55 +0200

    misc: Filter owner commands, add category and requiredPermissions

--- src/ipcMonitors/global/commands.ts
+++ src/ipcMonitors/global/commands.ts
@@ -6,7 +6,7 @@ export default class extends IPCMonitor {
 	public run({ category = null, lang = 'en-US' }: any = {}) {
 		try {
 			const language = this.client.languages.get(lang) || this.client.languages.default;
-			const commands = category ? this.client.commands.filter(cmd => cmd.category === category) : this.client.commands;
+			const commands = (category ? this.client.commands.filter(cmd => cmd.category === category) : this.client.commands).filter(cmd => cmd.permissionLevel < 9);
 			const serializedCommands = commands.map(cmd => ({
 				bucket: cmd.bucket,
 				cooldown: cmd.cooldown,
@@ -14,6 +14,8 @@ export default class extends IPCMonitor {
 				extendedHelp: typeof cmd.extendedHelp === 'function' ? cmd.extendedHelp(language) : cmd.extendedHelp,
 				guildOnly: !cmd.runIn.includes('dm'),
 				name: cmd.name,
+				category: cmd.category,
+				requiredPermissions: cmd.requiredPermissions.toArray(),
 				permissionLevel: cmd.permissionLevel,
 				usage: cmd.usageString
 			}));

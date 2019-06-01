c6452328b610a5c76e6980b15761eb6e8b68188b
kyranet <kyradiscord@gmail.com>
2019-06-01 02:10:21 +0200

    misc: Try harder with this commands thing

--- src/ipcMonitors/global/commands.ts
+++ src/ipcMonitors/global/commands.ts
@@ -7,7 +7,7 @@ export default class extends IPCMonitor {
 		try {
 			const language = this.client.languages.get(lang) || this.client.languages.default;
 			const commands = category ? this.client.commands.filter(cmd => cmd.category === category) : this.client.commands;
-			return commands.map(cmd => ({
+			const serializedCommands = commands.map(cmd => ({
 				bucket: cmd.bucket,
 				cooldown: cmd.cooldown,
 				description: typeof cmd.description === 'function' ? cmd.description(language) : cmd.description,
@@ -17,6 +17,7 @@ export default class extends IPCMonitor {
 				permissionLevel: cmd.permissionLevel,
 				usage: cmd.usageString
 			}));
+			return serializedCommands;
 		} catch (error) {
 			this.client.emit(Events.Wtf, error);
 		}

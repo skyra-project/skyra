b69b1f3722afaa7025571dc02516f4580c8db498
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-06-09 09:15:27 -0400

    Fix Cat Command (#182)
    * new api for getting random cat images
    
    * add a catch incase the fetch fails
    
    * semi
    
    * remove all comments
    
    * fix the kitty command with try catch

--- src/commands/Animal/kitty.ts
+++ src/commands/Animal/kitty.ts
@@ -1,19 +1,9 @@
 import { MessageEmbed } from 'discord.js';
 import { CommandStore, KlasaMessage } from 'klasa';
 import { SkyraCommand } from '../../lib/structures/SkyraCommand';
-import { getColor } from '../../lib/util/util';
+import { getColor, fetch } from '../../lib/util/util';
 
 export default class extends SkyraCommand {
-
-	private readonly ids = [
-		'77227', '60575', '202462', '164687', '344049', '112786', '103656',
-		'384799', '207142', '73164', '42265', '60578', '94171', '78621',
-		'138232', '60533', '73165', '54706', '32208', '25687', '20627',
-		'64954', '136661', '340024', '447939', '457236', '426098', '180398',
-		'313993', '230590', '100241', '54708', '306710', '32510', '344001'
-	];
-
-	private index = Math.ceil(Math.random() * this.ids.length);
 
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
@@ -27,13 +17,20 @@ export default class extends SkyraCommand {
 	}
 
 	public async run(message: KlasaMessage) {
-		if (this.index > this.ids.length - 1) this.index = 0;
-		else this.index += 1;
-
-		return message.sendEmbed(new MessageEmbed()
+		const embed = new MessageEmbed()
 			.setColor(getColor(message) || 0xFFAB2D)
-			.setImage(`https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-${this.ids[this.index]}.jpg`)
-			.setTimestamp());
+			.setTimestamp();
+
+		try {
+			const randomImageBuffer = await fetch('https://cataas.com/cat', 'buffer');
+			embed
+				.attachFiles([{ attachment: randomImageBuffer, name: 'randomcat.jpg' }])
+				.setImage('attachment://randomcat.jpg');
+		} catch {
+			embed
+				.setImage('https://wallpapercave.com/wp/wp3021105.jpg');
+		}
+		return message.sendEmbed(embed);
 	}
 
 }

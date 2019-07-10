2551d5d5422ceecff06878e1703fc7ea285bc337
Skillz <gankstarsleague@gmail.com>
2019-07-10 16:36:15 -0400

    language fixes

--- src/languages/es-ES.ts
+++ src/languages/es-ES.ts
@@ -598,8 +598,8 @@ export default class extends Language {
 			examples: ['6h A hug from Skyra.']
 		}),
 		COMMAND_GIVEAWAYSCHEDULE_DESCRIPTION: `Schedule a giveaway to start at a certain time.`,
-		COMMAND_GIVEAWAYSCHEDULE_EXTENDED: builder.display('giveaway', {
-			extendedHelp: `This command prepares a giveaway to start at a certain time if you do not wish to start it immediately. See the help for giveaway command for more details.`,
+		COMMAND_GIVEAWAYSCHEDULE_EXTENDED: builder.display('gcreate', {
+			extendedHelp: `This command prepares a giveaway to start at a certain time if you do not wish to start it immediately.`,
 			explainedUsage: [
 				['schedule', 'The time to wait before starting the giveaway.'],
 				['time', 'The time the giveaway should last.'],

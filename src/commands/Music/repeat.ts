d74ab13534a91c4babe368fe7293d7eb698666f8
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-07-11 01:54:33 -0400

    add repeat command and its hilarious strings (#216)
    * add repeat command and its hilarious strings
    
    * traduccion a espanol

---
+++ src/commands/Music/repeat.ts
@@ -0,0 +1,21 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+import { MusicCommand } from '../../lib/structures/MusicCommand';
+
+export default class extends MusicCommand {
+
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			aliases: ['replay'],
+			description: language => language.get('COMMAND_REPEAT_DESCRIPTION')
+		});
+	}
+
+	public async run(message: KlasaMessage) {
+		const isAlreadyRepeating = message.guild!.music.replay;
+		// Toggle the repeat option with its opposite value
+		message.guild!.music.setReplay(!isAlreadyRepeating);
+		// Alert the user whether the replay option was enabled or disabled
+		return message.sendLocale('COMMAND_REPEAT_SUCCESS', [!isAlreadyRepeating]);
+	}
+
+}

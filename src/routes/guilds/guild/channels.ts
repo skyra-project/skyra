a6a36b15e9e493fd5a48c3827183df25005e2b04
kyranet <kyradiscord@gmail.com>
2019-09-29 17:11:53 +0200

    routes: Added guilds/:guild/channels

---
+++ src/routes/guilds/guild/channels.ts
@@ -0,0 +1,32 @@
+import { Route, RouteStore } from 'klasa-dashboard-hooks';
+import ApiRequest from '../../../lib/structures/api/ApiRequest';
+import ApiResponse from '../../../lib/structures/api/ApiResponse';
+import { authenticated } from '../../../lib/util/util';
+import { Permissions } from 'discord.js';
+import { flattenChannel } from '../../../lib/util/Models/ApiTransform';
+
+const { FLAGS: { MANAGE_GUILD } } = Permissions;
+
+export default class extends Route {
+
+	public constructor(store: RouteStore, file: string[], directory: string) {
+		super(store, file, directory, { route: 'guilds/:guild/channels' });
+	}
+
+	@authenticated
+	public async get(request: ApiRequest, response: ApiResponse) {
+		const guildID = request.params.guild;
+
+		const guild = this.client.guilds.get(guildID);
+		if (!guild) return response.error(400);
+
+		const member = await guild.members.fetch(request.auth!.user_id).catch(() => null);
+		if (!member) return response.error(400);
+
+		const canManage = member.permissions.has(MANAGE_GUILD);
+		if (!canManage) return response.error(401);
+
+		return response.json(guild.channels.map(flattenChannel));
+	}
+
+}

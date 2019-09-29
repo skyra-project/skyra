455871e792a441059ebc08e4e185cf6dc710fe4e
kyranet <kyradiscord@gmail.com>
2019-09-29 17:12:52 +0200

    routes: Modify /users/@me to use flattenUser
    And not leak other information

--- src/routes/users/user.ts
+++ src/routes/users/user.ts
@@ -2,6 +2,7 @@ import { Route, RouteStore } from 'klasa-dashboard-hooks';
 import ApiRequest from '../../lib/structures/api/ApiRequest';
 import ApiResponse from '../../lib/structures/api/ApiResponse';
 import { authenticated } from '../../lib/util/util';
+import { flattenUser } from '../../lib/util/Models/ApiTransform';
 
 export default class extends Route {
 
@@ -12,7 +13,7 @@ export default class extends Route {
 	@authenticated
 	public async get(request: ApiRequest, response: ApiResponse) {
 		const user = await this.client.users.fetch(request.auth!.user_id);
-		return user ? response.json(user.toJSON()) : response.error(500);
+		return user ? response.json(flattenUser(user)) : response.error(500);
 	}
 
 }

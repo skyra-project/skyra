74b69e289b4dff40f8530a0944b2ebb03388f927
Gryffon Bellish <owenbellish@gmail.com>
2019-10-22 10:14:30 -0400

    feat(reminder): Simplify error catching (#388)
    * Simplify error catching
    
    * Fix lint

--- src/tasks/reminder.ts
+++ src/tasks/reminder.ts
@@ -8,23 +8,18 @@ export default class extends Task {
 	public async run(data: ReminderTaskData) {
 		// Fetch the user to send the message to
 		const user = await this.client.users.fetch(data.user)
-			.catch(error => this._catchErrorUser(error));
+			.catch(error => this._catchError(error));
 
 		if (user) {
 			await user.send(`⏲ Hey! You asked me on ${this.timestamp.displayUTC()} to remind you:\n*${data.content}*`)
-				.catch(error => this._catchErrorMessage(error));
+				.catch(error => this._catchError(error));
 		}
 	}
 
-	private _catchErrorUser(error: DiscordAPIError): void {
-		// 10013: Unknown user
-		if (error.code === 10013) return;
-		throw error;
-	}
-
-	private _catchErrorMessage(error: DiscordAPIError): void {
+	private _catchError(error: DiscordAPIError): void {
 		// 50007: Cannot send messages to this user
-		if (error.code === 50007) return;
+		// 10013: Unknown user
+		if ([50007, 10013].includes(error.code)) return;
 		throw error;
 	}
 

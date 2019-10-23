8bbba32caf8b244e559498255c5cbc8bf0b159a9
Antonio Román <kyradiscord@gmail.com>
2019-10-24 00:47:14 +0200

    misc: Use QB and custom parsers more (#400)
    * misc: Use QB and custom parsers more
    
    * misc: Suggested change from ESLint
    
    * src: More progress and more abstraction
    
    * lint: Resolved all eslint errors
    
    * src: I missed this.

--- src/providers/postgres.ts
+++ src/providers/postgres.ts
@@ -1,10 +1,11 @@
 // Copyright (c) 2017-2018 dirigeants. All rights reserved. MIT license.
 import { QueryBuilder } from '@klasa/querybuilder';
-import { SQLProvider, SchemaEntry, SchemaFolder, Type, SettingsFolderUpdateResult } from 'klasa';
+import { SQLProvider, SchemaEntry, SchemaFolder, SettingsFolderUpdateResult, Type } from 'klasa';
 import { Pool, Submittable, QueryResultRow, QueryArrayConfig, QueryConfig, QueryArrayResult, QueryResult, PoolConfig } from 'pg';
-import { isNumber, mergeDefault } from '@klasa/utils';
+import { mergeDefault } from '@klasa/utils';
 import { DEV_PGSQL } from '../../config';
 import { run as databaseInitRun } from '../lib/util/DatabaseInit';
+import { AnyObject } from '../lib/types/util';
 
 export default class extends SQLProvider {
 
@@ -13,14 +14,13 @@ export default class extends SQLProvider {
 	public qb = new QueryBuilder({
 		array: type => `${type}[]`,
 		arraySerializer: (values, piece, resolver) =>
-			values.length ? `array[${values.map(value => resolver(value, piece)).join(', ')}]` : "'{}'",
+			values.length ? `ARRAY[${values.map(value => resolver(value, piece)).join(', ')}]` : "'{}'",
 		formatDatatype: (name, datatype, def = null) => `"${name}" ${datatype}${def === null ? '' : ` NOT NULL DEFAULT ${def}`}`
 	})
-		.add('boolean', { type: 'BOOL' })
-		.add('integer', { type: ({ max }) => max !== null && max >= 2 ** 32 ? 'BIGINT' : 'INTEGER' })
-		.add('float', { type: 'DOUBLE PRECISION' })
-		.add('uuid', { type: 'UUID' })
-		.add('any', { type: 'JSON', serializer: input => `'${JSON.stringify(input)}'::json` })
+		.add('boolean', { type: 'BOOL', serializer: input => this.cBoolean(input as boolean) })
+		.add('integer', { type: ({ max }) => max !== null && max >= 2 ** 32 ? 'BIGINT' : 'INTEGER', serializer: input => this.cNumber(input as number | bigint) })
+		.add('float', { type: 'DOUBLE PRECISION', serializer: input => this.cNumber(input as number) })
+		.add('any', { type: 'JSON', serializer: input => this.cJson(input as AnyObject), arraySerializer: input => this.cArrayJson(input as AnyObject[]) })
 		.add('json', { 'extends': 'any' })
 		.add('permissionnode', { 'extends': 'any' });
 
@@ -58,43 +58,51 @@ export default class extends SQLProvider {
 		}
 	}
 
-	public createTable(table: string, rows?: readonly any[]) {
-		if (rows) return this.run(`CREATE TABLE ${sanitizeKeyName(table)} (${rows.map(([k, v]) => `${sanitizeKeyName(k)} ${v}`).join(', ')});`);
+	public createTable(table: string, rows?: readonly [string, string][]) {
+		// If rows were given, use them
+		if (rows) {
+			return this.run(/* sql */`
+				CREATE TABLE ${this.cIdentifier(table)} (${rows.map(([k, v]) => `${this.cIdentifier(k)} ${v}`).join(', ')});
+			`);
+		}
+
+		// Otherwise generate datatypes from the schema
 		const gateway = this.client.gateways.get(table);
 		if (!gateway) throw new Error(`There is no gateway defined with the name ${table} nor an array of rows with datatypes have been given. Expected any of either.`);
 
 		const schemaValues = [...gateway.schema.values(true)];
-		// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-		// @ts-ignore 2339
-		const idLength = gateway.idLength || 18;
-		return this.run(`
-			CREATE TABLE ${sanitizeKeyName(table)} (
-				${[`id VARCHAR(${idLength}) PRIMARY KEY NOT NULL UNIQUE`, ...schemaValues.map(this.qb.generateDatatype.bind(this.qb))].join(', ')}
-			)`);
+		const generatedColumns = schemaValues.map(this.qb.generateDatatype.bind(this.qb));
+		const columns = ['"id" VARCHAR(19) NOT NULL UNIQUE', ...generatedColumns];
+		return this.run(/* sql */`
+			CREATE TABLE ${this.cIdentifier(table)} (
+				${columns.join(', ')},
+				PRIMARY KEY(id)
+			);
+		`);
 	}
 
 	public deleteTable(table: string) {
-		return this.run(`DROP TABLE IF EXISTS ${sanitizeKeyName(table)};`);
-	}
-
-	public async countRows(table: string) {
-		const result = await this.runOne(`SELECT COUNT(*) FROM ${sanitizeKeyName(table)};`);
-		return Number(result.count);
+		return this.run(/* sql */`
+			DROP TABLE IF EXISTS ${this.cIdentifier(table)};
+		`);
 	}
 
 	/* Row methods */
 
 	public async getAll(table: string, entries: readonly string[] = []): Promise<unknown[]> {
-		if (entries.length) {
-			const results = await this.runAll(`SELECT * FROM ${sanitizeKeyName(table)} WHERE id IN ('${entries.join("', '")}');`);
-			return results.map(output => this.parseEntry(table, output));
-		}
-		const results = await this.runAll(`SELECT * FROM ${sanitizeKeyName(table)};`);
+		const filter = entries.length ? ` WHERE id IN ('${entries.join("', '")}')` : '';
+		const results = await this.runAll(/* sql */`
+			SELECT *
+			FROM ${this.cIdentifier(table)}${filter};
+		`);
 		return results.map(output => this.parseEntry(table, output));
 	}
 
 	public async getKeys(table: string): Promise<string[]> {
-		const rows = await this.runAll(`SELECT id FROM ${sanitizeKeyName(table)};`);
+		const rows = await this.runAll(/* sql */`
+			SELECT id
+			FROM ${this.cIdentifier(table)};
+		`);
 		return rows.map(row => row.id);
 	}
 
@@ -104,21 +112,25 @@ export default class extends SQLProvider {
 			value = key;
 			key = 'id';
 		}
-		const output = await this.runOne(`SELECT * FROM ${sanitizeKeyName(table)} WHERE ${sanitizeKeyName(key)} = $1 LIMIT 1;`, [value]);
+		const output = await this.runOne(/* sql */`
+			SELECT *
+			FROM ${this.cIdentifier(table)}
+			WHERE
+				${this.cIdentifier(key)} = ${this.cValue(table, key, value)}
+			LIMIT 1;
+		`);
 		return this.parseEntry(table, output);
 	}
 
-	public async has(table: string, id: string): Promise<boolean> {
-		const result = await this.runOne(`SELECT id FROM ${sanitizeKeyName(table)} WHERE id = $1 LIMIT 1;`, [id]);
+	public async has(table: string, id: string) {
+		const result = await this.runOne(/* sql */`
+			SELECT id
+			FROM ${this.cIdentifier(table)}
+			WHERE
+				id = ${this.cString(id)}
+			LIMIT 1;
+		`);
 		return Boolean(result);
-	}
-
-	public async getRandom(table: string): Promise<unknown[]> {
-		return this.runAll(`SELECT * FROM ${sanitizeKeyName(table)} ORDER BY RANDOM() LIMIT 1;`);
-	}
-
-	public getSorted(table: string, key: string, order: 'ASC' | 'DESC' = 'DESC', limitMin?: number, limitMax?: number) {
-		return this.runAll(`SELECT * FROM ${sanitizeKeyName(table)} ORDER BY ${sanitizeKeyName(key)} ${order} ${parseRange(limitMin, limitMax)};`);
 	}
 
 	public create(table: string, id: string, data: CreateOrUpdateValue) {
@@ -129,62 +141,71 @@ export default class extends SQLProvider {
 			keys.push('id');
 			values.push(id);
 		}
-		return this.pgsql!.query(`
-			INSERT INTO ${sanitizeKeyName(table)} (${keys.map(sanitizeKeyName).join(', ')})
-			VALUES (${Array.from({ length: keys.length }, (__, i) => `$${i + 1}`).join(', ')});`, values);
+		return this.pgsql!.query(/* sql */`
+			INSERT INTO ${this.cIdentifier(table)} (${keys.map(this.cIdentifier.bind(this)).join(', ')})
+			VALUES (${this.cValues(table, keys, values).join(', ')});
+		`);
 	}
 
 	public update(table: string, id: string, data: CreateOrUpdateValue) {
 		const [keys, values] = this.parseUpdateInput(data, false);
-		return this.pgsql!.query(`
-			UPDATE ${sanitizeKeyName(table)}
-			SET ${keys.map((key, i: number) => `${sanitizeKeyName(key)} = $${i + 1}`)}
-			WHERE id = '${id.replace(/'/g, "''")}';`, values);
+		const resolvedValues = this.cValues(table, keys, values);
+		return this.pgsql!.query(/* sql */`
+			UPDATE ${this.cIdentifier(table)}
+			SET ${keys.map((key, i) => `${this.cIdentifier(key)} = ${resolvedValues[i]}`)}
+			WHERE id = ${this.cString(id)};
+		`);
 	}
 
 	public replace(table: string, id: string, data: CreateOrUpdateValue) {
 		return this.update(table, id, data);
 	}
 
-	public incrementValue(table: string, id: string, key: string, amount = 1) {
-		return this.run(`UPDATE ${sanitizeKeyName(table)} SET $2 = $2 + $3 WHERE id = $1;`, [id, key, amount]);
-	}
-
-	public decrementValue(table: string, id: string, key: string, amount = 1) {
-		return this.run(`UPDATE ${sanitizeKeyName(table)} SET $2 = GREATEST(0, $2 - $3) WHERE id = $1;`, [id, key, amount]);
-	}
-
 	public delete(table: string, id: string) {
-		return this.run(`DELETE FROM ${sanitizeKeyName(table)} WHERE id = $1;`, [id]);
+		return this.run(/* sql */`
+			DELETE FROM ${this.cIdentifier(table)}
+			WHERE id = ${this.cString(id)};
+		`);
 	}
 
 	public addColumn(table: string, column: SchemaFolder | SchemaEntry) {
-		return this.run(column instanceof SchemaFolder
-			? `ALTER TABLE ${sanitizeKeyName(table)} ${[...column.values(true)].map(subpiece => `ADD COLUMN ${this.qb.generateDatatype(subpiece)}`).join(', ')};`
-			: `ALTER TABLE ${sanitizeKeyName(table)} ADD COLUMN ${this.qb.generateDatatype(column)};`);
+		const escapedTable = this.cIdentifier(table);
+		const columns = (column instanceof SchemaFolder ? [...column.values(true)] : [column])
+			.map(subpiece => `ADD COLUMN ${this.qb.generateDatatype(subpiece)}`).join(', ');
+		return this.run(/* sql */`
+			ALTER TABLE ${escapedTable} ${columns};
+		`);
 	}
 
 	public removeColumn(table: string, columns: string | string[]) {
-		if (typeof columns === 'string') return this.run(`ALTER TABLE ${sanitizeKeyName(table)} DROP COLUMN ${sanitizeKeyName(columns)};`);
-		if (Array.isArray(columns)) return this.run(`ALTER TABLE ${sanitizeKeyName(table)} DROP COLUMN ${columns.map(sanitizeKeyName).join(', ')};`);
-		throw new TypeError('Invalid usage of PostgreSQL#removeColumn. Expected a string or string[].');
+		const escapedTable = this.cIdentifier(table);
+		const escapedColumns = typeof columns === 'string' ? this.cIdentifier(columns) : columns.map(this.cIdentifier.bind(this)).join(', ');
+		return this.run(/* sql */`
+			ALTER TABLE ${escapedTable}
+			DROP COLUMN ${escapedColumns};
+		`);
 	}
 
 	public updateColumn(table: string, entry: SchemaEntry) {
 		const [column, datatype] = this.qb.generateDatatype(entry).split(' ');
-		return this.pgsql!.query(`ALTER TABLE ${sanitizeKeyName(table)} ALTER COLUMN ${column} TYPE ${datatype}${entry.default
-			? `, ALTER COLUMN ${column} SET NOT NULL, ALTER COLUMN ${column} SET DEFAULT ${this.qb.serialize(entry.default, entry)}`
-			: ''
-		};`);
+		const defaultConstraint = entry.default === null
+			? ''
+			: `, ALTER COLUMN ${column} SET NOT NULL, ALTER COLUMN ${column} SET DEFAULT ${this.qb.serialize(entry.default, entry)}`;
+
+		return this.pgsql!.query(/* sql */`
+			ALTER TABLE ${this.cIdentifier(table)}
+			ALTER COLUMN ${column}
+			TYPE ${datatype}${defaultConstraint};`);
 	}
 
 	public async getColumns(table: string, schema = 'public') {
-		const result = await this.runAll(`
+		const result = await this.runAll(/* sql */`
 			SELECT column_name
 			FROM information_schema.columns
-			WHERE table_schema = $1
-				AND table_name = $2;
-		`, [schema, table]);
+			WHERE
+				table_schema = ${this.cString(schema)} AND
+				table_name = ${this.cString(table)};
+		`);
 		return result.map(row => row.column_name);
 	}
 
@@ -218,43 +239,102 @@ export default class extends SQLProvider {
 		return results.rows[0] || null;
 	}
 
-}
+	private cValue(table: string, key: string, value: unknown) {
+		const gateway = this.client.gateways.get(table);
+		if (typeof gateway === 'undefined') return this.cUnknown(value);
 
-/**
- * @param value The string to sanitize as a key
- */
-function sanitizeKeyName(value: string) {
-	if (typeof value !== 'string') throw new TypeError(`[SANITIZE_NAME] Expected a string, got: ${new Type(value)}`);
-	if (/`|"/.test(value)) throw new TypeError(`Invalid input (${value}).`);
-	if (value.startsWith('"') && value.endsWith('"')) return value;
-	return `"${value}"`;
-}
+		const entry = gateway.schema.get(key) as SchemaEntry;
+		if (!entry || entry.type === 'Folder') return this.cUnknown(value);
 
-/**
- * @param min The minimum value
- * @param max The maximum value
- */
-function parseRange(min?: number, max?: number) {
-	// Min value validation
-	if (typeof min === 'undefined') return '';
-	if (!isNumber(min)) {
-		throw new TypeError(`[PARSE_RANGE] 'min' parameter expects an integer or undefined, got ${min}`);
-	}
-	if (min < 0) {
-		throw new RangeError(`[PARSE_RANGE] 'min' parameter expects to be equal or greater than zero, got ${min}`);
+		const qbEntry = this.qb.get(entry.type);
+		return qbEntry
+			? entry.array
+				? qbEntry.arraySerializer(value as unknown[], entry, qbEntry.serializer)
+				: qbEntry.serializer(value, entry)
+			: this.cUnknown(value);
 	}
 
-	// Max value validation
-	if (typeof max !== 'undefined') {
-		if (!isNumber(max)) {
-			throw new TypeError(`[PARSE_RANGE] 'max' parameter expects an integer or undefined, got ${max}`);
+	private cValues(table: string, keys: readonly string[], values: readonly unknown[]) {
+		const gateway = this.client.gateways.get(table);
+		if (typeof gateway === 'undefined') return values.map(value => this.cUnknown(value));
+
+		const { schema } = gateway;
+		const parsedValues: string[] = [];
+		for (let i = 0; i < keys.length; ++i) {
+			const key = keys[i];
+			const value = values[i];
+			const entry = schema.get(key) as SchemaEntry;
+			if (!entry || entry.type === 'Folder') {
+				parsedValues.push(this.cUnknown(value));
+				continue;
+			}
+
+			const qbEntry = this.qb.get(entry.type);
+			parsedValues.push(qbEntry
+				? entry.array
+					? qbEntry.arraySerializer(value as unknown[], entry, qbEntry.serializer)
+					: qbEntry.serializer(value, entry)
+				: this.cUnknown(value));
 		}
-		if (max <= min) {
-			throw new RangeError(`[PARSE_RANGE] 'max' parameter expects ${max} to be greater than ${min}. Got: ${max} <= ${min}`);
+		return parsedValues;
+	}
+
+	private cIdentifier(identifier: string) {
+		const escaped = identifier.replace(/\\/g, '\\\\').replace(/"/g, '\\"');
+		return `"${escaped}"`;
+	}
+
+	private cString(value: string) {
+		const escaped = value.replace(/'/g, "''");
+		return `'${escaped}'`;
+	}
+
+	private cNumber(value: number | bigint) {
+		return value.toString();
+	}
+
+	private cBoolean(value: boolean) {
+		return value ? 'TRUE' : 'FALSE';
+	}
+
+	private cDate(value: Date) {
+		return this.cNumber(value.getTime());
+	}
+
+	private cArray(value: readonly unknown[]) {
+		return `ARRAY[${value.map(this.cUnknown.bind(this)).join(', ')}]`;
+	}
+
+	private cJson(value: AnyObject) {
+		const escaped = this.cString(JSON.stringify(value));
+		return `${escaped}::JSON`;
+	}
+
+	private cArrayJson(value: AnyObject[]) {
+		return `ARRAY[${value.map(json => this.cString(JSON.stringify(json)))}]::JSON[]`;
+	}
+
+	private cUnknown(value: unknown): string {
+		switch (typeof value) {
+			case 'string':
+				return this.cString(value);
+			case 'bigint':
+			case 'number':
+				return this.cNumber(value);
+			case 'boolean':
+				return this.cBoolean(value);
+			case 'object':
+				if (value === null) return 'NULL';
+				if (Array.isArray(value)) return this.cArray(value);
+				if (value instanceof Date) return this.cDate(value);
+				return this.cJson(value);
+			case 'undefined':
+				return 'NULL';
+			default:
+				throw new TypeError(`Cannot serialize a ${new Type(value)}`);
 		}
 	}
 
-	return `LIMIT ${min}${typeof max === 'number' ? `,${max}` : ''}`;
 }
 
 type CreateOrUpdateValue = SettingsFolderUpdateResult[] | [string, unknown][] | Record<string, unknown>;

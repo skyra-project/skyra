b5be8f9cfa6f7c97100178c606b6b8b928f6ea28
Antonio Román <kyradiscord@gmail.com>
2019-11-13 20:24:31 +0100

    fix: Resolved one small bug

--- src/routes/users/leaderboard.ts
+++ src/routes/users/leaderboard.ts
@@ -14,11 +14,11 @@ export default class extends Route {
 		const limit = 'limit' in request.query ? Number(request.query.limit) : 10;
 		if (!Number.isInteger(limit) || limit <= 0 || limit > 100) return response.error(400);
 
-		const after = 'after' in request.query ? Number(request.query.after) : 0;
+		const after = 'after' in request.query ? Number(request.query.after) : 1;
 		if (!Number.isInteger(after) || after <= 0 || after > 25000 - limit) return response.error(400);
 
 		const leaderboard = await this.client.leaderboard.fetch();
-		const results = iteratorRange(leaderboard.entries(), after, limit);
+		const results = iteratorRange(leaderboard.entries(), after - 1, limit);
 
 		return response.json(await fetchAllLeaderboardEntries(this.client, results));
 	}

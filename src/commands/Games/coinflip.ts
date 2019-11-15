f98b9277fff330e9a6eae10fc6c53e027a8492fd
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2019-11-15 17:12:37 +0200

    refactor: use the cdn for image serving

--- src/commands/Games/coinflip.ts
+++ src/commands/Games/coinflip.ts
@@ -4,8 +4,6 @@ import { UserSettings } from '../../lib/types/settings/UserSettings';
 import { MessageEmbed } from 'discord.js';
 import { getColor } from '../../lib/util/util';
 
-const HEADS_IMAGE = 'https://camo.githubusercontent.com/54e9b61b351bbaf60be7559869e9d5f86808dfd5/68747470733a2f2f73746f726167652e676f6f676c65617069732e636f6d2f646174612d73756e6c696768742d3134363331332e61707073706f742e636f6d2f726962626f6e2f646e6468656164732e706e67';
-const TAILS_IMAGE = 'https://camo.githubusercontent.com/a1a9b13ceafcebe5e0be546065859b733d069443/68747470733a2f2f73746f726167652e676f6f676c65617069732e636f6d2f646174612d73756e6c696768742d3134363331332e61707073706f742e636f6d2f726962626f6e2f646e647461696c732e706e67';
 export default class extends SkyraCommand {
 
 	public constructor(store: CommandStore, file: string[], directory: string) {
@@ -29,7 +27,7 @@ export default class extends SkyraCommand {
 		const resultEmbed = new MessageEmbed()
 			.setColor(getColor(message));
 		if (money < wager) {
-			throw 'You can\'t wager more money than you have!'
+			throw 'You can\'t wager more money than you have!';
 		}
 
 		const result = Math.random() > 0.5 ? 'heads' : 'tails';
@@ -38,14 +36,14 @@ export default class extends SkyraCommand {
 			resultEmbed
 				.setTitle('You won!')
 				.setDescription(`The coin was flipped, and it showed ${result.charAt(0).toUpperCase() + result.slice(1)}! You won ${wager} shinies!`)
-				.setThumbnail(guess === 'heads' ? HEADS_IMAGE : TAILS_IMAGE);
+				.setThumbnail(`https://cdn.skyra.pw/img/coins/${result}.png`);
 			return message.sendEmbed(resultEmbed);
 		}
 		await message.author.settings.update(UserSettings.Money, money - wager);
 		resultEmbed
 			.setTitle('You lost!')
 			.setDescription(`The coin was flipped, and it showed ${result.charAt(0).toUpperCase() + result.slice(1)}. You lost ${wager} shinies.`)
-			.setThumbnail(guess === 'heads' ? HEADS_IMAGE : TAILS_IMAGE);
+			.setThumbnail(`https://cdn.skyra.pw/img/coins/${result}.png`);
 		return message.sendEmbed(resultEmbed);
 	}
 

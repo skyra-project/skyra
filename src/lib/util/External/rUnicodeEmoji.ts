f2dd5fb449174d37a770f366d83f1af2b545e615
Antonio Román <kyradiscord@gmail.com>
2019-11-16 14:29:43 +0100

    feat: Added all of Emoji 12.1.0 (#528)

--- src/lib/util/External/rUnicodeEmoji.ts
+++ src/lib/util/External/rUnicodeEmoji.ts
@@ -1,2 +1,4 @@
-export const REGEX_UNICODE_EMOJI = /\p{Emoji_Presentation}|\p{Emoji}\uFE0F|\p{Emoji_Modifier_Base}/gu;
+import emojiRegex = require('emoji-regex/es2015');
+
+export const REGEX_UNICODE_EMOJI = emojiRegex();
 export const REGEX_UNICODE_BOXNM = /^\d\u20E3$/;

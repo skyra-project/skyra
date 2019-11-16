d5f4812fef6a7efbafabd7c6e58670cdfcabf2a8
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2019-11-16 09:59:24 +0200

    refactor: argument now returns an enum member
    Co-Authored-By: Antonio Román <kyradiscord@gmail.com>

--- src/commands/Games/coinflip.ts
+++ src/commands/Games/coinflip.ts
@@ -22,13 +22,13 @@ export default class extends SkyraCommand {
 
 		this.createCustomResolver('cointype', (arg, possible, message) => {
 			const lArg = arg.toLowerCase();
-			const face = message.language.tget('COMMAND_COINFLIP_COINNAMES').find(coin => coin.toLowerCase() === lArg);
-			if (!face) throw message.language.tget('COMMAND_COINFLIP_INVALID_COINNAME', cleanMentions(message.guild!, arg));
+			const face = message.language.tget('COMMAND_COINFLIP_COINNAMES').findIndex(coin => coin.toLowerCase() === lArg);
+			if (!face === -1) throw message.language.tget('COMMAND_COINFLIP_INVALID_COINNAME', cleanMentions(message.guild!, arg));
 			return face;
 		});
 	}
 
-	public async run(message: KlasaMessage, [bet, guess]: [string, string]) {
+	public async run(message: KlasaMessage, [bet, guess]: [string, CoinTypes]) {
 		await message.author.settings.sync();
 		const wager = Number(bet);
 		const money = message.author.settings.get(UserSettings.Money);
@@ -40,13 +40,13 @@ export default class extends SkyraCommand {
 		const coinNames = message.language.tget('COMMAND_COINFLIP_COINNAMES');
 
 		const result = Math.random() > 0.5 ? CoinTypes.Heads : CoinTypes.Tails;
-		const updatedBalance = coinNames[result] === guess ? money + wager : money - wager;
+		const updatedBalance = result === guess ? money + wager : money - wager;
 
 		await message.author.settings.update(UserSettings.Money, updatedBalance);
 		return message.sendEmbed(new MessageEmbed()
 			.setColor(getColor(message))
-			.setTitle(message.language.tget(coinNames[result] === guess ? 'COMMAND_COINFLIP_WIN_TITLE' : 'COMMAND_COINFLIP_LOSE_TITLE'))
-			.setDescription(message.language.tget(coinNames[result] === guess ? 'COMMAND_COINFLIP_WIN_DESCRIPTION' : 'COMMAND_COINFLIP_LOSE_DESCRIPTION', coinNames[result], wager))
+			.setTitle(message.language.tget(result === guess ? 'COMMAND_COINFLIP_WIN_TITLE' : 'COMMAND_COINFLIP_LOSE_TITLE'))
+			.setDescription(message.language.tget(result === guess ? 'COMMAND_COINFLIP_WIN_DESCRIPTION' : 'COMMAND_COINFLIP_LOSE_DESCRIPTION', coinNames[result], wager))
 			.setThumbnail(`https://cdn.skyra.pw/img/coins/${result === CoinTypes.Heads ? 'heads' : 'tails'}.png`));
 	}
 

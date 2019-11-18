1990fa49269329f098c92e5fe18ff7fbd4c64056
Nejc <ethoarx@gmail.com>
2019-11-18 16:43:16 +0100

    settings

--- src/lib/types/settings/raw/RawSubscriptionSettings.ts
+++ src/lib/types/settings/raw/RawSubscriptionSettings.ts
@@ -1,20 +1,20 @@
-// TODO: Add all other planned types
-
 export interface RawSubscriptionSettings {
 	id: string;
-	isStreaming: boolean;
-	createdAt: number;
-	fetchedAt: number;
-	expiresAt: number;
+	is_streaming: boolean;
+	created_at: number;
+	fetched_at: number;
+	expires_at: number;
+	guild_ids: string[];
 }
 
 export const SQL_TABLE_SCHEMA = /* sql */`
 	CREATE TABLE IF NOT EXISTS subscriptions (
-		"id"           VARCHAR(16)   NOT NULL,
-		"isStreaming"  BOOLEAN       NOT NULL,
-		"createdAt"    BIGINT        NOT NULL,
-		"fetchedAt"    BIGINT        NOT NULL,
-		"expiresAt"    BIGINT        NOT NULL,
-		PRIMARY KEY("id"),
+		"id"            VARCHAR(16)                               NOT NULL,
+		"is_streaming"  BOOLEAN                                   NOT NULL,
+		"created_at"    BIGINT                                    NOT NULL,
+		"fetched_at"    BIGINT                                    NOT NULL,
+		"expires_at"    BIGINT                                    NOT NULL,
+		"guild_ids"     VARCHAR(19)[] DEFAULT ARRAY[]::VARCHAR[]  NOT NULL,
+		PRIMARY KEY("id")
 	);
 `;

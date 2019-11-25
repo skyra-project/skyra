e746d4547568e183e4e40c9c9b01223370757b08
gc <gc@users.noreply.github.com>
2019-11-26 03:09:33 +1100

    add post route for syncing user

--- src/routes/oauth/oauthUser.ts
+++ src/routes/oauth/oauthUser.ts
@@ -1,5 +1,8 @@
 import fetch from 'node-fetch';
 import { Route, RouteStore } from 'klasa-dashboard-hooks';
+import { ratelimit, authenticated } from '../../lib/util/util';
+import ApiRequest from '../../lib/structures/api/ApiRequest';
+import ApiResponse from '../../lib/structures/api/ApiResponse';
 
 export default class extends Route {
 
@@ -15,6 +18,22 @@ export default class extends Route {
 		user.guilds = await fetch('https://discordapp.com/api/users/@me/guilds', { headers: { Authorization: token } })
 			.then(result => result.json());
 		return this.client.dashboardUsers.add(user);
+	}
+
+	@authenticated
+	@ratelimit(1, 5000, true)
+	public async post(request: ApiRequest, response: ApiResponse) {
+		const requestBody = request.body as Record<string, string>;
+		if (!requestBody.action) {
+			return response.error(400);
+		}
+
+		if (requestBody.action === 'SYNC_USER') {
+			const user = await this.api(request.auth!.token);
+			return response.json({ user });
+		}
+
+		return response.error(400);
 	}
 
 }

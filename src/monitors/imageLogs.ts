22b46aa03b122543b5344d9a1dd6a25ecd9feb45
Antonio Román <kyradiscord@gmail.com>
2019-11-26 20:09:56 +0100

    feat: Added support for multiple image logs

--- src/monitors/imageLogs.ts
+++ src/monitors/imageLogs.ts
@@ -3,7 +3,7 @@ import { KlasaMessage, Monitor, util } from 'klasa';
 import { Events } from '../lib/types/Enums';
 import { GuildSettings } from '../lib/types/settings/GuildSettings';
 import { MessageLogsEnum } from '../lib/util/constants';
-import { getAttachment, fetch, FetchResultTypes } from '../lib/util/util';
+import { fetch, FetchResultTypes, IMAGE_EXTENSION } from '../lib/util/util';
 import { extname } from 'path';
 
 const MAXIMUM_SIZE = 300;
@@ -14,36 +14,35 @@ const MAXIMUM_LENGTH = 1024 * 1024;
 export default class extends Monitor {
 
 	public async run(message: KlasaMessage) {
-		const image = getAttachment(message);
-		if (image === null) return;
+		for (const image of this.getAttachments(message)) {
+			const dimensions = this.getDimensions(image.width, image.height);
+			if (!dimensions.height || !dimensions.width) return;
+			const url = new URL(image.proxyURL);
+			url.searchParams.append('width', dimensions.width.toString());
+			url.searchParams.append('height', dimensions.height.toString());
 
-		const dimensions = this.getDimensions(image.width, image.height);
-		if (!dimensions.height || !dimensions.width) return;
-		const url = new URL(image.proxyURL);
-		url.searchParams.append('width', dimensions.width.toString());
-		url.searchParams.append('height', dimensions.height.toString());
+			const result = await fetch(url, FetchResultTypes.Result).catch(error => {
+				throw new Error(`ImageLogs[${error}] ${url}`);
+			});
+			const contentLength = result.headers.get('content-length');
+			if (contentLength === null) return;
 
-		const result = await fetch(url, FetchResultTypes.Result).catch(error => {
-			throw new Error(`ImageLogs[${error}] ${url}`);
-		});
-		const contentLength = result.headers.get('content-length');
-		if (contentLength === null) return;
+			const parsedContentLength = parseInt(contentLength, 10);
+			if (!util.isNumber(parsedContentLength)) return;
+			if (parsedContentLength > MAXIMUM_LENGTH) return;
 
-		const parsedContentLength = parseInt(contentLength, 10);
-		if (!util.isNumber(parsedContentLength)) return;
-		if (parsedContentLength > MAXIMUM_LENGTH) return;
+			const buffer = await result.buffer();
+			const filename = `image${extname(url.pathname)}`;
 
-		const buffer = await result.buffer();
-		const filename = `image${extname(url.pathname)}`;
-
-		this.client.emit(Events.GuildMessageLog, MessageLogsEnum.Image, message.guild, () => new MessageEmbed()
-			.setColor(0xEFAE45)
-			.setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL({ size: 128 }))
-			.setDescription(`[${message.language.tget('JUMPTO')}](${message.url})`)
-			.setFooter(`#${(message.channel as TextChannel).name}`)
-			.attachFiles([new MessageAttachment(buffer, filename)])
-			.setImage(`attachment://${filename}`)
-			.setTimestamp());
+			this.client.emit(Events.GuildMessageLog, MessageLogsEnum.Image, message.guild, () => new MessageEmbed()
+				.setColor(0xEFAE45)
+				.setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL({ size: 128 }))
+				.setDescription(`[${message.language.tget('JUMPTO')}](${message.url})`)
+				.setFooter(`#${(message.channel as TextChannel).name}`)
+				.attachFiles([new MessageAttachment(buffer, filename)])
+				.setImage(`attachment://${filename}`)
+				.setTimestamp());
+		}
 	}
 
 	public shouldRun(message: KlasaMessage) {
@@ -58,6 +57,19 @@ export default class extends Monitor {
 			&& !message.guild.settings.get(GuildSettings.Selfmod.IgnoreChannels).includes(message.channel.id);
 	}
 
+	private *getAttachments(message: KlasaMessage) {
+		for (const attachment of message.attachments.values()) {
+			if (!IMAGE_EXTENSION.test(attachment.url)) continue;
+
+			yield {
+				url: attachment.url,
+				proxyURL: attachment.proxyURL,
+				height: attachment.height!,
+				width: attachment.width!
+			};
+		}
+	}
+
 	private getDimensions(width: number, height: number) {
 		if (width > height) {
 			// Landscape
@@ -68,10 +80,8 @@ export default class extends Monitor {
 			// width: 300
 			// height: 450 / (900 / 300) -> 450 / 3 -> 150
 			// 900x450 -> 300x150 keeps 2:1 proportion
-			return {
-				width: MAXIMUM_SIZE,
-				height: Math.floor(height / (width / MAXIMUM_SIZE))
-			};
+			const scaledHeight = Math.floor(height / (width / MAXIMUM_SIZE));
+			return scaledHeight === 0 ? { width, height } : { width: MAXIMUM_SIZE, height: scaledHeight };
 		}
 
 		if (width < height) {
@@ -83,10 +93,8 @@ export default class extends Monitor {
 			// width: 450 / (900 / 300) -> 450 / 3 -> 150
 			// height: 300
 			// 450x900 -> 150x300 keeps 1:2 proportion
-			return {
-				width: Math.floor(width / (height / MAXIMUM_SIZE)),
-				height: MAXIMUM_SIZE
-			};
+			const scaledWidth = Math.floor(width / (height / MAXIMUM_SIZE));
+			return scaledWidth === 0 ? { width, height } : { width: scaledWidth, height: MAXIMUM_SIZE };
 		}
 
 		// Square

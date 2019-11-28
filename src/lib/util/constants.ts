dcb6296d67e4a7ed8b9ad3f87096cc462b2902ec
Nejc <ethoarx@gmail.com>
2019-11-28 18:00:31 +0100

    refactor(Mime): dead code

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -187,20 +187,6 @@ export namespace Mime {
 		TextPlain = 'text/plain'
 	}
 
-	export const Data = new Map<Types, TypeData>([
-		[Types.ApplicationJson, { source: 'iana', charset: 'UTF-8', compressible: true, extensions: ['json', 'map'] }],
-		[Types.ApplicationTwitchV5Json, { source: 'twitch', extensions: ['json'] }],
-		[Types.ApplicationFormUrlEncoded, { source: 'iana', compressible: true }],
-		[Types.TextPlain, { source: 'iana', compressible: true, extensions: ['txt', 'text', 'conf', 'def', 'list', 'log', 'in', 'ini'] }]
-	]) as ReadonlyMap<Types, TypeData>;
-
-	export interface TypeData {
-		source: string;
-		charset?: string;
-		compressible?: boolean;
-		extensions?: ReadonlyArray<string>;
-	}
-
 }
 
 export const enum APIErrors {

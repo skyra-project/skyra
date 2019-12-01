6143bd2cbc1aca21e55c2aad9fd651593d205b99
Antonio Román <kyradiscord@gmail.com>
2019-12-01 10:22:40 +0100

    fix: Resolved emoji resolver misbehaving

--- src/lib/util/External/rUnicodeEmoji.ts
+++ src/lib/util/External/rUnicodeEmoji.ts
@@ -1,4 +1,6 @@
 import emojiRegex = require('emoji-regex/es2015');
 
-export const REGEX_UNICODE_EMOJI = emojiRegex();
+// This is dumb, but Mathias's package does the regex global.
+// This is a workaround I hate, but I have to deal with it.
+export const REGEX_UNICODE_EMOJI = new RegExp(emojiRegex().source, 'u');
 export const REGEX_UNICODE_BOXNM = /^\d\u20E3$/;

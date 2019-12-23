38252502594d86fb1feef913907c75493a3a1df6
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2019-12-23 15:18:44 +0200

    refactor: export the schema in a kyra-approved:tm: way

--- src/lib/schemas/Banners.ts
+++ src/lib/schemas/Banners.ts
@@ -1,9 +1,7 @@
 import { Schema } from 'klasa';
 
-const BannerSchema = new Schema()
+export const BannerSchema = new Schema()
 	.add('group', 'string')
 	.add('title', 'string')
 	.add('author_id', 'string')
 	.add('price', 'number');
-
-export { BannerSchema };

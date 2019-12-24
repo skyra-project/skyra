ab86511f0d8b9982a6a273d5441eb49f286eae4c
kyraNET <kyradiscord@gmail.com>
2019-12-24 11:08:59 +0100

    fix: Resolved echo command's bugs

--- src/commands/System/echo.ts
+++ src/commands/System/echo.ts
@@ -12,25 +12,24 @@ export default class extends SkyraCommand {
 			extendedHelp: language => language.tget('COMMAND_ECHO_EXTENDED'),
 			guarded: true,
 			permissionLevel: PermissionLevels.BotOwner,
-			usage: '[channel:channelname{2}] [message:string] [...]',
+			usage: '[channel:channel] [message:...string]',
 			usageDelim: ' '
 		});
 	}
 
-	public async run(message: KlasaMessage, [channel = message.channel as TextChannel, ...content]: [TextChannel?, ...string[]]) {
+	public async run(message: KlasaMessage, [channel = message.channel as TextChannel, content]: [TextChannel, string]) {
 		if (message.deletable) message.nuke().catch(() => null);
 
 		const attachment = message.attachments.size ? message.attachments.first()!.url : null;
-		const mesContent = content.length ? content.join(' ') : '';
 
-		if (!mesContent && !attachment) {
+		if (!content.length && !attachment) {
 			throw 'I have no content nor attachment to send, please write something.';
 		}
 
 		const options: MessageOptions = {};
 		if (attachment) options.files = [{ attachment }];
 
-		await channel.send(mesContent, options);
+		await channel.send(content.length, options);
 		if (channel !== message.channel) await message.alert(`Message successfully sent to ${channel}`);
 
 		return message;

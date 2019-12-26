73f3f7c132a2e960a595de71dc3d4dece931937a
Antonio Román <kyradiscord@gmail.com>
2019-12-26 12:03:45 +0100

    fix: Make remove command not require SAME_VOICE_CHANNEL

--- src/commands/Music/remove.ts
+++ src/commands/Music/remove.ts
@@ -6,7 +6,7 @@ export default class extends MusicCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
 			description: language => language.tget('COMMAND_REMOVE_DESCRIPTION'),
-			music: ['QUEUE_NOT_EMPTY', 'SAME_VOICE_CHANNEL'],
+			music: ['QUEUE_NOT_EMPTY'],
 			usage: '<number:integer>'
 		});
 	}
@@ -19,7 +19,7 @@ export default class extends MusicCommand {
 
 		index--;
 		const song = music.queue[index];
-		if (song.requester !== message.author.id && !await message.hasAtLeastPermissionLevel(5)) {
+		if (song.requester !== message.author.id && !message.member!.isDJ) {
 			throw message.language.tget('COMMAND_REMOVE_DENIED');
 		}
 

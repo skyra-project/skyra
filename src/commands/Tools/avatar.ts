687672be23491fcc3e884abde72930a31b4dcdc2
Jeroen Claassens <support@favware.tech>
2020-01-04 12:42:13 +0100

    fix: avatar as png when not animated because ios discord is dumb (#721)

--- src/commands/Tools/avatar.ts
+++ src/commands/Tools/avatar.ts
@@ -22,10 +22,12 @@ export default class extends SkyraCommand {
 
 	public async run(message: KlasaMessage, [user]: [KlasaUser]) {
 		if (!user.avatar) throw message.language.tget('COMMAND_AVATAR_NONE');
+
+		const format = user.avatar.startsWith('a_') ? 'gif' : 'png';
 		return message.sendEmbed(new MessageEmbed()
-			.setAuthor(user.tag, user.avatarURL({ size: 128 })!)
+			.setAuthor(user.tag, user.avatarURL({ format, size: 128 })!)
 			.setColor(getColor(message))
-			.setImage(user.avatarURL({ size: 2048 })!));
+			.setImage(user.avatarURL({ format, size: 2048 })!));
 	}
 
 }

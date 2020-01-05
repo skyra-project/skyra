a90b5083fa516b2cfc50d21a03c6140bdd596a6a
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2020-01-05 19:52:28 +0200

    feat: finish resolve-suggestion

--- src/commands/Suggestions/resolve-suggestion.ts
+++ src/commands/Suggestions/resolve-suggestion.ts
@@ -1,12 +1,11 @@
-import { CommandStore, KlasaMessage } from 'klasa';
+import { CommandStore, KlasaMessage, Serializer } from 'klasa';
 import { SkyraCommand } from '../../lib/structures/SkyraCommand';
 import { GuildSettings } from '../../lib/types/settings/GuildSettings';
 import { PermissionLevels } from '../../lib/types/Enums';
-import { MessageEmbed } from 'discord.js';
+import { MessageEmbed, TextChannel } from 'discord.js';
 import { Emojis } from '../../lib/util/constants';
 
-const IdRegex = /\((\d{16,18})\)/;
-
+const IdRegex = /\d{16,18}/;
 const enum SuggestionsColors {
 	ACCEPTED = '4CB02C',
 	CONSIDERED = 'CFA08D',
@@ -32,25 +31,27 @@ export default class extends SkyraCommand {
 			flagSupport: true
 		});
 
-		this.createCustomResolver('message', async (arg, possible, message) => {
-			const suggestionMessage = await this.client.arguments.get('message')!.run(arg, possible, message) as unknown as KlasaMessage;
+		this.createCustomResolver('message', async (arg, _, message) => {
 			const channelID = await message.guild!.settings.get(GuildSettings.Suggestions.SuggestionsChannel)!;
 			if (!channelID) throw `You haven't setup a suggestions channel yet`;
-			const suggestionChannel = await this.client.channels.fetch(channelID);
-
+			const suggestionChannel = await this.client.channels.fetch(channelID) as TextChannel;
+			const suggestionMessage = await Serializer.regex.snowflake.test(arg) ? await suggestionChannel.messages.fetch(arg).catch(() => null) : undefined;
+			if (!suggestionMessage) throw 'Invalid message';
 			// Review(kyranet)
 			// This feels like adequately checking. Please review if something is iffy
-			if (suggestionMessage.channel.id !== suggestionChannel.id || suggestionMessage.author.id !== this.client.user!.id || suggestionMessage.embeds.length === 0) throw `This isn't a valid suggestion!`;
+			if (suggestionMessage.author.id !== this.client.user!.id || suggestionMessage.embeds.length === 0) throw `This isn't a valid suggestion!`;
 			return suggestionMessage;
 		});
 	}
 
-	public async run(message: KlasaMessage, [action, suggestionMessage, comment = 'No comment was provided']: [string, KlasaMessage, string]) {
-		const [shouldDM, shouldHideAuthor, shouldRepostSuggestion] = message.guild!.settings.pluck(GuildSettings.Suggestions.OnAction.DM, GuildSettings.Suggestions.OnAction.HideAuthor, GuildSettings.Suggestions.OnAction.RepostMessage) as boolean[];
+	public async run(message: KlasaMessage, [suggestionMessage, action, comment]: [KlasaMessage, string, string | undefined]) {
 
+		const [shouldDM, shouldHideAuthor, shouldRepostSuggestion] = message.guild!.settings.pluck(GuildSettings.Suggestions.OnAction.DM, GuildSettings.Suggestions.OnAction.HideAuthor, GuildSettings.Suggestions.OnAction.RepostMessage) as boolean[];
 		const suggestion = suggestionMessage.embeds[0];
+
 		let newEmbed: MessageEmbed | null = null;
 		let messageContent = null;
+		if (typeof comment === 'undefined') comment = 'No comment was provided.';
 
 		const author = this.getAuthor(message, shouldHideAuthor);
 		switch (action) {
@@ -78,8 +79,8 @@ export default class extends SkyraCommand {
 		}
 
 		if (shouldDM) {
-			const [, userID] = IdRegex.exec(suggestion.title) as string[];
-			const user = this.client.users.get(userID);
+			const matches = IdRegex.exec(suggestion.author!.name!);
+			const user = this.client.users.get(matches![0]);
 			if (!user) return;
 			try {
 				await user!.send(`${messageContent} in ${message.guild!.name}`, { embed: newEmbed! });

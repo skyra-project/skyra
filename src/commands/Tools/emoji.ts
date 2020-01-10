b995463ea35eb3ea9b7ee648f6c94e63f1b3e1ad
Antonio Román <kyradiscord@gmail.com>
2020-01-11 00:21:51 +0100

    feat: Added emote as alias for emoji, and emojis as alias for emotes

--- src/commands/Tools/emoji.ts
+++ src/commands/Tools/emoji.ts
@@ -10,6 +10,7 @@ export default class extends SkyraCommand {
 
 	public constructor(store: CommandStore, file: string[], directory: string) {
 		super(store, file, directory, {
+			aliases: ['emote'],
 			cooldown: 10,
 			description: language => language.tget('COMMAND_EMOJI_DESCRIPTION'),
 			extendedHelp: language => language.tget('COMMAND_EMOJI_EXTENDED'),

31a3d9b3d724b56b808b7852f4b97ef3a809aa70
Antonio Román <kyradiscord@gmail.com>
2020-01-23 19:52:30 +0100

    feat: Add more fields, changed some datatypes

--- src/lib/types/settings/raw/RawGameSettings.ts
+++ src/lib/types/settings/raw/RawGameSettings.ts
@@ -1,15 +1,15 @@
 export interface RawRpgItem {
 	id: number;
 	name: string;
-	maximum_durability: string;
+	maximum_durability: number;
+	maximum_cooldown: number;
 	attack: number;
 	defense: number;
 	health: number;
 	required_energy: number;
-	maximum_cooldown: number;
-	rarity: string;
+	rarity: number;
 	accuracy: number;
-	effects: string[];
+	effects: Record<string, number>;
 }
 
 export interface RawRpgClass {
@@ -17,6 +17,7 @@ export interface RawRpgClass {
 	name: string;
 	attack_multiplier: number;
 	defense_multiplier: number;
+	agility_multiplier: number;
 	energy_multiplier: number;
 	luck_multiplier: number;
 }
@@ -51,8 +52,10 @@ export interface RawRpgUser {
 	crate_uncommon_count: number;
 	crate_rare_count: number;
 	crate_legendary_count: number;
-	energy: string;
-	luck: string;
+	attack: number;
+	agility: number;
+	energy: number;
+	luck: number;
 }
 
 export interface RawRpgUserItem {
@@ -69,29 +72,27 @@ export interface RawRpgBattle {
 	challenger_cooldown: number;
 	challenger_health: number;
 	challenger_energy: number;
-	challenger_effects: string[];
+	challenger_effects: Record<string, number>;
 	challenged: string;
 	challenged_cooldown: number;
 	challenged_health: number;
 	challenged_energy: number;
-	challenged_effects: string[];
+	challenged_effects: Record<string, number>;
 }
 
 export const SQL_TABLE_SCHEMA = /* sql */`
-	BEGIN;
-
 	CREATE TABLE IF NOT EXISTS rpg_items (
 		"id"                 SERIAL,
-		"name"               VARCHAR(50)                            NOT NULL,
-		"maximum_durability" BIGINT                                 NOT NULL,
-		"attack"             FLOAT       DEFAULT 0.0                NOT NULL,
-		"defense"            FLOAT       DEFAULT 0.0                NOT NULL,
-		"health"             FLOAT       DEFAULT 0.0                NOT NULL,
-		"required_energy"    FLOAT       DEFAULT 0.0                NOT NULL,
-		"maximum_cooldown"   SMALLINT    DEFAULT 0                  NOT NULL,
-		"rarity"             BIGINT                                 NOT NULL,
-		"accuracy"           SMALLINT    DEFAULT 100                NOT NULL,
-		"effects"            VARCHAR(50) DEFAULT ARRAY[]::VARCHAR[] NOT NULL,
+		"name"               VARCHAR(50)                     NOT NULL,
+		"maximum_durability" INTEGER                         NOT NULL,
+		"maximum_cooldown"   SMALLINT    DEFAULT 0           NOT NULL,
+		"attack"             FLOAT       DEFAULT 0.0         NOT NULL,
+		"defense"            FLOAT       DEFAULT 0.0         NOT NULL,
+		"health"             FLOAT       DEFAULT 0.0         NOT NULL,
+		"required_energy"    FLOAT       DEFAULT 0.0         NOT NULL,
+		"rarity"             INTEGER                         NOT NULL,
+		"accuracy"           SMALLINT    DEFAULT 100         NOT NULL,
+		"effects"            JSONB       DEFAULT '{}'::JSONB NOT NULL,
 		UNIQUE ("name", "rarity"),
 		PRIMARY KEY ("id"),
 		CHECK ("name"               <> ''),
@@ -111,6 +112,7 @@ export const SQL_TABLE_SCHEMA = /* sql */`
 		"name"               VARCHAR(20)             NOT NULL,
 		"attack_multiplier"  FLOAT       DEFAULT 1.0 NOT NULL,
 		"defense_multiplier" FLOAT       DEFAULT 1.0 NOT NULL,
+		"agility_multiplier" FLOAT       DEFAULT 1.0 NOT NULL,
 		"energy_multiplier"  FLOAT       DEFAULT 1.0 NOT NULL,
 		"luck_multiplier"    FLOAT       DEFAULT 1.0 NOT NULL,
 		UNIQUE ("name"),
@@ -118,6 +120,7 @@ export const SQL_TABLE_SCHEMA = /* sql */`
 		CHECK ("name"               <> ''),
 		CHECK ("attack_multiplier"  >= 0),
 		CHECK ("defense_multiplier" >= 0),
+		CHECK ("agility_multiplier" >= 0),
 		CHECK ("energy_multiplier"  >= 0),
 		CHECK ("luck_multiplier"    >= 0)
 	);
@@ -152,19 +155,21 @@ export const SQL_TABLE_SCHEMA = /* sql */`
 	);
 
 	CREATE TABLE IF NOT EXISTS rpg_users (
-		"id"                    VARCHAR(19)             NOT NULL,
-		"name"                  VARCHAR(32)             NOT NULL,
-		"win_count"             BIGINT      DEFAULT 0   NOT NULL,
-		"death_count"           BIGINT      DEFAULT 0   NOT NULL,
+		"id"                    VARCHAR(19)           NOT NULL,
+		"name"                  VARCHAR(32)           NOT NULL,
+		"win_count"             BIGINT      DEFAULT 0 NOT NULL,
+		"death_count"           BIGINT      DEFAULT 0 NOT NULL,
 		"guild_id"              INTEGER,
 		"guild_rank_id"         INTEGER,
 		"class_id"              INTEGER,
-		"crate_common_count"    INTEGER     DEFAULT 0   NOT NULL,
-		"crate_uncommon_count"  INTEGER     DEFAULT 0   NOT NULL,
-		"crate_rare_count"      INTEGER     DEFAULT 0   NOT NULL,
-		"crate_legendary_count" INTEGER     DEFAULT 0   NOT NULL,
-		"energy"                BIGINT      DEFAULT 0   NOT NULL,
-		"luck"                  NUMERIC     DEFAULT 1.0 NOT NULL,
+		"crate_common_count"    INTEGER     DEFAULT 0 NOT NULL,
+		"crate_uncommon_count"  INTEGER     DEFAULT 0 NOT NULL,
+		"crate_rare_count"      INTEGER     DEFAULT 0 NOT NULL,
+		"crate_legendary_count" INTEGER     DEFAULT 0 NOT NULL,
+		"attack"                INTEGER     DEFAULT 0 NOT NULL,
+		"agility"               INTEGER     DEFAULT 0 NOT NULL,
+		"energy"                INTEGER     DEFAULT 0 NOT NULL,
+		"luck"                  INTEGER     DEFAULT 1 NOT NULL,
 		UNIQUE ("id"),
 		FOREIGN KEY ("id")            REFERENCES users          ("id") ON DELETE CASCADE,
 		FOREIGN KEY ("guild_id")      REFERENCES rpg_guilds     ("id") ON DELETE SET NULL,
@@ -176,6 +181,8 @@ export const SQL_TABLE_SCHEMA = /* sql */`
 		CHECK ("crate_uncommon_count"  >= 0),
 		CHECK ("crate_rare_count"      >= 0),
 		CHECK ("crate_legendary_count" >= 0),
+		CHECK ("attack"                >= 1),
+		CHECK ("agility"               >= 1),
 		CHECK ("energy"                >= 0),
 		CHECK ("luck"                  >= 0)
 	);
@@ -204,17 +211,17 @@ export const SQL_TABLE_SCHEMA = /* sql */`
 	CREATE TABLE IF NOT EXISTS rpg_battles
 	(
 		"id"                  BIGSERIAL,
-		"challenger_turn"     BOOLEAN                                NOT NULL,
-		"challenger"          VARCHAR(19)                            NOT NULL,
-		"challenger_cooldown" SMALLINT    DEFAULT 0                  NOT NULL,
-		"challenger_health"   INTEGER                                NOT NULL,
-		"challenger_energy"   INTEGER                                NOT NULL,
-		"challenger_effects"  VARCHAR(50) DEFAULT ARRAY[]::VARCHAR[] NOT NULL,
-		"challenged"          VARCHAR(19)                            NOT NULL,
-		"challenged_cooldown" SMALLINT    DEFAULT 0                  NOT NULL,
-		"challenged_health"   INTEGER                                NOT NULL,
-		"challenged_energy"   INTEGER                                NOT NULL,
-		"challenged_effects"  VARCHAR(50) DEFAULT ARRAY[]::VARCHAR[] NOT NULL,
+		"challenger_turn"     BOOLEAN                         NOT NULL,
+		"challenger"          VARCHAR(19)                     NOT NULL,
+		"challenger_cooldown" SMALLINT    DEFAULT 0           NOT NULL,
+		"challenger_health"   INTEGER                         NOT NULL,
+		"challenger_energy"   INTEGER                         NOT NULL,
+		"challenger_effects"  JSONB       DEFAULT '{}'::JSONB NOT NULL,
+		"challenged"          VARCHAR(19)                     NOT NULL,
+		"challenged_cooldown" SMALLINT    DEFAULT 0           NOT NULL,
+		"challenged_health"   INTEGER                         NOT NULL,
+		"challenged_energy"   INTEGER                         NOT NULL,
+		"challenged_effects"  JSONB       DEFAULT '{}'::JSONB NOT NULL,
 		UNIQUE ("challenger"),
 		UNIQUE ("challenged"),
 		PRIMARY KEY ("id"),
@@ -234,6 +241,4 @@ export const SQL_TABLE_SCHEMA = /* sql */`
 		DO INSTEAD
 			DELETE FROM rpg_battles
 			WHERE id = OLD.id;
-
-	COMMIT;
 `;

112782a169b56752375ac5b2531f7565899a0aef
Jeroen Claassens <support@favware.tech>
2020-02-09 19:43:58 +0100

    test: cleanup i18n test

--- __tests__/i18n.test.ts
+++ __tests__/i18n.test.ts
@@ -30,69 +30,75 @@ describe('i18n tests', () => {
 	const english = new English(englishLanguageStore, ['en-US.ts'], '');
 	const spanish = new Spanish(spanishLanguageStore, ['es-ES.ts'], '');
 
-	test('GIVEN language files THEN should have identical language keys', () => {
-		const englishTuple = objectToTuples(english.language);
-		const spanishTuple = objectToTuples(spanish.language);
+	describe('Tuple based comparison testing', () => {
+		test('GIVEN language files THEN should have identical language keys', () => {
+			const englishTuple = objectToTuples(english.language);
+			const spanishTuple = objectToTuples(spanish.language);
 
-		expect(englishTuple).toStrictEqual(spanishTuple);
+			expect(spanishTuple).toStrictEqual(englishTuple);
+		});
+
+		test('GIVEN language files THEN should have identical permission nodes', () => {
+			const englishTuple = objectToTuples(english.PERMISSIONS);
+			const spanishTuple = objectToTuples(spanish.PERMISSIONS);
+
+			expect(spanishTuple).toStrictEqual(englishTuple);
+		});
 	});
 
-	test('GIVEN language files THEN should have identical permission nodes', () => {
-		const englishTuple = objectToTuples(english.PERMISSIONS);
-		const spanishTuple = objectToTuples(spanish.PERMISSIONS);
+	describe('English language testing', () => {
+		test('GIVEN English Language THEN should parse all ordinals', () => {
+			expect(english.ordinal(0)).toBe('0th');
+			expect(english.ordinal(1)).toBe('1st');
+			expect(english.ordinal(2)).toBe('2nd');
+			expect(english.ordinal(3)).toBe('3rd');
+			expect(english.ordinal(4)).toBe('4th');
+			expect(english.ordinal(5)).toBe('5th');
+			expect(english.ordinal(10)).toBe('10th');
+		});
 
-		expect(englishTuple).toStrictEqual(spanishTuple);
+		test('GIVEN English language THEN should have noop init', async () => {
+			const spy = jest.spyOn(english, 'init');
+
+			await english.init();
+
+			expect(spy).toHaveBeenCalledTimes(1);
+			expect(spy).toHaveBeenCalledWith();
+
+			// eslint-disable-next-line @typescript-eslint/no-floating-promises
+			expect(english.init()).resolves.toBe(undefined);
+
+			expect(spy).toHaveBeenCalledTimes(2);
+		});
 	});
 
-	test('GIVEN English Language THEN should parse all ordinals', () => {
-		expect(english.ordinal(0)).toBe('0th');
-		expect(english.ordinal(1)).toBe('1st');
-		expect(english.ordinal(2)).toBe('2nd');
-		expect(english.ordinal(3)).toBe('3rd');
-		expect(english.ordinal(4)).toBe('4th');
-		expect(english.ordinal(5)).toBe('5th');
-		expect(english.ordinal(10)).toBe('10th');
-	});
+	describe('Spanish language testing', () => {
+		test('GIVEN Spanish Language THEN should parse all ordinals', () => {
+			expect(spanish.ordinal(0)).toBe('0mo');
+			expect(spanish.ordinal(1)).toBe('1ro');
+			expect(spanish.ordinal(2)).toBe('2do');
+			expect(spanish.ordinal(3)).toBe('3ro');
+			expect(spanish.ordinal(4)).toBe('4to');
+			expect(spanish.ordinal(5)).toBe('5to');
+			expect(spanish.ordinal(6)).toBe('6to');
+			expect(spanish.ordinal(7)).toBe('7mo');
+			expect(spanish.ordinal(8)).toBe('8vo');
+			expect(spanish.ordinal(9)).toBe('9no');
+			expect(spanish.ordinal(10)).toBe('10mo');
+		});
 
-	test('GIVEN Spanish Language THEN should parse all ordinals', () => {
-		expect(spanish.ordinal(0)).toBe('0mo');
-		expect(spanish.ordinal(1)).toBe('1ro');
-		expect(spanish.ordinal(2)).toBe('2do');
-		expect(spanish.ordinal(3)).toBe('3ro');
-		expect(spanish.ordinal(4)).toBe('4to');
-		expect(spanish.ordinal(5)).toBe('5to');
-		expect(spanish.ordinal(6)).toBe('6to');
-		expect(spanish.ordinal(7)).toBe('7mo');
-		expect(spanish.ordinal(8)).toBe('8vo');
-		expect(spanish.ordinal(9)).toBe('9no');
-		expect(spanish.ordinal(10)).toBe('10mo');
-	});
+		test('GIVEN Spanish language THEN should have noop init', async () => {
+			const spy = jest.spyOn(spanish, 'init');
 
-	test('GIVEN English language THEN should have noop init', async () => {
-		const spy = jest.spyOn(english, 'init');
+			await spanish.init();
 
-		await english.init();
+			expect(spy).toHaveBeenCalledTimes(1);
+			expect(spy).toHaveBeenCalledWith();
 
-		expect(spy).toHaveBeenCalledTimes(1);
-		expect(spy).toHaveBeenCalledWith();
+			// eslint-disable-next-line @typescript-eslint/no-floating-promises
+			expect(spanish.init()).resolves.toBe(undefined);
 
-		// eslint-disable-next-line @typescript-eslint/no-floating-promises
-		expect(english.init()).resolves.toBe(undefined);
-
-		expect(spy).toHaveBeenCalledTimes(2);
-	});
-
-	test('GIVEN Spanish language THEN should have noop init', async () => {
-		const spy = jest.spyOn(spanish, 'init');
-
-		await spanish.init();
-
-		expect(spy).toHaveBeenCalledTimes(1);
-		expect(spy).toHaveBeenCalledWith();
-
-		// eslint-disable-next-line @typescript-eslint/no-floating-promises
-		expect(spanish.init()).resolves.toBe(undefined);
-
-		expect(spy).toHaveBeenCalledTimes(2);
+			expect(spy).toHaveBeenCalledTimes(2);
+		});
 	});
 });

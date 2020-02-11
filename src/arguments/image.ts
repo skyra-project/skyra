141911123ce37e8f62cd27c927521eb4a8f47bc4
gc <gc@users.noreply.github.com>
2020-02-12 10:15:04 +1100

    I did a tweak in the image arg

--- src/arguments/image.ts
+++ src/arguments/image.ts
@@ -7,8 +7,11 @@ export default class extends Argument {
 	public async run(arg: string, possible: Possible, message: KlasaMessage) {
 		// If theres nothing provided, search the channel for an image.
 		if (!arg) {
+			// Configurable minimum of messages
+			const minimum = possible.min || 20;
+
 			// Fetch messages if there are not enough in the channel
-			if (message.channel.messages.size < 20) await message.channel.messages.fetch({ limit: 20 });
+			if (message.channel.messages.size < minimum) await message.channel.messages.fetch({ limit: minimum });
 
 			const messages = [...message.channel.messages.values()];
 			for (let i = messages.length - 1; i >= 0; --i) {

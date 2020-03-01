34bb2f1f47e9fb0fe5c6e89296e7afac9736053e
Jeroen Claassens <support@favware.tech>
2020-03-01 09:05:41 +0100

    refactor: add 500K option to shiny bet options
    Signed-off-by: Jeroen Claassens <support@favware.tech>

--- src/commands/Games/slotmachine.ts
+++ src/commands/Games/slotmachine.ts
@@ -15,7 +15,7 @@ export default class extends SkyraCommand {
 			extendedHelp: language => language.tget('COMMAND_SLOTMACHINE_EXTENDED'),
 			requiredPermissions: ['ATTACH_FILES'],
 			runIn: ['text'],
-			usage: '<50|100|200|500|1000|2000|5000|10000|20000|25000|50000|100000>'
+			usage: '<50|100|200|500|1000|2000|5000|10000|20000|25000|50000|100000|500000>'
 		});
 	}
 

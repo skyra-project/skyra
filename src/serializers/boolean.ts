a576d0dc0800dab705ce737bc7d1d6f3a76c938c
Jeroen Claassens <support@favware.tech>
2020-03-30 01:18:03 +0200

    fix: Allow `enable[d]` and `disable[d]` in conf command (#898)
    * fix: allow all boolean-like values for conf
    
    * ci: install dependencies with frozen-lockfile for extra safety
    
    * big woops fixed
    
    * Revert "big woops fixed"
    
    This reverts commit 95e6cd7533a3ed28b94ce79658ece1930f1b202c.
    
    * Revert "fix: allow all boolean-like values for conf"
    
    This reverts commit 1af35bfb1453cb663b9ff98769e6ed94dee5544f.
    
    * better mode
    
    * class properties

---
+++ src/serializers/boolean.ts
@@ -0,0 +1,24 @@
+import { ApplyOptions } from '@skyra/decorators';
+import { Serializer, SerializerOptions, SerializerUpdateContext } from 'klasa';
+
+
+@ApplyOptions<SerializerOptions>({
+	aliases: ['bool']
+})
+export default class extends Serializer {
+
+	private kTruths = new Set(['true', 't', 'yes', 'y', 'on', 'enable', 'enabled', '1', '+']);
+	private kFalses = new Set(['false', 'f', 'no', 'n', 'off', 'disable', 'disabled', '0', '-']);
+
+	public validate(data: string, { entry, language }: SerializerUpdateContext) {
+		const boolean = String(data).toLowerCase();
+		if (this.kTruths.has(boolean)) return true;
+		if (this.kFalses.has(boolean)) return false;
+		throw language.tget('RESOLVER_INVALID_BOOL', entry.key);
+	}
+
+	public stringify(data: string) {
+		return data ? 'Enabled' : 'Disabled';
+	}
+
+}

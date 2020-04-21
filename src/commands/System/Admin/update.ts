5f361535461ec77e97ec6658d44f58e576024cfa
Jeroen Claassens <support@favware.tech>
2020-04-21 20:59:59 +0200

    fix(update): dont error on yarn warnings (#928)
    * Revert "hotfix(s!update): dont error on yarn warnings"
    
    This reverts commit da328954615bc14e6fea3939a8931484c878d09a.
    
    * fix(update): dont error on yarn warnings
    
    * better
    
    * use applyoptions

--- src/commands/System/Admin/update.ts
+++ src/commands/System/Admin/update.ts
@@ -1,20 +1,19 @@
-import { SkyraCommand } from '@lib/structures/SkyraCommand';
+import { codeBlock, exec, sleep } from '@klasa/utils';
+import { SkyraCommand, SkyraCommandOptions } from '@lib/structures/SkyraCommand';
 import { PermissionLevels } from '@lib/types/Enums';
+import { ApplyOptions } from '@skyra/decorators';
 import { Emojis } from '@utils/constants';
 import { cutText } from '@utils/util';
-import { CommandStore, KlasaMessage, util } from 'klasa';
+import { KlasaMessage } from 'klasa';
 
+@ApplyOptions<SkyraCommandOptions>({
+	aliases: ['pull'],
+	description: 'Update the bot',
+	guarded: true,
+	permissionLevel: PermissionLevels.BotOwner,
+	usage: '[branch:string]'
+})
 export default class extends SkyraCommand {
-
-	public constructor(store: CommandStore, file: string[], directory: string) {
-		super(store, file, directory, {
-			aliases: ['pull'],
-			description: 'Update the bot',
-			guarded: true,
-			permissionLevel: PermissionLevels.BotOwner,
-			usage: '[branch:string]'
-		});
-	}
 
 	public async run(message: KlasaMessage, [branch = 'master']: [string?]) {
 		await this.fetch(message, branch);
@@ -23,21 +22,20 @@ export default class extends SkyraCommand {
 	}
 
 	private async compile(message: KlasaMessage) {
-		await util.exec('yarn build')
-			.catch(error => ({ stdout: '', stderr: error?.message || error || '' }));
+		const { stderr, code } = await this.exec('yarn build');
+		if (code !== 0 && stderr.length) throw stderr.trim();
 		return message.channel.send(`${Emojis.GreenTick} Successfully compiled.`);
 	}
 
 	private async updateDependencies(message: KlasaMessage) {
-		const { stderr } = await util.exec('yarn install --frozen-lockfile')
-			.catch(error => ({ stdout: '', stderr: error?.message || error || '' }));
-		if (stderr.length) throw stderr.trim();
+		const { stderr, code } = await this.exec('yarn install --frozen-lockfile');
+		if (code !== 0 && stderr.length) throw stderr.trim();
 		return message.channel.send(`${Emojis.GreenTick} Successfully updated dependencies.`);
 	}
 
 	private async fetch(message: KlasaMessage, branch: string) {
-		await util.exec('git fetch');
-		const { stdout, stderr } = await util.exec(`git pull origin ${branch}`);
+		await this.exec('git fetch');
+		const { stdout, stderr } = await this.exec(`git pull origin ${branch}`);
 
 		// If it's up to date, do nothing
 		if (/already up(?: |-)to(?: |-)date/i.test(stdout)) throw `${Emojis.GreenTick} Up to date.`;
@@ -54,28 +52,28 @@ export default class extends SkyraCommand {
 		}
 
 		// For all other cases, return the original output
-		return message.send(util.codeBlock('prolog', [cutText(stdout, 1950) || Emojis.GreenTick, stderr || Emojis.GreenTick].join('\n-=-=-=-\n')));
+		return message.send(codeBlock('prolog', [cutText(stdout, 1950) || Emojis.GreenTick, stderr || Emojis.GreenTick].join('\n-=-=-=-\n')));
 	}
 
 	private async stash(message: KlasaMessage) {
 		await message.send('Unsuccessful pull, stashing...');
-		await util.sleep(1000);
-		const { stdout, stderr } = await util.exec(`git stash`);
+		await sleep(1000);
+		const { stdout, stderr } = await this.exec(`git stash`);
 		if (!this.isSuccessfulStash(stdout + stderr)) {
-			throw `Unsuccessful pull, stashing:\n\n${util.codeBlock('prolog', [stdout || '✔', stderr || '✔'].join('\n-=-=-=-\n'))}`;
+			throw `Unsuccessful pull, stashing:\n\n${codeBlock('prolog', [stdout || '✔', stderr || '✔'].join('\n-=-=-=-\n'))}`;
 		}
 
-		return message.send(util.codeBlock('prolog', [cutText(stdout, 1950) || '✔', stderr || '✔'].join('\n-=-=-=-\n')));
+		return message.send(codeBlock('prolog', [cutText(stdout, 1950) || '✔', stderr || '✔'].join('\n-=-=-=-\n')));
 	}
 
 	private async checkout(message: KlasaMessage, branch: string) {
 		await message.send(`Switching to ${branch}...`);
-		await util.exec(`git checkout ${branch}`);
+		await this.exec(`git checkout ${branch}`);
 		return message.send(`${Emojis.GreenTick} Switched to ${branch}.`);
 	}
 
 	private async isCurrentBranch(branch: string) {
-		const { stdout } = await util.exec('git symbolic-ref --short HEAD');
+		const { stdout } = await this.exec('git symbolic-ref --short HEAD');
 		return stdout === `refs/heads/${branch}\n` || stdout === `${branch}\n`;
 	}
 
@@ -89,6 +87,15 @@ export default class extends SkyraCommand {
 
 	private needsStash(output: string) {
 		return output.includes('Your local changes to the following files would be overwritten by merge');
+	}
+
+	private async exec(script: string) {
+		try {
+			const result = await exec(script);
+			return { ...result, code: 0 };
+		} catch (error) {
+			return { stdout: '', stderr: error?.message || error || '', code: error.code ?? 1 };
+		}
 	}
 
 }

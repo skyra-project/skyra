85e20bf21b96b5105cf7cacf009189ad162fe869
Jeroen Claassens <support@favware.tech>
2020-04-25 21:25:25 +0200

    use footerSuffix

--- src/commands/Tools/Dictionary/urban.ts
+++ src/commands/Tools/Dictionary/urban.ts
@@ -45,7 +45,8 @@ export default class extends SkyraCommand {
 		const display = new UserRichDisplay(new MessageEmbed()
 			.setTitle(`Urban Dictionary: ${util.toTitleCase(query)}`)
 			.setColor(getColor(message))
-			.setThumbnail('https://i.imgur.com/CcIZZsa.png'));
+			.setThumbnail('https://i.imgur.com/CcIZZsa.png'))
+			.setFooterSuffix('© Urban Dictionary');
 
 		for (const result of results) {
 			const definition = this.content(result.definition, result.permalink, message.language);

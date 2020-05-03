848cffd671c3b0bead2e52cf7c1b65f6e690e6c3
Jeroen Claassens <support@favware.tech>
2020-05-03 14:03:55 +0200

    Update src/commands/Social/remindme.ts

--- src/commands/Social/remindme.ts
+++ src/commands/Social/remindme.ts
@@ -73,8 +73,6 @@ export default class extends SkyraCommand {
 			if (!arg) return Actions.List;
 
 			switch (arg.toLowerCase()) {
-				case 's':
-				case 'show':
 				case 'a':
 				case 'all':
 				case 'l':

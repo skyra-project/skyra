4d1e73ae1093934c0b9eb3f66c6789a807a35089
Jeroen Claassens <support@favware.tech>
2020-05-07 00:01:17 +0200

    refactor(escaperope): send a gif and alias escape

--- src/commands/Fun/escaperope.ts
+++ src/commands/Fun/escaperope.ts
@@ -1,20 +1,21 @@
-import { SkyraCommand } from '@lib/structures/SkyraCommand';
-import { CommandStore, KlasaMessage } from 'klasa';
+import { SkyraCommand, SkyraCommandOptions } from '@lib/structures/SkyraCommand';
+import { ApplyOptions } from '@skyra/decorators';
+import { KlasaMessage } from 'klasa';
 
+@ApplyOptions<SkyraCommandOptions>({
+	aliases: ['escape'],
+	bucket: 2,
+	cooldown: 60,
+	description: language => language.tget('COMMAND_ESCAPEROPE_DESCRIPTION'),
+	extendedHelp: language => language.tget('COMMAND_ESCAPEROPE_EXTENDED')
+})
 export default class extends SkyraCommand {
 
-	public constructor(store: CommandStore, file: string[], directory: string) {
-		super(store, file, directory, {
-			bucket: 2,
-			cooldown: 60,
-			description: language => language.tget('COMMAND_ESCAPEROPE_DESCRIPTION'),
-			extendedHelp: language => language.tget('COMMAND_ESCAPEROPE_EXTENDED')
-		});
-	}
+	private readonly kEscapeGif = 'https://cdn.skyra.pw/img/pokemon/escape_rope.gif';
 
 	public async run(message: KlasaMessage) {
 		if (message.deletable) await message.nuke().catch(() => null);
-		return message.sendLocale('COMMAND_ESCAPEROPE_OUTPUT', [message.author]);
+		return message.sendLocale('COMMAND_ESCAPEROPE_OUTPUT', [message.author, this.kEscapeGif]);
 	}
 
 }

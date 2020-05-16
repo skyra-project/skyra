6966974b2d47386fd03b16f45ac9d6bb4323abd3
Jeroen Claassens <support@favware.tech>
2020-05-17 00:05:21 +0200

    Apply suggestions from code review
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>

--- src/monitors/preCommandHandler.ts
+++ src/monitors/preCommandHandler.ts
@@ -10,10 +10,10 @@ import { Colors, KlasaMessage, Monitor, MonitorOptions } from 'klasa';
 })
 export default class extends Monitor {
 
-	private kDebugColours = new Colors({ background: 'black', text: 'red' });
-	private kUserColours = new Colors({ background: 'yellow', text: 'black' });
-	private kShardColours = new Colors({ background: 'cyan', text: 'black' });
-	private kChannelColours: Record<string, Colors> = {
+	private readonly kDebugColours = new Colors({ background: 'black', text: 'red' });
+	private readonly kUserColours = new Colors({ background: 'yellow', text: 'black' });
+	private readonly kShardColours = new Colors({ background: 'cyan', text: 'black' });
+	private readonly kChannelColours: Record<string, Colors> = {
 		text: new Colors({ background: 'green', text: 'black' }),
 		dm: new Colors({ background: 'magenta' })
 	};
@@ -29,7 +29,7 @@ export default class extends Monitor {
 			this.kShardColours.format(`[${shard}]`),
 			`${message.command.name}(${message.args ? message.args.join(', ') : ''})`,
 			this.kUserColours.format(`${message.author.username}[${message.author.id}]`),
-			this.kChannelColours[type].format(type === 'text' ? this.text(message) : this.dm())
+			this.kChannelColours[type].format(type === 'dm' ? this.dm() : this.text(message))
 		].join(' '));
 	}
 

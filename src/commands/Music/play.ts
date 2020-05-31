e46373b4cf0b4c7d6b41fd736bfd88f23255630d
Antonio Román <kyradiscord@gmail.com>
2020-05-31 20:35:49 +0200

    fix(Play): add a 500ms delay before playing

--- src/commands/Music/play.ts
+++ src/commands/Music/play.ts
@@ -2,6 +2,7 @@ import { MusicCommand, MusicCommandOptions } from '@lib/structures/MusicCommand'
 import { ApplyOptions } from '@skyra/decorators';
 import { KlasaMessage } from 'klasa';
 import { Track } from 'lavalink';
+import { sleep } from '@klasa/utils';
 
 @ApplyOptions<MusicCommandOptions>({
 	description: language => language.tget('COMMAND_PLAY_DESCRIPTION'),
@@ -17,16 +18,23 @@ export default class extends MusicCommand {
 
 		if (songs) {
 			// If there are songs, add them
-			await this.client.commands.get('add')!.run(message, [songs]);
+			message.guild!.music.add(message.author.id, songs, this.getContext(message));
+
+			// If the music is already playing, return early to simulate add's behaviour
 			if (music.playing) return;
-		} else if (!music.canPlay) {
+		}
+
+		if (!music.canPlay) {
 			await message.sendLocale('COMMAND_QUEUE_EMPTY');
 			return;
 		}
 
-		// If Skyra is not in a voice channel, join
+		// If Skyra is not in a voice channel, join the member's
 		if (!music.voiceChannel) {
+			const { channel } = message.member!.voice;
+			if (!channel) throw message.language.tget('COMMAND_JOIN_NO_VOICECHANNEL');
 			await this.client.commands.get('join')!.run(message, []);
+			await sleep(500);
 		}
 
 		if (music.playing) {

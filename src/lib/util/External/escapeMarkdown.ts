f80edbf81f4c31d755689437d3668bdc5f147059
Kaoru <KunoichiZ@users.noreply.github.com>
2020-06-22 10:28:38 -0500

    fix(escapeMarkdown): remove unneeded lines

--- src/lib/util/External/escapeMarkdown.ts
+++ src/lib/util/External/escapeMarkdown.ts
@@ -108,14 +108,9 @@ function escapeBold(text: string) {
  */
 function escapeItalic(text: string) {
 	let i = 0;
-	text = text.replace(/(?<=^|[^*])\*([^*]|\*\*|$)/g, (_, match) => {
-		if (match === '**') return ++i % 2 ? `\\*${match}` : `${match}\\*`;
+	return text = text.replace(/(?<=^|[^*])\*([^*]|\*\*|$)/g, (_, match) => {
+		if (match === '**' || match === '__') return ++i % 2 ? `\\*${match}` : `${match}\\*`;
 		return `\\*${match}`;
-	});
-	i = 0;
-	return text.replace(/(?<=^|[^_])_([^_]|__|$)/g, (_, match) => {
-		if (match === '__') return ++i % 2 ? `\\_${match}` : `${match}\\_`;
-		return `\\_${match}`;
 	});
 }
 

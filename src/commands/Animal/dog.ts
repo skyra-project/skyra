fa5cf0f21887127a20b0c7bf534738344a02ef87
Jeroen Claassens <support@favware.tech>
2020-06-22 17:04:11 +0200

    fix(dog): ensure image is image url (#1056)

--- src/commands/Animal/dog.ts
+++ src/commands/Animal/dog.ts
@@ -1,20 +1,18 @@
-import { SkyraCommand } from '@lib/structures/SkyraCommand';
-import { fetch, FetchResultTypes, getColor } from '@utils/util';
+import { SkyraCommand, SkyraCommandOptions } from '@lib/structures/SkyraCommand';
+import { ApplyOptions } from '@skyra/decorators';
+import { fetch, FetchResultTypes, getColor, IMAGE_EXTENSION } from '@utils/util';
 import { MessageEmbed } from 'discord.js';
-import { CommandStore, KlasaMessage } from 'klasa';
+import { KlasaMessage } from 'klasa';
 
+@ApplyOptions<SkyraCommandOptions>({
+	aliases: ['doggo', 'puppy'],
+	cooldown: 10,
+	description: language => language.tget('COMMAND_DOG_DESCRIPTION'),
+	extendedHelp: language => language.tget('COMMAND_DOG_EXTENDED'),
+	requiredPermissions: ['EMBED_LINKS'],
+	spam: true
+})
 export default class extends SkyraCommand {
-
-	public constructor(store: CommandStore, file: string[], directory: string) {
-		super(store, file, directory, {
-			aliases: ['doggo', 'puppy'],
-			cooldown: 10,
-			description: language => language.tget('COMMAND_DOG_DESCRIPTION'),
-			extendedHelp: language => language.tget('COMMAND_DOG_EXTENDED'),
-			requiredPermissions: ['EMBED_LINKS'],
-			spam: true
-		});
-	}
 
 	public async run(message: KlasaMessage) {
 		const embed = new MessageEmbed()
@@ -23,7 +21,12 @@ export default class extends SkyraCommand {
 
 		try {
 			const randomDogData = await fetch<DogResultOk>('https://dog.ceo/api/breeds/image/random', FetchResultTypes.JSON);
-			if (randomDogData && randomDogData.status === 'success') embed.setImage(randomDogData.message);
+			if (randomDogData && randomDogData.status === 'success') {
+				// Just in case the image is not a valid image url then fallthrough to the catch
+				if (!IMAGE_EXTENSION.test(randomDogData.message)) throw '💥';
+
+				embed.setImage(randomDogData.message);
+			}
 		} catch {
 			embed.setImage('https://i.imgur.com/cF0XUF5.jpg');
 		}

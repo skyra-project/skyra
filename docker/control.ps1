68dfc19517c73fdae4e5ee1b78413bc50760702b
Jeroen Claassens <support@favware.tech>
2020-07-01 12:05:18 +0200

    refactor: move to docker-images
    This prepares the move from docker images in this repo to the docker-images repo

--- docker/control.ps1
+++ docker/control.ps1
@@ -26,29 +26,29 @@ tail		Tails the logs of a Docker container"
 
 function Step-Run {
 	Param (
-		[String]$command = $( Read-Host "What command do you want to run? If unsure type help" ),
+		[String]$Command = $( Read-Host "What command do you want to run? If unsure type help" ),
 		[string]$Service
 	)
 
 	Begin {
-		if ($command -Ne "help" -And $command -Ne "h" -And $command -Ne "removeall" -And $service -Eq "") {
-			$service = $( Read-Host "What Docker service do you want to control?" )
+		if ($Command -Ne "help" -And $Command -Ne "h" -And $Command -Ne "removeall" -And $Service -Eq "") {
+			$Service = $( Read-Host "What Docker service do you want to control?" )
 		}
 	}
 
 	Process {
-		switch ( $command ) {
-			build { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" -f "$($PSScriptRoot)\docker-build.yml" build $service }
-			start { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" up -d $service }
-			logs { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" logs $service }
-			tail { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" logs -f $service }
-			push { docker push $service }
-			remove { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" rm -fv $service }
-			update { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" pull $service; docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" up -d --force-recreate $service }
+		switch ($Command) {
+			build { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" -f "$($PSScriptRoot)\docker-build.yml" build $Service }
+			start { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" up -d $Service }
+			logs { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" logs $Service }
+			tail { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" logs -f $Service }
+			push { docker push $Service }
+			remove { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" rm -fv $Service }
+			update { docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" pull $Service; docker-compose -p skyra -f "$($PSScriptRoot)\docker-compose.yml" up -d --force-recreate $Service }
 			removeall { Remove-All-Containers }
 			default { Show-Help }
 		}
 	}
 }
 
-Step-Run
+Step-Run @Args

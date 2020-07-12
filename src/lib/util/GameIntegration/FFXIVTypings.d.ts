7eddc09284348b5228cbf326eaffe4637d841fea
QuantumlyTangled <ethoarx@gmail.com>
2020-07-12 16:54:43 +0200

    I think this is all of them

--- src/lib/util/GameIntegration/FFXIVTypings.d.ts
+++ src/lib/util/GameIntegration/FFXIVTypings.d.ts
@@ -327,7 +327,4 @@ export namespace FFXIV {
 		Name: string;
 		Points: number;
 	}
-
-	export type Servers = ['Adamantoise', 'Aegis', 'Alexander', 'Anima', 'Asura', 'Atomos', 'Bahamut', 'Balmung', 'Behemoth', 'Belias', 'Brynhildr', 'Cactuar', 'Carbuncle', 'Cerberus', 'Chocobo', 'Coeurl', 'Diabolos', 'Durandal', 'Excalibur', 'Exodus', 'Faerie', 'Famfrit', 'Fenrir', 'Garuda', 'Gilgamesh', 'Goblin', 'Gungnir', 'Hades', 'Hyperion', 'Ifrit', 'Ixion', 'Jenova', 'Kujata', 'Lamia', 'Leviathan', 'Lich', 'Louisoix', 'Malboro', 'Mandragora', 'Masamune', 'Mateus', 'Midgardsormr', 'Moogle', 'Odin', 'Omega', 'Pandaemonium', 'Phoenix', 'Ragnarok', 'Ramuh', 'Ridill', 'Sargatanas', 'Shinryu', 'Shiva', 'Siren', 'Tiamat', 'Titan', 'Tonberry', 'Typhon', 'Ultima', 'Ultros', 'Unicorn', 'Valefor', 'Yojimbo', 'Zalera', 'Zeromus', 'Zodiark', 'Spriggan', 'Twintania'];
-	export type Server = Servers[number];
 }

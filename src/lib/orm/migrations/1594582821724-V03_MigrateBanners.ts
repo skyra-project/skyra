8f1c04a0c5d866135e34eab9f5ceca842b3ccc0b
Antonio Román <kyradiscord@gmail.com>
2020-07-13 10:29:47 +0200

    refactor(orm): use renameTable instead of SQL for banners migration

--- src/lib/orm/migrations/1594582821724-V03_MigrateBanners.ts
+++ src/lib/orm/migrations/1594582821724-V03_MigrateBanners.ts
@@ -7,12 +7,11 @@ export class V03MigrateBanners1594582821724 implements MigrationInterface {
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(/* sql */`ALTER TABLE public.banner RENAME TO "banners"`);
+		await queryRunner.renameTable('banner', 'banners');
 	}
 
 	private async migrateBanners(queryRunner: QueryRunner): Promise<void> {
-		// Rename "banners" table to "banner"
-		await queryRunner.query(/* sql */`ALTER TABLE public.banners RENAME TO "banner"`);
+		await queryRunner.renameTable('banners', 'banner');
 	}
 
 }

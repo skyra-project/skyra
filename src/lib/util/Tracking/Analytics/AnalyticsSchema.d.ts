726d89e83cb46626db2d84d4f8e54e1cc12470e4
QuantumlyTangled <ethoarx@gmail.com>
2020-07-13 12:04:37 +0200

    Hey it works

---
+++ src/lib/util/Tracking/Analytics/AnalyticsSchema.d.ts
@@ -0,0 +1,15 @@
+export namespace AnalyticsSchema {
+
+	export const enum Tags {
+		Shard = 'shard',
+		Guild = 'guild_id',
+		Client = 'client_id',
+		Action = 'action'
+	}
+
+	export const enum Actions {
+		Addition = 'addition',
+		Subtraction = 'subtraction'
+	}
+
+}

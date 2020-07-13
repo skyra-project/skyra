6d8dfea69eaa66181338b5fe9459ce540ccd90c1
Jeroen Claassens <support@favware.tech>
2020-07-13 23:29:54 +0200

    fix clear old tables

--- src/lib/orm/migrations/1594669268323-V12_ClearOldTables.ts
+++ src/lib/orm/migrations/1594669268323-V12_ClearOldTables.ts
@@ -3,10 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class V12ClearOldTables1594669268323 implements MigrationInterface {
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(/* sql */`DROP TABLE dashboard_users CASCADE`);
-		await queryRunner.query(/* sql */`DROP TABLE clientStorage CASCADE`);
-		await queryRunner.query(/* sql */`DROP TABLE users CASCADE`);
-		await queryRunner.query(/* sql */`DROP TABLE twitch_stream_subscriptions CASCADE`);
+		await queryRunner.query(/* sql */`DROP TABLE public.dashboard_users CASCADE`);
+		await queryRunner.query(/* sql */`DROP TABLE public."clientStorage" CASCADE`);
+		await queryRunner.query(/* sql */`DROP TABLE public."users" CASCADE`);
+		await queryRunner.query(/* sql */`DROP TABLE public.twitch_stream_subscriptions CASCADE`);
 	}
 
 	public async down(): Promise<void> {

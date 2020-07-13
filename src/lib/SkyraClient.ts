0289a12cc303ace7793bc11d742d55ec36fc7031
Nejc Drobnic <7919610+QuantumlyTangled@users.noreply.github.com>
2020-07-13 18:10:20 +0200

    Update src/lib/SkyraClient.ts
    Co-authored-by: Jeroen Claassens <support@favware.tech>

--- src/lib/SkyraClient.ts
+++ src/lib/SkyraClient.ts
@@ -4,10 +4,10 @@ import { KlasaClient, KlasaClientOptions, Schema, util } from 'klasa';
 import { Colors } from '@klasa/console';
 import { Manager as LavalinkManager } from '@utils/Music/ManagerWrapper';
 import { Client as VezaClient } from 'veza';
+import { InfluxDB, WriteApi, WritePrecision } from '@influxdata/influxdb-client';
 import { Webhook } from 'discord.js';
 import { FSWatcher } from 'chokidar';
 import { DashboardClient } from 'klasa-dashboard-hooks';
-import { InfluxDB, WriteApi, WritePrecision } from '@influxdata/influxdb-client';
 
 // Import all types
 import { Events } from './types/Enums';

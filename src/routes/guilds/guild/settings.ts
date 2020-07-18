c5b248e9f3c867630b314bddf91a3936450ba310
Jeroen Claassens <support@favware.tech>
2020-07-18 20:46:17 +0200

    refactor: remove raw guild settings (#1114)
    Keeping the entire file (and its folder) just for the single use in the KDH route seems like a hgue waste to me especially because it wasn't accurate anyway as it didn't have the dedicated types that we have in the GuildEntity or GuildSettings files

--- src/routes/guilds/guild/settings.ts
+++ src/routes/guilds/guild/settings.ts
@@ -2,19 +2,16 @@ import { objectToTuples } from '@klasa/utils';
 import { ApiRequest } from '@lib/structures/api/ApiRequest';
 import { ApiResponse } from '@lib/structures/api/ApiResponse';
 import { Events } from '@lib/types/Enums';
-import { RawGuildSettings } from '@lib/types/settings/raw/RawGuildSettings';
 import { ApplyOptions } from '@skyra/decorators';
 import { canManage } from '@utils/API';
 import { authenticated, ratelimit } from '@utils/util';
 import { Route, RouteOptions } from 'klasa-dashboard-hooks';
 import { inspect } from 'util';
 
-type Keys = keyof RawGuildSettings;
-
 @ApplyOptions<RouteOptions>({ name: 'guildSettings', route: 'guilds/:guild/settings' })
 export default class extends Route {
 
-	private readonly kBlacklist: Keys[] = ['commandUses'];
+	private readonly kBlacklist: string[] = ['commandUses'];
 
 	@authenticated()
 	@ratelimit(2, 5000, true)
@@ -35,7 +32,7 @@ export default class extends Route {
 	@authenticated()
 	@ratelimit(2, 1000, true)
 	public async post(request: ApiRequest, response: ApiResponse) {
-		const requestBody = request.body as { guild_id: string; data: Record<Keys, unknown> | [Keys, unknown][] | undefined };
+		const requestBody = request.body as { guild_id: string; data: Record<string, unknown> | [string, unknown][] | undefined };
 
 		if (!requestBody.guild_id || !requestBody.data || requestBody.guild_id !== request.params.guild) {
 			return response.error(400);
@@ -49,7 +46,7 @@ export default class extends Route {
 
 		if (!canManage(botGuild, member)) return response.error(403);
 
-		const entries = Array.isArray(requestBody.data) ? requestBody.data : objectToTuples(requestBody.data) as [Keys, unknown][];
+		const entries = Array.isArray(requestBody.data) ? requestBody.data : objectToTuples(requestBody.data) as [string, unknown][];
 		if (entries.some(([key]) => this.kBlacklist.includes(key))) return response.error(400);
 
 		await botGuild.settings.sync();

679f2db91a1f1dbd4f1727fc6104613d2fcef2da
Jeroen Claassens <support@favware.tech>
2020-07-18 13:42:36 +0200

    fix: fixed mute & stickyrole interopability

--- src/serializers/stickyrole.ts
+++ src/serializers/stickyrole.ts
@@ -1,7 +1,7 @@
-import { Serializer, SerializerUpdateContext } from 'klasa';
-import { Guild } from 'discord.js';
-import { StickyRole } from '@lib/types/settings/GuildSettings';
 import { isObject } from '@klasa/utils';
+import { StickyRole } from '@lib/types/settings/GuildSettings';
+import { Guild } from 'discord.js';
+import { Serializer, SerializerUpdateContext } from 'klasa';
 
 export default class extends Serializer {
 
@@ -10,7 +10,7 @@ export default class extends Serializer {
 			&& Object.keys(data).length === 2
 			&& typeof data.user === 'string'
 			&& Array.isArray(data.roles)
-			&& data.roles.every(role => typeof role === 'string' && guild!.roles.has(role))) return data;
+			&& data.roles.every(role => typeof role === 'string')) return this.cleanupStickyRoles(data, guild!);
 
 		throw language.tget('SERIALIZER_STICKY_ROLE_INVALID');
 	}
@@ -19,6 +19,17 @@ export default class extends Serializer {
 		const username = this.client.userTags.get(value.user)?.username ?? guild.language.tget('UNKNOWN_USER');
 		const roles = value.roles.map(role => guild.roles.get(role)?.name ?? guild.language.tget('UNKNOWN_ROLE'));
 		return `[${username} -> ${roles}]`;
+	}
+
+	// TODO: Resolve this to a better solution (i.e. using `guild.roles.sticky.add`) (see https://discordapp.com/channels/541738403230777351/541740581832097792/734011404750684220)
+	private cleanupStickyRoles(data: StickyRole, guild: Guild): StickyRole {
+		const stickyRolesInGuild: string[] = [];
+		for (const role of [...new Set(data.roles)]) {
+			if (guild.roles.has(role)) stickyRolesInGuild.push(role);
+			continue;
+		}
+
+		return { user: data.user, roles: stickyRolesInGuild };
 	}
 
 }

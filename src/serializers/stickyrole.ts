e192e2a9ca690475de9b94c28f073b535f2367ef
Jeroen Claassens <support@favware.tech>
2020-07-18 13:44:55 +0200

    add comments

--- src/serializers/stickyrole.ts
+++ src/serializers/stickyrole.ts
@@ -23,12 +23,17 @@ export default class extends Serializer {
 
 	// TODO: Resolve this to a better solution (i.e. using `guild.roles.sticky.add`) (see https://discordapp.com/channels/541738403230777351/541740581832097792/734011404750684220)
 	private cleanupStickyRoles(data: StickyRole, guild: Guild): StickyRole {
+		// Sticky roles that should be saved back to the database
 		const stickyRolesInGuild: string[] = [];
+
+		// Deduplicate stickyroles by using a set
 		for (const role of [...new Set(data.roles)]) {
+			// If the guild has the specified role then we'll add it
 			if (guild.roles.has(role)) stickyRolesInGuild.push(role);
 			continue;
 		}
 
+		// Return the new StickyRole object
 		return { user: data.user, roles: stickyRolesInGuild };
 	}
 

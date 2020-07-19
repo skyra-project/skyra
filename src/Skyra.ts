b7d1dbb416cef125b7bf591a10f44e9f4e187ac2
QuantumlyTangled <ethoarx@gmail.com>
2020-07-19 21:55:22 +0200

    Initial

--- src/Skyra.ts
+++ src/Skyra.ts
@@ -4,6 +4,7 @@ import 'reflect-metadata';
 import { SkyraClient } from '@lib/SkyraClient';
 import { DbSet } from '@lib/structures/DbSet';
 import { TOKENS } from '@root/config';
+import * as sentry from '@sentry/node';
 import { floatPromise } from '@utils/util';
 import { inspect } from 'util';
 inspect.defaultOptions.depth = 1;
@@ -11,6 +12,7 @@ inspect.defaultOptions.depth = 1;
 const client = new SkyraClient();
 
 async function main() {
+	sentry.init({ dsn: TOKENS.SENTRY_URL });
 	await DbSet.connect();
 	await client.login(TOKENS.BOT_TOKEN);
 }
--- src/config.example.ts
+++ src/config.example.ts
@@ -185,5 +185,6 @@ export const TOKENS = {
 	XIVAPI_KEY: '',
 	FORTNITE_KEY: '',
 	WEBHOOK_B4D: '',
-	WEBHOOK_TOPGG: ''
+	WEBHOOK_TOPGG: '',
+	SENTRY_URL: ''
 };

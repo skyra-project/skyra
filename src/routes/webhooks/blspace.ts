9a6b1d0fe27c82f9b7ed461bed724d750e56cd12
Antonio Román <kyradiscord@gmail.com>
2020-07-22 17:40:06 +0200

    feat(bot-lists): added botlist.space integration (#1115)

---
+++ src/routes/webhooks/blspace.ts
@@ -0,0 +1,47 @@
+import { ApiRequest } from '@lib/structures/api/ApiRequest';
+import { ApiResponse } from '@lib/structures/api/ApiResponse';
+import { DbSet } from '@lib/structures/DbSet';
+import { TOKENS } from '@root/config';
+import { ApplyOptions } from '@skyra/decorators';
+import { Route, RouteOptions } from 'klasa-dashboard-hooks';
+
+@ApplyOptions<RouteOptions>({ name: 'webhooks/blspace', route: 'webhooks/blspace' })
+export default class extends Route {
+
+	public async post(request: ApiRequest, response: ApiResponse) {
+		if (request.headers.authorization !== TOKENS.BOTLIST_SPACE_KEY) return response.forbidden();
+		if (!request.body) return response.badRequest();
+
+		const body = request.body as Body;
+		try {
+			const { users } = await DbSet.connect();
+			await users.lock([body.user.id], async id => {
+				const user = await users.ensure(id);
+
+				user.money += 400;
+				await user.save();
+			});
+
+			return response.noContent();
+		} catch (error) {
+			this.client.emit('error', error);
+			return response.error(error.message ?? 'Unknown error');
+		}
+	}
+
+}
+
+export interface Body {
+	site: 'botlist.space';
+	bot: string;
+	user: BodyUser;
+	timestamp: number;
+}
+
+export interface BodyUser {
+	id: string;
+	username: string;
+	discriminator: string;
+	avatar: string | null;
+	short_description: string | null;
+}
--- src/tasks/poststats.ts
+++ src/tasks/poststats.ts
@@ -1,7 +1,7 @@
 import { Colors } from '@klasa/console';
 import { Events } from '@lib/types/Enums';
 import { PartialResponseValue, ResponseType } from '@orm/entities/ScheduleEntity';
-import { DEV, ENABLE_INFLUX, TOKENS } from '@root/config';
+import { DEV, ENABLE_INFLUX, TOKENS, CLIENT_ID } from '@root/config';
 import { Mime } from '@utils/constants';
 import { fetch, FetchResultTypes } from '@utils/util';
 import { Task } from 'klasa';
@@ -12,6 +12,7 @@ const b = new Colors({ text: 'lightblue' });
 const header = b.format('[POST STATS   ]');
 
 enum Lists {
+	BotListSpace = 'botlist.space',
 	BotsForDiscord = 'botsfordiscord.com',
 	DiscordBotList = 'discordbotlist.com',
 	TopGG = 'top.gg',
@@ -33,16 +34,18 @@ export default class extends Task {
 		const guilds = rawGuilds.toString();
 		const users = rawUsers.toString();
 		const results = (await Promise.all([
-			this.query(`https://top.gg/api/bots/${this.client.user!.id}/stats`,
+			this.query(`https://top.gg/api/bots/${CLIENT_ID}/stats`,
 				`{"server_count":${guilds}}`, TOKENS.TOP_GG, Lists.TopGG),
-			this.query(`https://discord.bots.gg/api/v1/bots/${this.client.user!.id}/stats`,
+			this.query(`https://discord.bots.gg/api/v1/bots/${CLIENT_ID}/stats`,
 				`{"guildCount":${guilds}}`, TOKENS.DISCORD_BOTS, Lists.DiscordBotsGG),
-			this.query(`https://botsfordiscord.com/api/bot/${this.client.user!.id}`,
+			this.query(`https://botsfordiscord.com/api/bot/${CLIENT_ID}`,
 				`{"server_count":${guilds}}`, TOKENS.BOTS_FOR_DISCORD_KEY, Lists.BotsForDiscord),
-			this.query(`https://discordbotlist.com/api/v1/bots/${this.client.user!.id}/stats`,
+			this.query(`https://discordbotlist.com/api/v1/bots/${CLIENT_ID}/stats`,
 				`{"guilds":${guilds},"users":${users}}`, TOKENS.DISCORD_BOT_LIST ? `Bot ${TOKENS.DISCORD_BOT_LIST}` : null, Lists.DiscordBotList),
-			this.query(`https://bots.ondiscord.xyz/bot-api/bots/${this.client.user!.id}/guilds`,
-				`{"guildCount":${guilds}}`, TOKENS.BOTS_ON_DISCORD_KEY, Lists.BotsOnDiscord)
+			this.query(`https://bots.ondiscord.xyz/bot-api/bots/${CLIENT_ID}/guilds`,
+				`{"guildCount":${guilds}}`, TOKENS.BOTS_ON_DISCORD_KEY, Lists.BotsOnDiscord),
+			this.query(`https://api.botlist.space/v1/bots/${CLIENT_ID}`,
+				`{"server_count":${guilds}}`, TOKENS.BOTLIST_SPACE_KEY, Lists.BotListSpace)
 		])).filter(value => value !== null);
 
 		if (results.length) this.client.emit(Events.Verbose, `${header} [ ${guilds} [G] ] [ ${users} [U] ] | ${results.join(' | ')}`);

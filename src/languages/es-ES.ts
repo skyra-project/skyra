736bb3ea49cf93624fccb405ac49ffb28fa7c897
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-07-24 16:28:01 +0530

    fix: unit tests

--- src/languages/es-ES.ts
+++ src/languages/es-ES.ts
@@ -2530,15 +2530,6 @@ export default class extends Language {
 		COMMAND_WIKIPEDIA_EXTENDED: builder.display('wikipedia', {}),
 		COMMAND_YOUTUBE_DESCRIPTION: 'Search something through YouTube.',
 		COMMAND_YOUTUBE_EXTENDED: builder.display('youtube', {}),
-		COMMAND_DEFINE_DESCRIPTION: 'Busca la definición de una palabra en inglés.',
-		COMMAND_DEFINE_EXTENDED: builder.display('define', {
-			extendedHelp: `¿Qué significa "heel"?`,
-			explainedUsage: [
-				['Word', 'La palabra o frase cuya definición quieres buscar.']
-			],
-			examples: ['heel']
-		}),
-		COMMAND_DEFINE_NOTFOUND: 'No pude encontrar la definición de esta palabra.',
 
 		/**
 		 * ################
@@ -3924,6 +3915,15 @@ export default class extends Language {
 		COMMAND_WIKIPEDIA_NOTFOUND: 'Lo siento, pero no he podido encontrar algo que coincida con el término que buscas a través de Wikipedia.',
 		COMMAND_YOUTUBE_NOTFOUND: 'Lo siento, pero no he podido encontrar algo que coincida con el término que buscas a través de YouTube.',
 		COMMAND_YOUTUBE_INDEX_NOTFOUND: 'Quizá quieras probar con un índice de página menor, porque no soy capaz de encontrar algo en éste.',
+		COMMAND_DEFINE_DESCRIPTION: 'Busca la definición de una palabra en inglés.',
+		COMMAND_DEFINE_EXTENDED: builder.display('define', {
+			extendedHelp: `¿Qué significa "heel"?`,
+			explainedUsage: [
+				['Word', 'La palabra o frase cuya definición quieres buscar.']
+			],
+			examples: ['heel']
+		}),
+		COMMAND_DEFINE_NOTFOUND: 'No pude encontrar la definición de esta palabra.',
 
 		/**
 		 * #############

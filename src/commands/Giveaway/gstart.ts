9afee04277d3fc3f48e98c5acdbc044ecd889363
Antonio Román <kyradiscord@gmail.com>
2020-07-25 16:58:27 +0200

    fix(gstart): throw on unmatching winners value

--- src/commands/Giveaway/gstart.ts
+++ src/commands/Giveaway/gstart.ts
@@ -1,11 +1,11 @@
 import { SkyraCommand, SkyraCommandOptions } from '@lib/structures/SkyraCommand';
 import { ApplyOptions, CreateResolvers } from '@skyra/decorators';
+import { Time } from '@utils/constants';
 import { cleanMentions } from '@utils/util';
 import { TextChannel } from 'discord.js';
-import { CommandStore, KlasaMessage } from 'klasa';
+import { KlasaMessage } from 'klasa';
 
-const YEAR = 1000 * 60 * 60 * 24 * 365;
-const WINNERS_ARG_REGEX = /^(\d+)w$/i;
+const kWinnersArgRegex = /^(\d+)w$/i;
 
 @ApplyOptions<SkyraCommandOptions>({
 	aliases: ['giveaway'],
@@ -13,35 +13,26 @@ const WINNERS_ARG_REGEX = /^(\d+)w$/i;
 	extendedHelp: language => language.tget('COMMAND_GIVEAWAY_EXTENDED'),
 	requiredPermissions: ['EMBED_LINKS', 'ADD_REACTIONS', 'READ_MESSAGE_HISTORY'],
 	runIn: ['text'],
-	usage: '[channel:textchannelname{2}] [winners:winners] <time:time> <title:...string{,256}>',
+	usage: '[channel:textchannelname{2}] <time:time> [winners:winners] <title:...string{,256}>',
 	flagSupport: true,
 	usageDelim: ' '
 })
 @CreateResolvers([
 	[
-		'winners', arg => parseInt(arg.match(WINNERS_ARG_REGEX)?.[1] ?? '1', 10)
+		'winners', arg => {
+			const match = kWinnersArgRegex.exec(arg);
+			if (match) return parseInt(match[1], 10);
+			throw 'Invalid winners value.';
+		}
 	]
 ])
 export default class extends SkyraCommand {
-
-	public constructor(store: CommandStore, file: string[], directory: string) {
-		super(store, file, directory, {
-			aliases: ['giveaway'],
-			description: language => language.tget('COMMAND_GIVEAWAY_DESCRIPTION'),
-			extendedHelp: language => language.tget('COMMAND_GIVEAWAY_EXTENDED'),
-			requiredPermissions: ['EMBED_LINKS', 'ADD_REACTIONS', 'READ_MESSAGE_HISTORY'],
-			runIn: ['text'],
-			usage: '[channel:textchannelname{2}] <time:time> [winners:winners] <title:...string{,256}>',
-			flagSupport: true,
-			usageDelim: ' '
-		});
-	}
 
 	public async run(message: KlasaMessage, [channel = message.channel as TextChannel, time, winners, title]: [TextChannel, Date, number, string]) {
 		const offset = time.getTime() - Date.now();
 
 		if (offset < 9500) throw message.language.tget('GIVEAWAY_TIME');
-		if (offset > YEAR) throw message.language.tget('GIVEAWAY_TIME_TOO_LONG');
+		if (offset > Time.Year) throw message.language.tget('GIVEAWAY_TIME_TOO_LONG');
 
 		await this.client.giveaways.create({
 			channelID: channel.id,

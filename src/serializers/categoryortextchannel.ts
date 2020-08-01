ee6b5739d97aea8d73095045e433b8cfc6d1172d
Soumil <soumil.paranjpay@gmail.com>
2020-08-02 00:52:56 +0530

    fix lint

--- src/serializers/categoryortextchannel.ts
+++ src/serializers/categoryortextchannel.ts
@@ -3,24 +3,25 @@ import { Channel, Guild } from 'discord.js';
 
 export default class extends Serializer {
 
-	checkChannel(data: Channel, entry: SchemaEntry, language: Language) {
-		if (data.type === 'text' || data.type === 'category') return data;
-		throw language.tget('RESOLVER_INVALID_CHANNEL', entry.key);
-	}
-
-	async validate(data: string | Channel, { entry, language, guild }: SerializerUpdateContext) {
+	// eslint-disable-next-line @typescript-eslint/require-await
+	public async validate(data: string | Channel, { entry, language, guild }: SerializerUpdateContext) {
 		if (data instanceof Channel) return this.checkChannel(data, entry, language);
 		const channel = Serializer.regex.channel.test(data) ? (guild || this.client).channels.get(Serializer.regex.channel.exec(data)![1]) : null;
 		if (channel) return this.checkChannel(channel, entry, language);
 		throw language.get('RESOLVER_INVALID_CHANNEL', entry.key);
 	}
 
-	serialize(value: Channel) {
+	public serialize(value: Channel) {
 		return value.id;
 	}
 
-	stringify(value: string, guild: Guild) {
+	public stringify(value: string, guild: Guild) {
 		return guild.channels.get(value)!.name;
 	}
 
-};
+	private checkChannel(data: Channel, entry: SchemaEntry, language: Language) {
+		if (data.type === 'text' || data.type === 'category') return data;
+		throw language.tget('RESOLVER_INVALID_CHANNEL', entry.key);
+	}
+
+}

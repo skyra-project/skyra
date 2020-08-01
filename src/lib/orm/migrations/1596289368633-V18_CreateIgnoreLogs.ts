ad336efccb14905ded63a40bf68751bebec01135
Soumil <soumil.paranjpay@gmail.com>
2020-08-01 20:26:23 +0530

    add migrations

--- src/lib/orm/migrations/1596289368633-V18_CreateIgnoreLogs.ts
+++ src/lib/orm/migrations/1596289368633-V18_CreateIgnoreLogs.ts
@@ -1,11 +1,26 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+const NEW_COLUMNS = ['channels.ignore.all', 'channels.ignore.message-edit', 'channels.ignore.message-delete', 'channels.ignore.reaction-add'];
 
 export class V18CreateIgnoreLogs1596289368633 implements MigrationInterface {
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
+		for (const column of NEW_COLUMNS) {
+			await queryRunner.addColumn('guilds', new TableColumn({
+				'name': column,
+				'type': 'varchar',
+				'length': '19',
+				'isNullable': false,
+				'isArray': true,
+				'default': 'ARRAY[]::VARCHAR[]'
+			}));
+		}
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
+		for (const column of NEW_COLUMNS) {
+			await queryRunner.dropColumn('guilds', column);
+		}
 	}
 
 }

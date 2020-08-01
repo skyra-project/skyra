d68b839b117370431ff7489b26dbeb73172d3b01
Antonio Román <kyradiscord@gmail.com>
2020-08-01 11:51:25 +0200

    fix(migration): inline values
    TypeORM is so bugged we keep finding bugs and they kinda stack up indefinitely

--- src/lib/orm/migrations/1596269328762-V17_MigrateModeratorNonNull.ts
+++ src/lib/orm/migrations/1596269328762-V17_MigrateModeratorNonNull.ts
@@ -4,15 +4,15 @@ import { CLIENT_ID } from '@root/config';
 export class V17MigrateModeratorNonNull1596269328762 implements MigrationInterface {
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(/* sql */`UPDATE public.moderation SET moderator_id = $1 WHERE moderator_id IS NULL;`, [CLIENT_ID]);
-		await queryRunner.query(/* sql */`ALTER TABLE public.moderation ALTER COLUMN moderator_id SET DEFAULT $1;`, [CLIENT_ID]);
+		await queryRunner.query(/* sql */`UPDATE public.moderation SET moderator_id = '${CLIENT_ID}' WHERE moderator_id IS NULL;`);
+		await queryRunner.query(/* sql */`ALTER TABLE public.moderation ALTER COLUMN moderator_id SET DEFAULT '${CLIENT_ID}';`);
 		await queryRunner.query(/* sql */`ALTER TABLE public.moderation ALTER COLUMN moderator_id SET NOT NULL;`);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(/* sql */`ALTER TABLE public.moderation ALTER COLUMN moderator_id DROP NOT NULL;`);
 		await queryRunner.query(/* sql */`ALTER TABLE public.moderation ALTER COLUMN moderator_id SET DEFAULT NULL;`);
-		await queryRunner.query(/* sql */`UPDATE public.moderation SET moderator_id = NULL WHERE moderator_id = $1;`, [CLIENT_ID]);
+		await queryRunner.query(/* sql */`UPDATE public.moderation SET moderator_id = NULL WHERE moderator_id = '${CLIENT_ID}';`);
 	}
 
 }

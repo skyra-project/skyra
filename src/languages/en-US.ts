2b6397de9385a2b8f36ddd99c32fe1e75eeec9b0
Jeroen Claassens <j.claassens@cgi.com>
2020-08-15 16:13:12 +0200

    fix(en-us): fixed wrong text for horoscope fail

--- src/languages/en-US.ts
+++ src/languages/en-US.ts
@@ -3967,7 +3967,7 @@ export default class extends Language {
 			],
 			examples: ['pisces', 'virgo tomorrow', 'gemini yesterday', 'aries today']
 		}),
-		COMMAND_HOROSCOPE_INVALID_SUNSIGN: (sign, maybe) => `${sign} is an invalid sun sign, maybe try ${maybe}}`,
+		COMMAND_HOROSCOPE_INVALID_SUNSIGN: (sign, maybe) => `${sign} is an invalid sun sign, maybe try ${maybe}`,
 		COMMAND_HOROSCOPE_TITLES: {
 			DAILY_HOROSCOPE: (sign) => `Daily horoscope for ${sign}`,
 			METADATA_TITLE: 'Metadata',

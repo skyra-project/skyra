fad0a587cce05461a7f2acadd9e1b18f6ca22ea3
QuantumlyTangled <ethoarx@gmail.com>
2020-08-24 14:57:13 +0200

    fix: add proper ts support acording to sentry

--- src/Skyra.ts
+++ src/Skyra.ts
@@ -4,16 +4,27 @@ import 'reflect-metadata';
 import { SkyraClient } from '@lib/SkyraClient';
 import { DbSet } from '@lib/structures/DbSet';
 import { TOKENS } from '@root/config';
-import * as sentry from '@sentry/node';
+import * as Sentry from '@sentry/node';
 import { floatPromise } from '@utils/util';
 import { inspect } from 'util';
+import { RewriteFrames } from '@sentry/integrations';
 inspect.defaultOptions.depth = 1;
 
 const client = new SkyraClient();
 
 async function main() {
 	if (TOKENS.SENTRY_URL) {
-		sentry.init({ dsn: TOKENS.SENTRY_URL });
+		Sentry.init({
+			dsn: TOKENS.SENTRY_URL,
+			integrations: [
+				new Sentry.Integrations.Modules(),
+				new Sentry.Integrations.FunctionToString(),
+				new Sentry.Integrations.LinkedErrors(),
+				new Sentry.Integrations.Console(),
+				new Sentry.Integrations.Http({ breadcrumbs: true, tracing: true }),
+				new RewriteFrames({ root: __dirname || process.cwd() })
+			]
+		});
 	}
 	await DbSet.connect();
 	await client.login(TOKENS.BOT_TOKEN);

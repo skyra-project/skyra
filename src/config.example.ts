3ba668fa1a51a3cce8d78f1eb14b5e6d0b72e918
Soumil <soumil.paranjpay@gmail.com>
2020-09-06 22:30:55 +0530

    feat: use intents

--- src/config.example.ts
+++ src/config.example.ts
@@ -92,7 +92,19 @@ export const CLIENT_OPTIONS: KlasaClientOptions = {
 	clientID: CLIENT_ID,
 	clientSecret: CLIENT_SECRET,
 	dev: DEV,
-	disabledEvents: ['CHANNEL_PINS_UPDATE', 'GUILD_CREATE', 'GUILD_MEMBER_UPDATE', 'PRESENCE_UPDATE', 'TYPING_START', 'USER_UPDATE'],
+	ws: {
+		intents: [
+			'GUILDS',
+			'GUILD_MEMBERS',
+			'GUILD_BANS',
+			'GUILD_EMOJIS',
+			'GUILD_VOICE_STATES',
+			'GUILD_MESSAGES',
+			'GUILD_MESSAGE_REACTIONS',
+			'DIRECT_MESSAGES',
+			'DIRECT_MESSAGE_REACTIONS'
+		]
+	},
 	lavalink: [
 		{
 			id: CLIENT_ID,

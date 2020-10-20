e3ff0d9634777a744e53bd4281154c91cbccbc3e
Antonio Román <kyradiscord@gmail.com>
2020-10-20 20:15:32 +0200

    fix: {[SHARD 0]} -> [SHARD 0] (#1347)

--- src/lib/structures/ShardEvent.ts
+++ src/lib/structures/ShardEvent.ts
@@ -5,6 +5,6 @@ export abstract class ShardEvent extends Event {
 	protected abstract readonly title: string;
 
 	protected header(shardID: number): string {
-		return `${bold(magenta(`{[SHARD ${shardID}]}`))} ${this.title}`;
+		return `${bold(magenta(`[SHARD ${shardID}]`))} ${this.title}`;
 	}
 }

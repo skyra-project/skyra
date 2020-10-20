b8040b5957957474c1ce7c84d5dc95ad0ce77bd0
Antonio Román <kyradiscord@gmail.com>
2020-10-20 15:02:20 +0200

    feat: added better format to error logs

--- src/events/commandError.ts
+++ src/events/commandError.ts
@@ -1,7 +1,7 @@
 import { Colors } from '@lib/types/constants/Constants';
 import { Events } from '@lib/types/Enums';
 import { LanguageKeys } from '@lib/types/namespaces/LanguageKeys';
-import { codeBlock, inlineCodeBlock } from '@sapphire/utilities';
+import { codeBlock } from '@sapphire/utilities';
 import { rootFolder } from '@utils/constants';
 import { DiscordAPIError, HTTPError, MessageEmbed } from 'discord.js';
 import { Command, Event, KlasaMessage } from 'klasa';
@@ -31,7 +31,7 @@ export default class extends Event {
 
 		// If the error was an AbortError, tell the user to re-try:
 		if (error.name === 'AbortError') {
-			this.client.emit(Events.Warn, `${this._getWarnError(message)} (${message.author.id}) | ${error.constructor.name}`);
+			this.client.emit(Events.Warn, `${this.getWarnError(message)} (${message.author.id}) | ${error.constructor.name}`);
 			try {
 				return await message.alert(message.language.get(LanguageKeys.System.DiscordAborterror));
 			} catch (err) {
@@ -44,11 +44,11 @@ export default class extends Event {
 			if (BLACKLISTED_CODES.includes(error.code)) return;
 			this.client.emit(Events.ApiError, error);
 		} else {
-			this.client.emit(Events.Warn, `${this._getWarnError(message)} (${message.author.id}) | ${error.constructor.name}`);
+			this.client.emit(Events.Warn, `${this.getWarnError(message)} (${message.author.id}) | ${error.constructor.name}`);
 		}
 
 		// Send a detailed message:
-		await this._sendErrorChannel(message, command, error);
+		await this.sendErrorChannel(message, command, error);
 
 		// Emit where the error was emitted
 		this.client.emit(Events.Wtf, `[COMMAND] ${command.path}\n${error.stack || error.message}`);
@@ -63,28 +63,18 @@ export default class extends Event {
 		}
 	}
 
-	private async _sendErrorChannel(message: KlasaMessage, command: Command, error: Error) {
-		let output: string | undefined = undefined;
+	private async sendErrorChannel(message: KlasaMessage, command: Command, error: Error) {
+		const lines = [this.getLinkLine(message.url), this.getCommandLine(command), this.getArgumentsLine(message.args), this.getErrorLine(error)];
+
+		// If it's a DiscordAPIError or a HTTPError, add the HTTP path and code lines after the second one.
 		if (error instanceof DiscordAPIError || error instanceof HTTPError) {
-			output = [
-				`${inlineCodeBlock('Command   ::')} ${command.path.slice(rootFolder.length)}`,
-				`${inlineCodeBlock('Path      ::')} ${error.path}`,
-				`${inlineCodeBlock('Code      ::')} ${error.code}`,
-				`${inlineCodeBlock('Arguments ::')} ${message.args.length ? `[\`${message.args.join('`, `')}\`]` : 'Not Supplied'}`,
-				`${inlineCodeBlock('Error     ::')} ${codeBlock('js', error.stack || error)}`
-			].join('\n');
-		} else {
-			output = [
-				`${inlineCodeBlock('Command   ::')} ${command.path.slice(rootFolder.length)}`,
-				`${inlineCodeBlock('Arguments ::')} ${message.args.length ? `[\`${message.args.join('`, `')}\`]` : 'Not Supplied'}`,
-				`${inlineCodeBlock('Error     ::')} ${codeBlock('js', error.stack || error)}`
-			].join('\n');
+			lines.splice(2, 0, this.getPathLine(error), this.getCodeLine(error));
 		}
 
 		try {
 			await this.client.webhookError.send(
 				new MessageEmbed()
-					.setDescription(output)
+					.setDescription(lines.join('\n'))
 					.setColor(Colors.Red)
 					.setAuthor(message.author.tag, message.author.displayAvatarURL({ size: 128, format: 'png', dynamic: true }), message.url)
 					.setTimestamp()
@@ -94,7 +84,56 @@ export default class extends Event {
 		}
 	}
 
-	private _getWarnError(message: KlasaMessage) {
+	/**
+	 * Formats a message url line.
+	 * @param url The url to format.
+	 */
+	private getLinkLine(url: string): string {
+		return `[**Jump to Message!**](${url})`;
+	}
+
+	/**
+	 * Formats a command line.
+	 * @param command The command to format.
+	 */
+	private getCommandLine(command: Command): string {
+		return `**Command**: ${command.path.slice(rootFolder.length)}`;
+	}
+
+	/**
+	 * Formats an error path line.
+	 * @param error The error to format.
+	 */
+	private getPathLine(error: DiscordAPIError | HTTPError): string {
+		return `**Path**: ${error.path}`;
+	}
+
+	/**
+	 * Formats an error code line.
+	 * @param error The error to format.
+	 */
+	private getCodeLine(error: DiscordAPIError | HTTPError): string {
+		return `**Code**: ${error.code}`;
+	}
+
+	/**
+	 * Formats an arguments line.
+	 * @param args The arguments the user used when running the command.
+	 */
+	private getArgumentsLine(args: readonly string[]): string {
+		if (args.length === 0) return '**Arguments**: Not Supplied';
+		return `[\`${args.map((arg) => arg.trim() || '\u200B').join('`, `')}\`]`;
+	}
+
+	/**
+	 * Formats an error codeblock.
+	 * @param error The error to format.
+	 */
+	private getErrorLine(error: Error): string {
+		return `**Error**: ${codeBlock('js', error.stack || error)}`;
+	}
+
+	private getWarnError(message: KlasaMessage) {
 		return `ERROR: /${message.guild ? `${message.guild.id}/${message.channel.id}` : `DM/${message.author.id}`}/${message.id}`;
 	}
 }

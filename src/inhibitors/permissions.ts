d3b27acbd0e67ef2d5b51a7ab7427f477d53a9a6
Jeroen Claassens <support@favware.tech>
2020-11-14 21:18:56 +0100

    fix: permission actual fix

--- src/inhibitors/permissions.ts
+++ src/inhibitors/permissions.ts
@@ -3,14 +3,11 @@ import { Command, Inhibitor, KlasaMessage } from 'klasa';
 
 export default class extends Inhibitor {
 	public async run(message: KlasaMessage, command: Command) {
-		// If this command is being used outside of a guild context then always allow it
-		if (!message.guild || !message.member) return;
-
-		const [pnodes, language] = await message.guild.readSettings((settings) => [settings.permissionNodes, settings.getLanguage()]);
-
 		// If the message was sent in a guild, the command isn't guarded (they are all 0, and
 		// cannot be denied), and the permission level is lower than 9, run the permission nodes.
-		if (message.guild && message.author.id !== message.guild.ownerID && !command.guarded && command.permissionLevel < 9) {
+		if (message.guild && message.member && message.author.id !== message.guild.ownerID && !command.guarded && command.permissionLevel < 9) {
+			const [pnodes, language] = await message.guild.readSettings((settings) => [settings.permissionNodes, settings.getLanguage()]);
+
 			const result = pnodes.run(message.member, command.name);
 			if (result) return;
 			if (result === false) throw language.get(LanguageKeys.Inhibitors.Permissions);
@@ -19,6 +16,6 @@ export default class extends Inhibitor {
 		const { broke, permission } = await this.client.permissionLevels.run(message, command.permissionLevel);
 		if (permission) return;
 
-		throw broke ? language.get(LanguageKeys.Inhibitors.Permissions) : true;
+		throw broke ? await message.fetchLocale(LanguageKeys.Inhibitors.Permissions) : true;
 	}
 }

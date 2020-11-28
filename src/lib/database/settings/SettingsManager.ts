ab8c59ba614b855b7d7ae6503106c84c18d1fdf8
Kaoru <KunoichiZ@users.noreply.github.com>
2020-11-28 12:05:34 -0600

    feat: add Task and TaskStore structures to SettingsManager

--- src/lib/database/settings/SettingsManager.ts
+++ src/lib/database/settings/SettingsManager.ts
@@ -1,15 +1,18 @@
 import type { Client } from 'discord.js';
 import { GuildSettingsCollection } from './structures/collections/GuildSettingsCollection';
 import { SerializerStore } from './structures/SerializerStore';
+import { TaskStore } from './structures/TaskStore';
 
 export class SettingsManager {
 	public readonly client: Client;
 	public readonly serializers: SerializerStore;
+	public readonly tasks: TaskStore;
 	public readonly guilds: GuildSettingsCollection;
 
 	public constructor(client: Client) {
 		this.client = client;
 		this.guilds = new GuildSettingsCollection(client);
 		this.serializers = new SerializerStore(client);
+		this.tasks = new TaskStore(client);
 	}
 }

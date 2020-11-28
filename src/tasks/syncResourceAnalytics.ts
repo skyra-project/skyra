5f2dd11ecb69596a9b04b642896b9787a21022b9
Kaoru <KunoichiZ@users.noreply.github.com>
2020-11-28 12:21:16 -0600

    fix: import Task from #lib/database

--- src/tasks/syncResourceAnalytics.ts
+++ src/tasks/syncResourceAnalytics.ts
@@ -1,8 +1,9 @@
 import { Events } from '#lib/types/Enums';
-import { Task } from 'klasa';
+import { Task } from '#lib/database';
 
 export default class extends Task {
 	public run() {
-		return this.client.emit(Events.ResourceAnalyticsSync);
+		this.client.emit(Events.ResourceAnalyticsSync);
+		return null;
 	}
 }

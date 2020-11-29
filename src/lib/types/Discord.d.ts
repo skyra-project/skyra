770dbf19189168199767080567b800e616c0d0a6
Kaoru <KunoichiZ@users.noreply.github.com>
2020-11-28 18:30:24 -0600

    fix(discord.d): update MessageAcknowledgeable

--- src/lib/types/Discord.d.ts
+++ src/lib/types/Discord.d.ts
@@ -1,4 +1,16 @@
-import type { DMChannel, Guild, GuildMember, NewsChannel, PartialSendAliases, TextChannel } from 'discord.js';
+import type {
+	APIMessage,
+	DMChannel,
+	Guild,
+	GuildMember,
+	MessageAdditions,
+	MessageOptions,
+	NewsChannel,
+	PartialSendAliases,
+	SplitOptions,
+	StringResolvable,
+	TextChannel
+} from 'discord.js';
 import type { KlasaMessage } from 'klasa';
 
 export interface GuildMessage extends KlasaMessage {
@@ -15,4 +27,12 @@ export interface DMMessage extends KlasaMessage {
 
 export interface MessageAcknowledgeable extends PartialSendAliases {
 	readonly guild: Guild;
+	send(content?: StringResolvable, options?: MessageOptions | MessageAdditions): Promise<KlasaMessage>;
+	// eslint-disable-next-line @typescript-eslint/unified-signatures
+	send(content?: StringResolvable, options?: (MessageOptions & { split?: false }) | MessageAdditions): Promise<KlasaMessage>;
+	send(content?: StringResolvable, options?: (MessageOptions & { split: true | SplitOptions }) | MessageAdditions): Promise<KlasaMessage[]>;
+	send(options?: MessageOptions | MessageAdditions | APIMessage): Promise<KlasaMessage>;
+	// eslint-disable-next-line @typescript-eslint/unified-signatures
+	send(options?: (MessageOptions & { split?: false }) | MessageAdditions | APIMessage): Promise<KlasaMessage>;
+	send(options?: (MessageOptions & { split: true | SplitOptions }) | MessageAdditions | APIMessage): Promise<KlasaMessage[]>;
 }

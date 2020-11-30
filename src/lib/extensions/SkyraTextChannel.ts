5b3bf5d1f50e18c7e8c4dcb2f83c05059c740fdc
Kaoru <KunoichiZ@users.noreply.github.com>
2020-11-30 00:48:10 -0600

    feat: add MessageExtendables to SkyraTextChannel

--- src/lib/extensions/SkyraTextChannel.ts
+++ src/lib/extensions/SkyraTextChannel.ts
@@ -1,4 +1,4 @@
-import { Message, Structures, TextChannel } from 'discord.js';
+import { Message, Structures, TextChannel, Permissions } from 'discord.js';
 
 const snipes = new WeakMap<TextChannel, SnipedMessage>();
 
@@ -21,6 +21,22 @@ export class SkyraTextChannel extends Structures.get('TextChannel') {
 	public get sniped() {
 		const current = snipes.get(this);
 		return typeof current === 'undefined' ? null : current.message;
+	}
+
+	public get attachable() {
+		return !this.guild || (this.postable && this.permissionsFor(this.guild.me!)!.has(Permissions.FLAGS.ATTACH_FILES, false));
+	}
+
+	public get embedable() {
+		return !this.guild || (this.postable && this.permissionsFor(this.guild.me!)!.has(Permissions.FLAGS.EMBED_LINKS, false));
+	}
+
+	public get postable() {
+		return !this.guild || this.permissionsFor(this.guild.me!)!.has([Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES], false);
+	}
+
+	public get readable() {
+		return !this.guild || this.permissionsFor(this.guild.me!)!.has(Permissions.FLAGS.VIEW_CHANNEL, false);
 	}
 }
 

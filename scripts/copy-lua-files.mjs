cfc3c5accbd62877ca2eaad5d27cda18c53eafc9
Jeroen Claassens <support@favware.tech>
2020-12-08 22:12:29 +0100

    refactor: use fs/promises everywheree

--- scripts/copy-lua-files.mjs
+++ scripts/copy-lua-files.mjs
@@ -1,4 +1,5 @@
-import { existsSync, promises as fsp } from 'fs';
+import { existsSync } from 'fs';
+import { copyFile, mkdir, readdir } from 'fs/promises';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -10,9 +11,9 @@ const LUA_SCRIPTS_DIR = resolve(__dirname, 'audio');
 const OUTPUT_FOLDER = resolve(DIST_DIR, 'lib', 'audio', 'scripts');
 
 if (!existsSync(OUTPUT_FOLDER)) {
-	await fsp.mkdir(OUTPUT_FOLDER, { recursive: true });
+	await mkdir(OUTPUT_FOLDER, { recursive: true });
 }
 
-for (const script of await fsp.readdir(LUA_SCRIPTS_DIR)) {
-	await fsp.copyFile(resolve(LUA_SCRIPTS_DIR, script), resolve(OUTPUT_FOLDER, script));
+for (const script of await readdir(LUA_SCRIPTS_DIR)) {
+	await copyFile(resolve(LUA_SCRIPTS_DIR, script), resolve(OUTPUT_FOLDER, script));
 }

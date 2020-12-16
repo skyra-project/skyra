613de9c35d197f0bd029ad7f08c5fcc7c9205da9
Antonio Román <kyradiscord@gmail.com>
2020-12-17 00:41:14 +0100

    chore: move almost all of the remaining keys

--- src/lib/types/namespaces/languages/settings/Gateway.ts
+++ src/lib/types/namespaces/languages/settings/Gateway.ts
@@ -1,9 +1,5 @@
-import { FT, T } from '#lib/types';
+import { FT } from '#lib/types';
 
-export const KeyNoext = FT<{ key: string }, string>('settingGatewayKeyNoext');
 export const ChooseKey = FT<{ keys: string }, string>('settingGatewayChooseKey');
-export const UnconfigurableFolder = T<string>('settingGatewayUnconfigurableFolder');
-export const UnconfigurableKey = FT<{ key: string }, string>('settingGatewayUnconfigurableKey');
 export const MissingValue = FT<{ path: string; value: string }, string>('settingGatewayMissingValue');
 export const DuplicateValue = FT<{ path: string; value: string }, string>('settingGatewayDuplicateValue');
-export const InvalidFilteredValue = FT<{ path: string; value: unknown }, string>('settingGatewayInvalidFilteredValue');

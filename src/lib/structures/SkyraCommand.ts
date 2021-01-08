687fa240135baf42275df39a71e3cbdaf85374e4
Nejc Drobnic <ethoarx@gmail.com>
2021-01-08 13:20:17 +0100

    feat: add command description and extended help as options

--- src/lib/structures/SkyraCommand.ts
+++ src/lib/structures/SkyraCommand.ts
@@ -1,16 +1,24 @@
 import { PermissionResolvable, Permissions } from 'discord.js';
+import { TFunction } from 'i18next';
 import { Command, CommandOptions, CommandStore, KlasaMessage } from 'klasa';
 
 export abstract class SkyraCommand extends Command {
+	public description: string | ((language: TFunction) => string);
+	public extendedHelp: string | ((language: TFunction) => Record<string, unknown>);
+
 	public spam: boolean;
 	public requiredGuildPermissions: Permissions;
 
-	public constructor(store: CommandStore, file: string[], directory: string, options: SkyraCommandOptions = {}) {
+	public constructor(store: CommandStore, file: string[], directory: string, options: SkyraCommandOptions) {
 		super(store, file, directory, {
 			spam: false,
 			requiredGuildPermissions: 0,
 			...options
 		});
+
+		this.description = options.description;
+		this.extendedHelp = options.extendedHelp;
+
 		this.spam = options.spam!;
 		this.requiredGuildPermissions = new Permissions(options.requiredGuildPermissions);
 	}
@@ -27,6 +35,8 @@ export abstract class SkyraCommand extends Command {
 }
 
 export interface SkyraCommandOptions extends CommandOptions {
+	description: string | ((language: TFunction) => string);
+	extendedHelp: string | ((language: TFunction) => Record<string, unknown>);
 	spam?: boolean;
 	requiredGuildPermissions?: PermissionResolvable;
 }

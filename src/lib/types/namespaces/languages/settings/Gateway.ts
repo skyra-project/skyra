1497c2d084cb81db372b256c74f544bd8883fa0d
Antonio Román <kyradiscord@gmail.com>
2021-01-12 12:07:20 +0100

    fix: more and more fixes

--- src/lib/types/namespaces/languages/settings/Gateway.ts
+++ src/lib/types/namespaces/languages/settings/Gateway.ts
@@ -1,5 +1,5 @@
 import { FT } from '#lib/types';
 
-export const ChooseKey = FT<{ keys: string }, string>('settingGatewayChooseKey');
-export const MissingValue = FT<{ path: string; value: string }, string>('settingGatewayMissingValue');
-export const DuplicateValue = FT<{ path: string; value: string }, string>('settingGatewayDuplicateValue');
+export const ChooseKey = FT<{ keys: string }, string>('setting:validationChooseKey');
+export const MissingValue = FT<{ path: string; value: string }, string>('setting:validationMissingValue');
+export const DuplicateValue = FT<{ path: string; value: string }, string>('setting:validationDuplicatedValue');

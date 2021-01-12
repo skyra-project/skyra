e6773b4f4e45dde673400eaea30f744f57c5bf8c
Antonio Román <kyradiscord@gmail.com>
2021-01-12 12:35:21 +0100

    fix: resolved mismatching ns bug

--- src/lib/types/namespaces/languages/settings/Gateway.ts
+++ src/lib/types/namespaces/languages/settings/Gateway.ts
@@ -1,5 +1,5 @@
 import { FT } from '#lib/types';
 
-export const ChooseKey = FT<{ keys: string }, string>('setting:validationChooseKey');
-export const MissingValue = FT<{ path: string; value: string }, string>('setting:validationMissingValue');
-export const DuplicateValue = FT<{ path: string; value: string }, string>('setting:validationDuplicatedValue');
+export const ChooseKey = FT<{ keys: string }, string>('settings:validationChooseKey');
+export const MissingValue = FT<{ path: string; value: string }, string>('settings:validationMissingValue');
+export const DuplicateValue = FT<{ path: string; value: string }, string>('settings:validationDuplicatedValue');

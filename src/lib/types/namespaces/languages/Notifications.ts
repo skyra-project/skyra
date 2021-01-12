cf13d70e5e347956d70eed979ab6a56b1d567be1
Antonio Román <kyradiscord@gmail.com>
2021-01-12 12:54:41 +0100

    fix: added missing namespaces

--- src/lib/types/namespaces/languages/Notifications.ts
+++ src/lib/types/namespaces/languages/Notifications.ts
@@ -1,6 +1,6 @@
 import { FT, T } from '#lib/types';
 
-export const TwitchNoGameName = T<string>('twitchNoGameName');
-export const TwitchEmbedDescription = FT<{ userName: string }, string>('twitchEmbedDescription');
-export const TwitchEmbedDescriptionWithGame = FT<{ userName: string; gameName: string }, string>('twitchEmbedDescriptionWithGame');
-export const TwitchEmbedFooter = T<string>('twitchEmbedFooter');
+export const TwitchNoGameName = T<string>('notifications:twitchNoGameName');
+export const TwitchEmbedDescription = FT<{ userName: string }, string>('notifications:twitchEmbedDescription');
+export const TwitchEmbedDescriptionWithGame = FT<{ userName: string; gameName: string }, string>('notifications:twitchEmbedDescriptionWithGame');
+export const TwitchEmbedFooter = T<string>('notifications:twitchEmbedFooter');

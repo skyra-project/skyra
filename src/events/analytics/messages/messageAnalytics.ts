7f39ff74b10bc15a784662741d13eb36948d5f09
Antonio Román <kyradiscord@gmail.com>
2021-01-15 21:01:24 +0100

    fix: resolved messageAnalytics bug

--- src/events/analytics/messages/messageAnalytics.ts
+++ src/events/analytics/messages/messageAnalytics.ts
@@ -1,5 +1,9 @@
 import { AnalyticsEvent } from '#lib/structures/events/AnalyticsEvent';
+import { Events } from '#lib/types/Enums';
+import { ApplyOptions } from '@skyra/decorators';
+import { EventOptions } from 'klasa';
 
+@ApplyOptions<EventOptions>({ event: Events.Message })
 export default class extends AnalyticsEvent {
 	public run(): void {
 		this.client.analytics!.messageCount++;

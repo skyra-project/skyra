da6f7760250b15eb2a8caf7314413bb6db781931
Antonio Román <kyradiscord@gmail.com>
2021-01-17 10:31:57 +0100

    refactor: add better message delete event handlers

---
+++ src/events/messages/messageDeleteBulkRemoveResponses.ts
@@ -0,0 +1,15 @@
+import { Events } from '#lib/types/Enums';
+import { ApplyOptions } from '@skyra/decorators';
+import { Collection, Message, Snowflake } from 'discord.js';
+import { Event, EventOptions } from 'klasa';
+
+@ApplyOptions<EventOptions>({ event: Events.MessageDeleteBulk })
+export default class extends Event {
+	public async run(messages: Collection<Snowflake, Message>) {
+		for (const message of messages.values()) {
+			for (const response of message.responses) {
+				await response.nuke();
+			}
+		}
+	}
+}

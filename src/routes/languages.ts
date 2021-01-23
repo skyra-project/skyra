bec83b3398281831e506d64305a3bb6cb02ce683
Jeroen Claassens <support@favware.tech>
2021-01-23 14:18:29 +0100

    feat: add languages route

---
+++ src/routes/languages.ts
@@ -0,0 +1,11 @@
+import { ratelimit } from '#utils/util';
+import { ApplyOptions } from '@sapphire/decorators';
+import { ApiRequest, ApiResponse, methods, Route, RouteOptions } from '@sapphire/plugin-api';
+
+@ApplyOptions<RouteOptions>({ route: 'languages' })
+export default class extends Route {
+	@ratelimit(2, 2500)
+	public [methods.GET](_: ApiRequest, response: ApiResponse) {
+		return response.json([...this.context.client.i18n.languages.keys()]);
+	}
+}

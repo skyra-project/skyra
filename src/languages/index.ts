e263f7d186b309bfa7bfbefcb4df9fe4bccbd4d2
Adrian Castro <adrifcastr@gmail.com>
2021-01-23 15:15:46 +0100

    feat: add de-DE handler & constants (#1518)
    * add de-DE handler
    
    * add de-DE constants
    
    * proper intendation
    
    * commit kyra's suggestion
    
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>
    
    * commit this suggestion aswell
    
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>
    
    * sort handlers alphabetically
    
    * also sort the import alphabetically
    
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -1,10 +1,12 @@
 import type { Handler } from '#lib/i18n/structures/Handler';
+import { ExtendedHandler as DeDeHandler } from './de-DE/constants';
 import { ExtendedHandler as EnGbHandler } from './en-GB/constants';
 import { ExtendedHandler as EnUsHandler } from './en-US/constants';
 import { ExtendedHandler as EsEsHandler } from './es-ES/constants';
 import { ExtendedHandler as NlNlHandler } from './nl-NL/constants';
 
 export const handlers = new Map<string, Handler>([
+	['de-DE', new DeDeHandler()],
 	['en-US', new EnUsHandler()],
 	['en-GB', new EnGbHandler()],
 	['es-ES', new EsEsHandler()],

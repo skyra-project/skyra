af672397a1d416ac3e1b79f89ad7c9b8b475f6f3
Antonio Román <kyradiscord@gmail.com>
2021-02-15 10:30:03 +0100

    refactor: improve permissions handling (#1551)

--- src/lib/structures/preconditions/PermissionsPrecondition.ts
+++ src/lib/structures/preconditions/PermissionsPrecondition.ts
@@ -1,26 +1,35 @@
 import { LanguageKeys } from '#lib/i18n/languageKeys';
 import type { GuildMessage } from '#lib/types';
 import { AsyncPreconditionResult, Identifiers, Precondition, PreconditionContext, PreconditionResult } from '@sapphire/framework';
-import type { Message } from 'discord.js';
 import type { SkyraCommand } from '../commands/SkyraCommand';
 
 export abstract class PermissionsPrecondition extends Precondition {
-	public async run(message: Message, command: SkyraCommand, context: PermissionsPrecondition.Context): PermissionsPrecondition.AsyncResult {
-		// If not in a guild, skip:
+	public async run(message: GuildMessage, command: SkyraCommand, context: PermissionsPrecondition.Context): PermissionsPrecondition.AsyncResult {
+		// If not in a guild, resolve on an error:
 		if (message.guild === null || message.member === null) return this.error({ identifier: Identifiers.PreconditionGuildOnly });
-		// Guarded commands cannot be modified:
-		if (command.guarded) return this.handle(message as GuildMessage, command, context);
-		// If the author is owner of the guild, skip:
-		if (message.author.id === message.guild.ownerID) return this.ok();
 
-		const nodes = await message.guild.readSettings((settings) => settings.permissionNodes);
-		const result = nodes.run(message.member, command.name);
-		if (result) return this.ok();
-		if (result === false) return this.error({ identifier: LanguageKeys.Preconditions.PermissionNodes });
-		return this.handle(message as GuildMessage, command, context);
+		// If it should skip, go directly to handle:
+		if (this.shouldRun(message, command)) {
+			const nodes = await message.guild.readSettings((settings) => settings.permissionNodes);
+			const result = nodes.run(message.member, command.name);
+			if (result) return this.ok();
+			if (result === false) return this.error({ identifier: LanguageKeys.Preconditions.PermissionNodes });
+		}
+
+		// Run the specific precondition's logic:
+		return this.handle(message, command, context);
 	}
 
 	public abstract handle(message: GuildMessage, command: SkyraCommand, context: PermissionsPrecondition.Context): PermissionsPrecondition.Result;
+
+	private shouldRun(message: GuildMessage, command: SkyraCommand) {
+		// Guarded commands cannot be modified:
+		if (command.guarded) return false;
+		// If the author is owner of the guild, skip:
+		if (message.author.id === message.guild.ownerID) return false;
+		// In any other case, permission nodes should always run:
+		return true;
+	}
 }
 
 // eslint-disable-next-line @typescript-eslint/no-namespace

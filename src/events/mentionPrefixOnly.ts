115d42c7a013734cf64bcca5c0bcc562a9c83f42
Antonio Román <kyradiscord@gmail.com>
2021-02-16 11:08:38 +0100

    fix: restore command and channel disable checks (#1562)
    * fix: restore command and channel disable checks
    
    * fix: update all dependencies
    
    * fix: use ApplyOptions

--- src/events/mentionPrefixOnly.ts
+++ src/events/mentionPrefixOnly.ts
@@ -1,10 +1,27 @@
+import { GuildSettings } from '#lib/database';
 import { LanguageKeys } from '#lib/i18n/languageKeys';
+import type { DMMessage, GuildMessage } from '#lib/types';
 import { Event, Events } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
 export default class extends Event<Events.MentionPrefixOnly> {
-	public async run(message: Message) {
+	public run(message: Message) {
+		return message.guild ? this.guild(message as GuildMessage) : this.dm(message as DMMessage);
+	}
+
+	private async dm(message: DMMessage) {
 		const prefix = await this.context.client.fetchPrefix(message);
 		return message.sendTranslated(LanguageKeys.Misc.PrefixReminder, [{ prefix }]);
+	}
+
+	private async guild(message: GuildMessage) {
+		const [prefix, disabled, t] = await message.guild.readSettings((settings) => [
+			settings[GuildSettings.Prefix],
+			settings[GuildSettings.DisabledChannels],
+			settings.getLanguage()
+		]);
+
+		if (disabled.includes(message.channel.id) && !(await message.member.isModerator())) return;
+		return message.send(t(LanguageKeys.Misc.PrefixReminder, { prefix }));
 	}
 }

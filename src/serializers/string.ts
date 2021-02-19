2ffa9a93eb0e3b571b826f2343368d3858d8f5b1
Antonio Román <kyradiscord@gmail.com>
2021-02-19 11:22:07 +0100

    fix(serializers): string should rest

--- src/serializers/string.ts
+++ src/serializers/string.ts
@@ -3,7 +3,7 @@ import type { Awaited } from '@sapphire/utilities';
 
 export class UserSerializer extends Serializer<string> {
 	public async parse(args: Serializer.Args, { entry }: SerializerUpdateContext) {
-		return this.result(args, await args.pickResult('string', { minimum: entry.minimum, maximum: entry.maximum }));
+		return this.result(args, await args.restResult('string', { minimum: entry.minimum, maximum: entry.maximum }));
 	}
 
 	public isValid(value: string, context: SerializerUpdateContext): Awaited<boolean> {

26d91a9930e9601bec5db2febbfed02de8ad3511
Antonio Román <kyradiscord@gmail.com>
2021-02-21 12:14:36 +0100

    fix: resolved second bug

---
+++ tests/lib/comparators.ts
@@ -0,0 +1,76 @@
+import { hasAtLeastOneKeyInMap, isNullishOrEmpty, isNullishOrZero } from '#utils/comparators';
+
+describe('comparators', () => {
+	describe('isNullishOrZero', () => {
+		test('GIVEN undefined THEN passes test', () => {
+			expect(isNullishOrZero(undefined)).toBe(true);
+		});
+
+		test('GIVEN null THEN passes test', () => {
+			expect(isNullishOrZero(null)).toBe(true);
+		});
+
+		test('GIVEN 0 THEN passes test', () => {
+			expect(isNullishOrZero(0)).toBe(true);
+		});
+
+		test('GIVEN "" THEN does not pass test', () => {
+			expect(isNullishOrZero('')).toBe(false);
+		});
+	});
+
+	describe('isNullishOrEmpty', () => {
+		test('GIVEN undefined THEN passes test', () => {
+			expect(isNullishOrEmpty(undefined)).toBe(true);
+		});
+
+		test('GIVEN null THEN passes test', () => {
+			expect(isNullishOrEmpty(null)).toBe(true);
+		});
+
+		test('GIVEN 0 THEN does not pass test', () => {
+			expect(isNullishOrEmpty(0)).toBe(false);
+		});
+
+		test('GIVEN "" THEN does passes test', () => {
+			expect(isNullishOrEmpty('')).toBe(true);
+		});
+	});
+
+	describe('hasAtLeastOneKeyInMap', () => {
+		test('GIVEN empty map and empty array THEN does not pass test', () => {
+			expect(hasAtLeastOneKeyInMap(new Map<string, string>([]), [])).toEqual(false);
+		});
+
+		test('GIVEN filled map and empty array THEN does not pass test', () => {
+			expect(
+				hasAtLeastOneKeyInMap(
+					new Map<string, string>([['foo', 'bar']]),
+					[]
+				)
+			).toEqual(false);
+		});
+
+		test('GIVEN empty map and filled array THEN does not pass test', () => {
+			expect(hasAtLeastOneKeyInMap(new Map<string, string>([]), ['foo'])).toEqual(false);
+		});
+
+		test('GIVEN filled map and filled array with no common keys THEN passes test', () => {
+			expect(
+				hasAtLeastOneKeyInMap(
+					new Map<string, string>([['foo', 'bar']]),
+					['world', 'baz', 'foo']
+				)
+			).toEqual(true);
+		});
+
+		test('GIVEN filled map and filled array with at least one common key THEN passes test', () => {
+			expect(
+				hasAtLeastOneKeyInMap(
+					new Map<string, string>([['foo', 'bar']]),
+					['world', 'baz']
+				)
+			).toEqual(true);
+		});
+	});
+});

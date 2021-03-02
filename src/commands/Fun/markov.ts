6530a10b919464a2f36612ca8d7d530546f0dace
Soumil <soumildiscord@gmail.com>
2021-03-02 20:21:54 +0530

    fix: better error message

--- src/commands/Fun/markov.ts
+++ src/commands/Fun/markov.ts
@@ -36,7 +36,7 @@ export class UserCommand extends SkyraCommand {
 	public async run(message: GuildMessage, args: SkyraCommand.Args) {
 		const channel = await args.pick('textChannelName').catch(() => message.channel as TextChannel);
 		if (channel.nsfw && !message.channel.nsfw) {
-			return this.error(LanguageKeys.Commands.Fun.MarkovNsfwChannel);
+			return this.error(LanguageKeys.Commands.Fun.MarkovNsfwChannel, { channel: channel.toString() });
 		}
 		const username = args.finished ? message.author : await args.pick('userName');
 

18d97c5f9b828680e6473d7e2c6fdc0e649afc6e
Soumil <soumildiscord@gmail.com>
2021-03-12 13:35:21 +0530

    fix: missed these

--- src/commands/Misc/Birthday/view-birthday.ts
+++ src/commands/Misc/Birthday/view-birthday.ts
@@ -2,6 +2,7 @@ import { SkyraCommand } from '#lib/structures';
 import { LanguageKeys } from '#lib/i18n/languageKeys';
 import { ApplyOptions } from '@sapphire/decorators';
 import { GuildMessage } from '#lib/types';
+import { CommandContext } from '@sapphire/framework';
 
 @ApplyOptions<SkyraCommand.Options>({
 	cooldown: 10,
@@ -10,18 +11,19 @@ import { GuildMessage } from '#lib/types';
 	runIn: ['text']
 })
 export default class extends SkyraCommand {
-	public async run(message: GuildMessage, args: SkyraCommand.Args) {
+	public async run(message: GuildMessage, args: SkyraCommand.Args, context: CommandContext) {
 		const user = args.finished ? message.author : await args.pick('userName');
 		const birthDate = this.context.client.schedules.queue.find(
 			(schedule) => schedule.taskID === 'birthday' && schedule.data.guildID === message.guild.id && schedule.data.userID === user.id
 		)?.time;
 
 		return message.send(
-			args.t(
-				birthDate
-					? args.t(LanguageKeys.Commands.Misc.ViewBirthdaySet, { birthDate: birthDate.getTime(), user: user.tag })
-					: args.t(LanguageKeys.Commands.Misc.ViewBirthdayNotSet, { user: user.tag })
-			)
+			birthDate
+				? (args.t(LanguageKeys.Commands.Misc.ViewBirthdaySet, {
+						birthDate: birthDate.getTime(),
+						user: user.tag
+				  }) as string)
+				: (args.t(LanguageKeys.Commands.Misc.ViewBirthdayNotSet, { user: user.tag, prefix: context.commandPrefix }) as string)
 		);
 	}
 }

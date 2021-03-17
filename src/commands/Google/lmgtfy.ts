418041d0d205a54542ba3c567a91cfb20f087cf1
Jeroen Claassens <support@favware.tech>
2021-03-17 13:32:05 +0100

    feat: add back lmgtfy command

---
+++ src/commands/Google/lmgtfy.ts
@@ -0,0 +1,32 @@
+import { DbSet } from '#lib/database';
+import { LanguageKeys } from '#lib/i18n/languageKeys';
+import { SkyraCommand } from '#lib/structures';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Message, MessageEmbed } from 'discord.js';
+
+@ApplyOptions<SkyraCommand.Options>({
+	aliases: ['letmegooglethatforyou', 'letmegooglethat'],
+	cooldown: 10,
+	description: LanguageKeys.Commands.Google.LmgtfyDescription,
+	extendedHelp: LanguageKeys.Commands.Google.LmgtfyExtended,
+	permissions: ['EMBED_LINKS']
+})
+export class UserCommand extends SkyraCommand {
+	public async run(message: Message, args: SkyraCommand.Args) {
+		const [query, color] = await Promise.all([args.rest('string'), DbSet.fetchColor(message)]);
+
+		const url = new URL('https://letmegooglethat.com/');
+		url.searchParams.append('q', query);
+
+		return message.send(
+			new MessageEmbed() //
+				.setColor(color) //
+				.setDescription(
+					args.t(
+						LanguageKeys.Commands.Google.LmgtfyClick,
+						{ link: url } //
+					)
+				)
+		);
+	}
+}

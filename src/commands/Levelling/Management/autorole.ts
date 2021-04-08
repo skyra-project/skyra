32835cce48ae137fa6b9badbf737e3b2286e1182
Antonio Román <kyradiscord@gmail.com>
2021-04-08 12:31:29 +0200

    chore: argh! about to give up :(

---
+++ src/commands/Levelling/Management/autorole.ts
@@ -0,0 +1,166 @@
+import { GuildSettings, RolesAuto } from '#lib/database';
+import { LanguageKeys } from '#lib/i18n/languageKeys';
+import { SkyraCommand } from '#lib/structures';
+import type { GuildMessage } from '#lib/types';
+import { PermissionLevels } from '#lib/types/Enums';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Args, Identifiers } from '@sapphire/framework';
+import { deepClone } from '@sapphire/utilities';
+
+const SORT = (x: RolesAuto, y: RolesAuto) => Number(x.points > y.points) || Number(x.points === y.points) - 1;
+
+@ApplyOptions<SkyraCommand.Options>({
+	aliases: ['autoroles', 'levelrole', 'lvlrole'],
+	cooldown: 10,
+	description: LanguageKeys.Commands.Social.AutoRoleDescription,
+	extendedHelp: LanguageKeys.Commands.Social.AutoRoleExtended,
+	permissionLevel: PermissionLevels.Administrator,
+	permissions: ['MANAGE_ROLES'],
+	runIn: ['text', 'news'],
+	subCommands: ['add', 'remove', 'update', { input: 'show', default: true }]
+})
+export class UserCommand extends SkyraCommand {
+	public async add(message: GuildMessage, args: SkyraCommand.Args) {
+		const role = await args.pick('roleName');
+		const points = await this.parseLevel(args);
+
+		await message.guild.writeSettings((settings) => {
+			const roles = settings[GuildSettings.Roles.Auto];
+
+			if (roles.length && roles.some((entry) => entry.id === role.id)) {
+				this.error(LanguageKeys.Commands.Social.AutoRoleUpdateConfigured);
+			}
+
+			const sorted = [...roles, { id: role.id, points }].sort(SORT);
+
+			settings[GuildSettings.Roles.Auto] = sorted;
+		});
+
+		return message.send(args.t(LanguageKeys.Commands.Social.AutoRoleAdd, { role: role.toString(), points }), {
+			allowedMentions: { users: [], roles: [] }
+		});
+	}
+
+	public async remove(message: GuildMessage, args: SkyraCommand.Args) {
+		const role = await args.pick('roleName');
+
+		const roleEntry = await message.guild.writeSettings((settings) => {
+			const roles = settings[GuildSettings.Roles.Auto];
+			const roleIndex = roles.findIndex((entry) => entry.id === role.id);
+
+			if (roleIndex === -1) {
+				this.error(LanguageKeys.Commands.Social.AutoRoleUpdateUnconfigured);
+			}
+
+			const roleEntry = roles[roleIndex];
+			roles.splice(roleIndex, 1);
+
+			return roleEntry;
+		});
+
+		return message.send(args.t(LanguageKeys.Commands.Social.AutoRoleRemove, { role: role.toString(), before: roleEntry.points }), {
+			allowedMentions: { users: [], roles: [] }
+		});
+	}
+
+	public async update(message: GuildMessage, args: SkyraCommand.Args) {
+		const role = await args.pick('roleName');
+		const points = await this.parseLevel(args);
+
+		const autoRole = await message.guild.writeSettings((settings) => {
+			const autoRoles = settings[GuildSettings.Roles.Auto];
+			const roleIndex = autoRoles.findIndex((entry) => entry.id === role.id);
+
+			if (roleIndex === -1) {
+				this.error(LanguageKeys.Commands.Social.AutoRoleUpdateUnconfigured);
+			}
+
+			const clone = deepClone(autoRoles);
+			clone[roleIndex].points = points;
+
+			settings[GuildSettings.Roles.Auto] = clone.sort(SORT);
+			return autoRoles[roleIndex];
+		});
+
+		return message.send(args.t(LanguageKeys.Commands.Social.AutoRoleUpdate, { role: role.toString(), points, before: autoRole.points }), {
+			allowedMentions: { users: [], roles: [] }
+		});
+	}
+
+	public async show(message: GuildMessage) {
+		const output = await message.guild.writeSettings((settings) => {
+			const autoRoles = settings[GuildSettings.Roles.Auto];
+
+			if (!autoRoles.length) this.error(LanguageKeys.Commands.Social.AutoRoleListEmpty);
+
+			const filtered = new Set(autoRoles);
+			const output: string[] = [];
+			for (const obj of autoRoles) {
+				const role = message.guild.roles.cache.get(obj.id);
+				if (role) output.push(`${obj.points.toString().padStart(6, ' ')} : ${role.name}`);
+				else filtered.delete(obj);
+			}
+
+			if (filtered.size !== autoRoles.length) {
+				settings[GuildSettings.Roles.Auto] = [...filtered];
+			}
+
+			if (!output.length) this.error(LanguageKeys.Commands.Social.AutoRoleListEmpty);
+
+			return output;
+		});
+
+		return message.send(output.join('\n'), { allowedMentions: { users: [], roles: [] }, code: 'http' });
+	}
+
+	private async parseLevel(args: SkyraCommand.Args) {
+		const result = await args.pickResult('integer', { minimum: 1, maximum: UserCommand.maximumPoints });
+
+		// If the integer parse was successful, return it:
+		if (result.success) return result.value;
+
+		// If it was erroneous, but it was because it wasn't a valid integer, try parsing level:
+		if (result.error.identifier === Identifiers.ArgumentInteger) return args.pick(UserCommand.level);
+
+		// It was a valid integer, but the number was out of range:
+		throw result.error;
+	}
+
+	private static readonly maximumPoints = 100_000_000;
+	private static readonly level = Args.make<number>((parameter, { argument }) => {
+		if (parameter.length === 1 || (!parameter.endsWith('l') && !parameter.endsWith('L'))) {
+			return Args.error({ argument, parameter, identifier: LanguageKeys.Commands.Social.AutoRoleInvalidLevel });
+		}
+
+		const level = Number(parameter.slice(0, -1));
+		if (Number.isNaN(level)) {
+			return Args.error({ argument, parameter, identifier: LanguageKeys.Commands.Social.AutoRoleInvalidLevel });
+		}
+
+		// There cannot be a level below 0
+		if (level <= 0) {
+			return Args.error({ argument, parameter, identifier: LanguageKeys.Commands.Social.AutoRoleInvalidNegativeOrZeroLevel });
+		}
+
+		const points = Math.floor((level / 0.2) ** 2);
+		if (points < 1) {
+			return Args.error({
+				argument,
+				parameter,
+				identifier: LanguageKeys.Commands.Social.AutoRoleTooLow,
+				context: { minimum: 1, maximum: this.maximumPoints, points }
+			});
+		}
+
+		if (points > this.maximumPoints) {
+			return Args.error({
+				argument,
+				parameter,
+				identifier: LanguageKeys.Commands.Social.AutoRoleTooHigh,
+				context: { minimum: 1, maximum: this.maximumPoints, points }
+			});
+		}
+
+		return Args.ok(points);
+	});
+}
---
+++ src/commands/Levelling/Management/social.ts
@@ -0,0 +1,99 @@
+import { MemberEntity } from '#lib/database';
+import { LanguageKeys } from '#lib/i18n/languageKeys';
+import { SkyraCommand } from '#lib/structures';
+import type { GuildMessage } from '#lib/types';
+import { PermissionLevels } from '#lib/types/Enums';
+import { ApplyOptions } from '@sapphire/decorators';
+
+@ApplyOptions<SkyraCommand.Options>({
+	bucket: 2,
+	cooldown: 10,
+	description: LanguageKeys.Commands.Social.SocialDescription,
+	extendedHelp: LanguageKeys.Commands.Social.SocialExtended,
+	permissionLevel: PermissionLevels.Administrator,
+	runIn: ['text', 'news'],
+	subCommands: ['add', 'remove', 'set', 'reset']
+})
+export class UserCommand extends SkyraCommand {
+	public async add(message: GuildMessage, args: SkyraCommand.Args) {
+		const user = await args.pick('userName');
+		const amount = await args.pick('integer', { minimum: 1, maximum: 1000000 });
+
+		const { members } = this.context.db;
+		const settings = await members.findOne({ where: { userID: user.id, guildID: message.guild.id } });
+		if (settings) {
+			const newAmount = settings.points + amount;
+			settings.points = newAmount;
+			await settings.save();
+
+			return message.send(args.t(LanguageKeys.Commands.Social.SocialAdd, { user: user.username, amount: newAmount, count: amount }));
+		}
+
+		const created = new MemberEntity();
+		created.userID = user.id;
+		created.guildID = message.guild.id;
+		created.points = amount;
+		await members.insert(created);
+
+		return message.send(args.t(LanguageKeys.Commands.Social.SocialAdd, { user: user.username, amount, count: amount }));
+	}
+
+	public async remove(message: GuildMessage, args: SkyraCommand.Args) {
+		const user = await args.pick('userName');
+		const amount = await args.pick('integer', { minimum: 1, maximum: 100000000 });
+
+		const { members } = this.context.db;
+		const settings = await members.findOne({ where: { userID: user.id, guildID: message.guild.id } });
+		if (!settings) this.error(LanguageKeys.Commands.Social.SocialMemberNotExists);
+
+		const newAmount = Math.max(settings.points - amount, 0);
+		settings.points = newAmount;
+		await settings.save();
+
+		return message.send(args.t(LanguageKeys.Commands.Social.SocialRemove, { user: user.username, amount: newAmount, count: amount }));
+	}
+
+	public async set(message: GuildMessage, args: SkyraCommand.Args) {
+		const user = await args.pick('userName');
+		const amount = await args.pick('integer', { minimum: 0, maximum: 1000000 });
+
+		const { members } = this.context.db;
+		const settings = await members.findOne({ where: { userID: user.id, guildID: message.guild.id } });
+		let oldValue = 0;
+		if (settings) {
+			oldValue = settings.points;
+			settings.points = amount;
+			await settings.save();
+		} else {
+			const created = new MemberEntity();
+			created.userID = user.id;
+			created.guildID = message.guild.id;
+			created.points = amount;
+			await members.insert(created);
+		}
+
+		const variation = amount - oldValue;
+		if (variation === 0) return args.t(LanguageKeys.Commands.Social.SocialUnchanged, { user: user.username });
+
+		return message.send(
+			variation > 0
+				? args.t(LanguageKeys.Commands.Social.SocialAdd, {
+						user: user.username,
+						amount,
+						count: variation
+				  })
+				: args.t(LanguageKeys.Commands.Social.SocialRemove, {
+						user: user.username,
+						amount,
+						count: -variation
+				  })
+		);
+	}
+
+	public async reset(message: GuildMessage, args: SkyraCommand.Args) {
+		const user = await args.pick('userName');
+		const { members } = this.context.db;
+		await members.delete({ userID: user.id, guildID: message.guild.id });
+		return message.send(args.t(LanguageKeys.Commands.Social.SocialReset, { user: user.username }));
+	}
+}

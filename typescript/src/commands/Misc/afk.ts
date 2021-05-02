cb5b541d39af086661dcd3067185e2a5ab572320
Antonio Román <kyradiscord@gmail.com>
2021-05-02 18:28:52 +0200

    feat(afk): add a bunch of control sub-commands (#1821)

--- typescript/src/commands/Misc/afk.ts
+++ typescript/src/commands/Misc/afk.ts
@@ -1,20 +1,95 @@
 import { envParseBoolean } from '#lib/env';
 import { LanguageKeys } from '#lib/i18n/languageKeys';
-import { SkyraCommand } from '#lib/structures';
+import { SkyraCommand, UserPaginatedMessage } from '#lib/structures';
 import { GuildMessage } from '#lib/types';
+import { PermissionLevels } from '#lib/types/Enums';
+import { LongWidthSpace } from '#utils/constants';
+import { requiresLevel } from '#utils/decorators';
 import { ApplyOptions } from '@sapphire/decorators';
+import { Args } from '@sapphire/framework';
 import { Time } from '@sapphire/time-utilities';
-import { GuildMember, Permissions } from 'discord.js';
+import { chunk } from '@sapphire/utilities';
+import { GuildMember, MessageEmbed, Permissions } from 'discord.js';
 
 @ApplyOptions<SkyraCommand.Options>({
 	enabled: envParseBoolean('REDIS_ENABLED'),
 	cooldown: 20,
 	description: LanguageKeys.Commands.Misc.AfkDescription,
 	extendedHelp: LanguageKeys.Commands.Misc.AfkExtended,
-	runIn: ['text', 'news']
+	runIn: ['text', 'news'],
+	subCommands: ['ignore', 'reset', 'clear', 'list', 'show', { input: 'set', default: true }]
 })
 export class UserCommand extends SkyraCommand {
-	public async run(message: GuildMessage, args: SkyraCommand.Args) {
+	public async ignore(message: GuildMessage, args: SkyraCommand.Args) {
+		const key = this.getKey(message.member);
+		const raw = await this.context.afk.get(key);
+		if (raw === null) this.error(LanguageKeys.Commands.Misc.AfkNotSetSelf);
+
+		const channel = args.finished ? message.channel : await args.pick('textOrNewsChannelName');
+		const value = JSON.parse(raw) as AfkEntry;
+		const index = (value.channels ??= []).indexOf(channel.id);
+		if (index === -1) {
+			value.channels.push(channel.id);
+			await this.insert(key, value);
+			return message.send(args.t(LanguageKeys.Commands.Misc.AfkAddedIgnoredChannel, { channel: channel.toString() }));
+		}
+
+		value.channels.splice(index, 1);
+		await this.insert(key, value);
+		return message.send(args.t(LanguageKeys.Commands.Misc.AfkRemovedIgnoredChannel, { channel: channel.toString() }));
+	}
+
+	public reset(message: GuildMessage, args: SkyraCommand.Args) {
+		return args.finished ? this.resetSelf(message, args) : this.resetUser(message, args);
+	}
+
+	public async clear(message: GuildMessage, args: SkyraCommand.Args) {
+		if (args.finished) return this.clearSelf(message, args);
+
+		const allResult = await args.pickResult(UserCommand.all);
+		return allResult.success ? this.clearAll(message, args) : this.clearUser(message, args);
+	}
+
+	@requiresLevel(PermissionLevels.Moderator, async (_: GuildMessage, args: SkyraCommand.Args) => {
+		throw args.t(LanguageKeys.Commands.Misc.AfkPermissionLevelList);
+	})
+	public async list(message: GuildMessage, args: SkyraCommand.Args) {
+		const entries = await this.fetchEntries(this.getTemplate(message.member), false);
+		if (entries.size === 0) this.error(LanguageKeys.Commands.Misc.AfkNoEntries);
+
+		const entriesPerPage = 20;
+		const display = new UserPaginatedMessage({
+			template: new MessageEmbed()
+				.setAuthor(message.guild.name, message.guild.iconURL({ size: 128, format: 'png', dynamic: true }) ?? undefined)
+				.setTitle(args.t(LanguageKeys.Commands.Misc.AfkListTitle))
+				.setColor(await this.context.db.fetchColor(message))
+				.setFooter(` - ${args.t(LanguageKeys.Commands.Misc.AfkListFooter, { count: entries.size })}`)
+				.setTimestamp()
+		});
+
+		let i = 0;
+		for (const page of chunk([...entries.entries()], entriesPerPage)) {
+			display.addPageEmbed((embed) => embed.setDescription(this.generatePage(page, i)));
+			i += entriesPerPage;
+		}
+
+		return display.start(message);
+	}
+
+	public async show(message: GuildMessage, args: SkyraCommand.Args) {
+		const member = args.finished ? message.member : await args.pick('member');
+		const [, entry] = await this.fetchEntry(member, message.member === member);
+		return message.send(
+			new MessageEmbed()
+				.setAuthor(`${entry.name} (${member.id})`, member.user.displayAvatarURL({ size: 128, dynamic: true, format: 'png' }))
+				.setColor(await this.context.db.fetchColor(message))
+				.setDescription(entry.content)
+				.setFooter(args.t(LanguageKeys.Commands.Misc.AfkShowFooter))
+				.setTimestamp()
+		);
+	}
+
+	public async set(message: GuildMessage, args: SkyraCommand.Args) {
 		const content = args.finished ? args.t(LanguageKeys.Commands.Misc.AfkDefault) : await args.rest('string', { maximum: 100 });
 		const name = this.removeAfkPrefix(message.member.displayName, args);
 
@@ -42,18 +117,205 @@ export class UserCommand extends SkyraCommand {
 	}
 
 	private async saveAfkMessage(member: GuildMember, name: string, content: string) {
-		const entry: AfkEntry = { time: Date.now(), name, content };
-		await this.context.afk.psetex(`afk:${member.guild.id}:${member.id}`, Time.Day, JSON.stringify(entry));
+		await this.insert(this.getKey(member), { time: Date.now(), name, content });
 	}
 
 	private removeAfkPrefix(name: string, args: SkyraCommand.Args) {
 		const prefix = args.t(LanguageKeys.Commands.Misc.AfkPrefix);
 		return name.startsWith(prefix) ? name.slice(prefix.length + 1) : name;
 	}
+
+	private async resetSelf(message: GuildMessage, args: SkyraCommand.Args) {
+		await this.handleResetAfk(args, message.member, true);
+		return message.send(args.t(LanguageKeys.Commands.Misc.AfkResetSelf));
+	}
+
+	@requiresLevel(PermissionLevels.Moderator, async (_: GuildMessage, args: SkyraCommand.Args) => {
+		throw args.t(LanguageKeys.Commands.Misc.AfkPermissionLevelResetUser);
+	})
+	private async resetUser(message: GuildMessage, args: SkyraCommand.Args) {
+		const member = await args.pick('member');
+		if (member.id === message.member.id) return this.resetSelf(message, args);
+
+		await this.handleResetAfk(args, member, false);
+		return message.send(args.t(LanguageKeys.Commands.Misc.AfkReset, { user: member.toString() }), { allowedMentions: { roles: [], users: [] } });
+	}
+
+	private async handleResetAfk(args: SkyraCommand.Args, member: GuildMember, targetIsSelf: boolean) {
+		const key = this.getKey(member);
+		const raw = await this.context.afk.get(key);
+		if (raw === null) {
+			this.error(targetIsSelf ? LanguageKeys.Commands.Misc.AfkNotSetSelf : LanguageKeys.Commands.Misc.AfkNotSet, { user: member.toString() });
+		}
+
+		return this.handleResetAfkWithEntry(args, key, JSON.parse(raw));
+	}
+
+	private async handleResetAfkWithEntry(args: SkyraCommand.Args, key: string, value: AfkEntry) {
+		value.content = args.t(LanguageKeys.Commands.Misc.AfkDefault);
+		await this.insert(key, value);
+	}
+
+	private async clearSelf(message: GuildMessage, args: SkyraCommand.Args) {
+		await this.handleClearAfk(message.member, true);
+		return message.send(args.t(LanguageKeys.Commands.Misc.AfkClearSelf));
+	}
+
+	@requiresLevel(PermissionLevels.Moderator, async (_: GuildMessage, args: SkyraCommand.Args) => {
+		throw args.t(LanguageKeys.Commands.Misc.AfkPermissionLevelClearUser);
+	})
+	private async clearUser(message: GuildMessage, args: SkyraCommand.Args) {
+		const member = await args.pick('member');
+		if (member.id === message.member.id) return this.clearSelf(message, args);
+
+		await this.handleClearAfk(member, false);
+		return message.send(args.t(LanguageKeys.Commands.Misc.AfkClear, { user: member.toString() }), { allowedMentions: { roles: [], users: [] } });
+	}
+
+	@requiresLevel(PermissionLevels.Moderator, async (_: GuildMessage, args: SkyraCommand.Args) => {
+		throw args.t(LanguageKeys.Commands.Misc.AfkPermissionLevelClearAll);
+	})
+	private async clearAll(message: GuildMessage, args: SkyraCommand.Args) {
+		const template = this.getTemplate(message.member);
+		const entries = await this.fetchEntries(template);
+		if (entries.size === 0) this.error(LanguageKeys.Commands.Misc.AfkNoEntries);
+
+		await message.send(args.t(LanguageKeys.Commands.Misc.AfkClearAllStarting, { count: entries.size }));
+		let i = 0;
+		let failed = 0;
+
+		const idOffset = template.length - 1;
+		const members = message.guild.members.cache;
+		for (const [id, value] of entries) {
+			const member = members.get(id.slice(idOffset));
+			if (member === undefined) await this.context.afk.del(id);
+			else await this.handleClearAfkWithEntry(member, id, value).catch(() => ++failed);
+
+			if (++i % 10 === 0) {
+				const percentage = (i / entries.size) * 100;
+				await message.send(args.t(LanguageKeys.Commands.Misc.AfkClearAllProgress, { count: i, percentage: Math.round(percentage) }));
+			}
+		}
+
+		const totalLine = i === failed ? '' : args.t(LanguageKeys.Commands.Misc.AfkClearAllTotal, { count: i });
+		const failedLine = failed === 0 ? '' : `\n${args.t(LanguageKeys.Commands.Misc.AfkClearAllFailed, { count: failed })}`;
+
+		// We're done!
+		const embed = new MessageEmbed()
+			.setColor(await this.context.db.fetchColor(message))
+			.setTitle(args.t(LanguageKeys.Commands.Misc.AfkClearAllTitle))
+			.setDescription(totalLine + failedLine);
+		return message.send(null, embed);
+	}
+
+	private async handleClearAfk(member: GuildMember, targetIsSelf: boolean) {
+		const key = this.getKey(member);
+		const raw = await this.context.afk.get(key);
+		if (raw === null) {
+			this.error(targetIsSelf ? LanguageKeys.Commands.Misc.AfkNotSetSelf : LanguageKeys.Commands.Misc.AfkNotSet, { user: member.toString() });
+		}
+
+		return this.handleClearAfkWithEntry(member, key, JSON.parse(raw));
+	}
+
+	private async handleClearAfkWithEntry(member: GuildMember, key: string, value: AfkEntry) {
+		await this.context.afk.del(key);
+		await this.handleNicknameChange(member, value.name);
+	}
+
+	private async handleNicknameChange(member: GuildMember, name: string) {
+		// If the target member is the guild's owner, return:
+		if (member.isGuildOwner()) return;
+
+		const me = member.guild.me!;
+
+		// If Skyra does not have permissions to manage nicknames, return:
+		if (!me.permissions.has(Permissions.FLAGS.MANAGE_NICKNAMES)) return;
+
+		// If the target member has higher role hierarchy than Skyra, return:
+		if (member.roles.highest.position >= me.roles.highest.position) return;
+
+		await member.setNickname(name);
+	}
+
+	private generatePage(entries: [string, AfkEntry][], index: number) {
+		const lines: string[] = [];
+		for (const [id, value] of entries) {
+			lines.push(`❯ \`${++index}\`: **${value.name}** (\`${id}\`)`, `${LongWidthSpace}└─ ${value.content}`);
+		}
+
+		return lines.join('\n');
+	}
+
+	private async fetchEntry(member: GuildMember, targetIsSelf: boolean): Promise<[string, AfkEntry]> {
+		const key = this.getKey(member);
+		const raw = await this.context.afk.get(key);
+		if (raw === null) {
+			this.error(targetIsSelf ? LanguageKeys.Commands.Misc.AfkNotSetSelf : LanguageKeys.Commands.Misc.AfkNotSet, { user: member.toString() });
+		}
+
+		return [key, JSON.parse(raw)];
+	}
+
+	private async fetchEntries(template: string, fullKeys = true) {
+		const values = new Map<string, AfkEntry>();
+
+		const keys = await this.fetchKeys(template);
+		if (keys.length === 0) return values;
+
+		const rawValues = await this.context.afk.mget(...keys);
+
+		const keyIterator = keys.values();
+		const offset = fullKeys ? 0 : template.length - 1;
+		for (const rawValue of rawValues) {
+			const id = keyIterator.next().value as string;
+			if (rawValue !== null) values.set(offset === 0 ? id : id.slice(offset), JSON.parse(rawValue));
+		}
+
+		return values;
+	}
+
+	private async fetchKeys(template: string) {
+		const users = new Set<string>();
+		const cache = this.context.afk;
+
+		let cursor = '0';
+		do {
+			// `scan` returns a tuple with the next cursor (which must be used for the
+			// next iteration) and an array of the matching keys. The iterations end when
+			// cursor becomes '0' again.
+			const response = await cache.scan(cursor, 'MATCH', template);
+			[cursor] = response;
+
+			for (const key of response[1]) {
+				users.add(key);
+			}
+		} while (cursor !== '0');
+
+		return [...users];
+	}
+
+	private getTemplate(member: GuildMember) {
+		return `afk:${member.guild.id}:*`;
+	}
+
+	private getKey(member: GuildMember) {
+		return `afk:${member.guild.id}:${member.id}`;
+	}
+
+	private insert(key: string, value: AfkEntry) {
+		return this.context.afk.psetex(key, Time.Day, JSON.stringify(value));
+	}
+
+	private static readonly all = Args.make<true>(async (parameter, { argument, args }) => {
+		if (parameter.toLowerCase() === args.t(LanguageKeys.Arguments.All)) return Args.ok(true);
+		return Args.error({ argument, parameter });
+	});
 }
 
 interface AfkEntry {
 	time: number;
 	name: string;
 	content: string;
+	channels?: string[];
 }

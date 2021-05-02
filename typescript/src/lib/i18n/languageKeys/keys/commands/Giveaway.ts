ee95fcea30c7b0775008e7480661e310736ff6ff
Antonio Román <kyradiscord@gmail.com>
2021-05-02 20:56:16 +0200

    feat(giveaway): added giveaway-end command

--- typescript/src/lib/i18n/languageKeys/keys/commands/Giveaway.ts
+++ typescript/src/lib/i18n/languageKeys/keys/commands/Giveaway.ts
@@ -1,10 +1,15 @@
 import type { LanguageHelpDisplayOptions } from '#lib/i18n/LanguageHelp';
-import { T } from '#lib/types';
+import { FT, T } from '#lib/types';
 
-export const GiveawayDescription = T<string>('commands/giveaway:giveawayDescription');
+export const GiveawayDescription = T('commands/giveaway:giveawayDescription');
 export const GiveawayExtended = T<LanguageHelpDisplayOptions>('commands/giveaway:giveawayExtended');
-export const GiveawayRerollDescription = T<string>('commands/giveaway:giveawayRerollDescription');
+export const GiveawayRerollDescription = T('commands/giveaway:giveawayRerollDescription');
 export const GiveawayRerollExtended = T<LanguageHelpDisplayOptions>('commands/giveaway:giveawayRerollExtended');
-export const GiveawayScheduleDescription = T<string>('commands/giveaway:giveawayScheduleDescription');
+export const GiveawayScheduleDescription = T('commands/giveaway:giveawayScheduleDescription');
 export const GiveawayScheduleExtended = T<LanguageHelpDisplayOptions>('commands/giveaway:giveawayScheduleExtended');
-export const GiveawayRerollInvalid = T<string>('commands/giveaway:giveawayRerollInvalid');
+export const GiveawayRerollInvalid = T('commands/giveaway:giveawayRerollInvalid');
+export const GiveawayEndDescription = T('commands/giveaway:giveawayEndDescription');
+export const GiveawayEndExtended = T<LanguageHelpDisplayOptions>('commands/giveaway:giveawayEndExtended');
+export const GiveawayEndMessageNotMine = T('commands/giveaway:giveawayEndMessageNotMine');
+export const GiveawayEndMessageNotActive = T('commands/giveaway:giveawayEndMessageNotActive');
+export const GiveawayEnd = FT<{ url: string }>('commands/giveaway:giveawayEnd');

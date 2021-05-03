4032cb1a9911901a4239dc772b25ee367abc9170
Antonio Román <kyradiscord@gmail.com>
2021-05-03 19:06:45 +0200

    fix(cad): use timespan

--- typescript/src/commands/Management/Configuration/manage-command-auto-delete.ts
+++ typescript/src/commands/Management/Configuration/manage-command-auto-delete.ts
@@ -4,6 +4,7 @@ import { SkyraCommand } from '#lib/structures';
 import type { GuildMessage } from '#lib/types';
 import { PermissionLevels } from '#lib/types/Enums';
 import { ApplyOptions } from '@sapphire/decorators';
+import { Time } from '@sapphire/time-utilities';
 import { codeBlock } from '@sapphire/utilities';
 import type { TextChannel } from 'discord.js';
 
@@ -20,14 +21,15 @@ import type { TextChannel } from 'discord.js';
 export class UserCommand extends SkyraCommand {
 	public async add(message: GuildMessage, args: SkyraCommand.Args) {
 		const channel = await args.pick('textChannelName');
-		const duration = await args.pick('integer', { minimum: 1, maximum: 120 });
+		const time = await args.pick('timespan', { minimum: Time.Second, maximum: Time.Minute * 2 });
+		const duration = Math.round(time / Time.Second);
 		await message.guild.writeSettings((settings) => {
-			const commandAutodelete = settings[GuildSettings.CommandAutoDelete];
-			const index = commandAutodelete.findIndex(([id]) => id === channel.id);
+			const commandAutoDelete = settings[GuildSettings.CommandAutoDelete];
+			const index = commandAutoDelete.findIndex(([id]) => id === channel.id);
 			const value: readonly [string, number] = [channel.id, duration];
 
-			if (index === -1) commandAutodelete.push(value);
-			else commandAutodelete[index] = value;
+			if (index === -1) commandAutoDelete.push(value);
+			else commandAutoDelete[index] = value;
 		});
 
 		return message.send(args.t(LanguageKeys.Commands.Management.ManageCommandAutoDeleteAdd, { channel: channel.toString(), time: duration }));
@@ -36,14 +38,14 @@ export class UserCommand extends SkyraCommand {
 	public async remove(message: GuildMessage, args: SkyraCommand.Args) {
 		const channel = await args.pick('textChannelName');
 		await message.guild.writeSettings((settings) => {
-			const commandAutodelete = settings[GuildSettings.CommandAutoDelete];
-			const index = commandAutodelete.findIndex(([id]) => id === channel.id);
+			const commandAutoDelete = settings[GuildSettings.CommandAutoDelete];
+			const index = commandAutoDelete.findIndex(([id]) => id === channel.id);
 
 			if (index === -1) {
 				this.error(LanguageKeys.Commands.Management.ManageCommandAutoDeleteRemoveNotSet, { channel: channel.toString() });
 			}
 
-			commandAutodelete.splice(index, 1);
+			commandAutoDelete.splice(index, 1);
 		});
 
 		return message.send(args.t(LanguageKeys.Commands.Management.ManageCommandAutoDeleteRemove, { channel: channel.toString() }));
@@ -61,7 +63,7 @@ export class UserCommand extends SkyraCommand {
 		const list: string[] = [];
 		for (const entry of commandAutoDelete) {
 			const channel = this.context.client.channels.cache.get(entry[0]) as TextChannel;
-			if (channel) list.push(`${channel.name.padEnd(26)} :: ${args.t(LanguageKeys.Globals.DurationValue, { value: entry[1] / 60000 })}`);
+			if (channel) list.push(`${channel.name.padEnd(26)} :: ${args.t(LanguageKeys.Globals.DurationValue, { value: entry[1] * Time.Second })}`);
 		}
 		if (!list.length) this.error(LanguageKeys.Commands.Management.ManageCommandAutoDeleteShowEmpty);
 		return message.send(

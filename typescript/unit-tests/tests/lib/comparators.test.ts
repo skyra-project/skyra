74f22de3ac3bf5c1cf1b1bf33f0dd61cb6bd951a
Jeroen Claassens <support@favware.tech>
2021-06-19 14:02:39 +0200

    chore: don’t even try to refactor it.

--- typescript/unit-tests/tests/lib/comparators.test.ts
+++ typescript/unit-tests/tests/lib/comparators.test.ts
@@ -1,64 +1,6 @@
-import { andMix, hasAtLeastOneKeyInMap, isNullishOrEmpty, isNullishOrZero } from '#utils/comparators';
+import { andMix } from '#utils/comparators';
 
 describe('comparators', () => {
-	describe('isNullishOrZero', () => {
-		test('GIVEN undefined THEN passes test', () => {
-			expect(isNullishOrZero(undefined)).toBe(true);
-		});
-
-		test('GIVEN null THEN passes test', () => {
-			expect(isNullishOrZero(null)).toBe(true);
-		});
-
-		test('GIVEN 0 THEN passes test', () => {
-			expect(isNullishOrZero(0)).toBe(true);
-		});
-
-		test('GIVEN "" THEN does not pass test', () => {
-			expect(isNullishOrZero('')).toBe(false);
-		});
-	});
-
-	describe('isNullishOrEmpty', () => {
-		test('GIVEN undefined THEN passes test', () => {
-			expect(isNullishOrEmpty(undefined)).toBe(true);
-		});
-
-		test('GIVEN null THEN passes test', () => {
-			expect(isNullishOrEmpty(null)).toBe(true);
-		});
-
-		test('GIVEN 0 THEN does not pass test', () => {
-			expect(isNullishOrEmpty(0)).toBe(false);
-		});
-
-		test('GIVEN "" THEN does passes test', () => {
-			expect(isNullishOrEmpty('')).toBe(true);
-		});
-	});
-
-	describe('hasAtLeastOneKeyInMap', () => {
-		test('GIVEN empty map and empty array THEN does not pass test', () => {
-			expect(hasAtLeastOneKeyInMap(new Map<string, string>([]), [])).toEqual(false);
-		});
-
-		test('GIVEN filled map and empty array THEN does not pass test', () => {
-			expect(hasAtLeastOneKeyInMap(new Map<string, string>([['foo', 'bar']]), [])).toEqual(false);
-		});
-
-		test('GIVEN empty map and filled array THEN does not pass test', () => {
-			expect(hasAtLeastOneKeyInMap(new Map<string, string>([]), ['foo'])).toEqual(false);
-		});
-
-		test('GIVEN filled map and filled array with at least one common key THEN passes test', () => {
-			expect(hasAtLeastOneKeyInMap(new Map<string, string>([['foo', 'bar']]), ['world', 'baz', 'foo'])).toEqual(true);
-		});
-
-		test('GIVEN filled map and filled array with no common keys THEN fails test', () => {
-			expect(hasAtLeastOneKeyInMap(new Map<string, string>([['foo', 'bar']]), ['world', 'baz'])).toEqual(false);
-		});
-	});
-
 	describe('andMix', () => {
 		test('GIVEN two callbacks AND passing value THEN passes test', () => {
 			const a = jest.fn((v: number) => v > 0);

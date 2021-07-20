8070bdd2c4d4210ac1763fd6e879586907c9e883
Tyler Davis <tgfdavis98@gmail.com>
2021-07-20 07:09:05 +0100

    Update RequestCacheTests.cs

--- services/Skyra.UnitTests/Notifications/RequestCacheTests.cs
+++ services/Skyra.UnitTests/Notifications/RequestCacheTests.cs
@@ -59,7 +59,7 @@ namespace Skyra.UnitTests.Notifications
 			cache.AddRequest(id, isSubscription);
 			var _ = cache.GetRequest(id, isSubscription, false);
 			var result = cache.GetRequest(id, isSubscription);
-			
+
 			// assert
 
 			Assert.IsTrue(result);

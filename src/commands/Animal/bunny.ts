012a19642c1982b4d662d294ac77cd537fd172df
Jeroen Claassens <support@favware.tech>
2021-08-23 15:15:01 +0200

    feat: add bunny command
    They're so cuuuteeee 🐰🐰

---
+++ src/commands/Animal/bunny.ts
@@ -0,0 +1,60 @@
+import { LanguageKeys } from '#lib/i18n/languageKeys';
+import { SkyraCommand } from '#lib/structures';
+import { getImageUrl } from '#utils/util';
+import { ApplyOptions } from '@sapphire/decorators';
+import { fetch, FetchResultTypes } from '@sapphire/fetch';
+import { send } from '@skyra/editable-commands';
+import { Message, MessageEmbed } from 'discord.js';
+import { URL } from 'url';
+import { isNullishOrEmpty } from '@sapphire/utilities';
+
+const url = new URL('https://api.bunnies.io/v2/loop/random/?media=gif,png');
+
+@ApplyOptions<SkyraCommand.Options>({
+	aliases: ['bunbun', 'rabbit'],
+	description: LanguageKeys.Commands.Animal.BunnyDescription,
+	extendedHelp: LanguageKeys.Commands.Animal.BunnyExtended,
+	requiredClientPermissions: ['EMBED_LINKS'],
+	spam: true
+})
+export class UserCommand extends SkyraCommand {
+	public async run(message: Message, args: SkyraCommand.Args) {
+		const {
+			media: { gif },
+			source: bunnySource
+		} = await fetch<BunnyResultOk>(url, FetchResultTypes.JSON);
+
+		const imageUrl = getImageUrl(gif) ?? 'https://i.imgur.com/FnAPcxj.jpg';
+		const translations = args.t(LanguageKeys.Commands.Animal.BunnyEmbedData);
+		const source = this.getSource(bunnySource);
+
+		const embed = new MessageEmbed()
+			.setURL(imageUrl)
+			.setTitle(translations.title)
+			.setColor(await this.container.db.fetchColor(message))
+			.setImage(imageUrl)
+			.setTimestamp();
+
+		if (source) {
+			embed.setDescription(`[${translations.source}](${source})`);
+		}
+
+		return send(message, { embeds: [embed] });
+	}
+
+	private getSource(bunnySource: string): string | null {
+		if (isNullishOrEmpty(bunnySource) || bunnySource.toLowerCase() === 'unknown') return null;
+		return bunnySource;
+	}
+}
+
+interface BunnyResultOk {
+	id: string;
+	media: {
+		gif: string;
+		poster: string;
+	};
+	source: string;
+	thisServed: number;
+	totalServed: number;
+}
--- src/lib/i18n/languageKeys/keys/commands/Animal.ts
+++ src/lib/i18n/languageKeys/keys/commands/Animal.ts
@@ -1,14 +1,17 @@
 import type { LanguageHelpDisplayOptions } from '#lib/i18n/LanguageHelp';
 import { T } from '#lib/types';
 
-export const CatfactDescription = T<string>('commands/animal:catfactDescription');
+export const BunnyDescription = T('commands/animal:bunnyDescription');
+export const BunnyExtended = T<LanguageHelpDisplayOptions>('commands/animal:bunnyExtended');
+export const BunnyEmbedData = T<{ title: string; source: string }>('commands/animal:bunnyEmbedData');
+export const CatfactDescription = T('commands/animal:catfactDescription');
 export const CatfactExtended = T<LanguageHelpDisplayOptions>('commands/animal:catfactExtended');
-export const CatfactTitle = T<string>('commands/animal:catfactTitle');
-export const DogDescription = T<string>('commands/animal:dogDescription');
+export const CatfactTitle = T('commands/animal:catfactTitle');
+export const DogDescription = T('commands/animal:dogDescription');
 export const DogExtended = T<LanguageHelpDisplayOptions>('commands/animal:dogExtended');
-export const FoxDescription = T<string>('commands/animal:foxDescription');
+export const FoxDescription = T('commands/animal:foxDescription');
 export const FoxExtended = T<LanguageHelpDisplayOptions>('commands/animal:foxExtended');
-export const KittyDescription = T<string>('commands/animal:kittyDescription');
+export const KittyDescription = T('commands/animal:kittyDescription');
 export const KittyExtended = T<LanguageHelpDisplayOptions>('commands/animal:kittyExtended');
-export const ShibeDescription = T<string>('commands/animal:shibeDescription');
+export const ShibeDescription = T('commands/animal:shibeDescription');
 export const ShibeExtended = T<LanguageHelpDisplayOptions>('commands/animal:shibeExtended');

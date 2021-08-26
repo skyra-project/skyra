580a5d09a231ba4a53948364bbad9bd9e3991988
Antonio Román <kyradiscord@gmail.com>
2021-08-26 18:56:49 +0200

    chore: committing fixes in the dark, seriously, who killed my power!?

--- src/lib/types/shims/stream/consumers.d.ts
+++ src/lib/types/shims/stream/consumers.d.ts
@@ -1,4 +1,5 @@
 // TODO: Remove when @types/node ships those (alongside 3 more methods):
+// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/55311
 
 declare module 'stream/consumers' {
 	export function buffer(stream: NodeJS.ReadableStream): Buffer;

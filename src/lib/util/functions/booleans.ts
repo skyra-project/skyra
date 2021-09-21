a80a660ef21f32e7e5144bb9566e97ed3ad38d87
Jeroen Claassens <support@favware.tech>
2021-09-21 22:58:29 +0200

    chore: don’t mess with voodoo
    permalink: http://whatthecommit.com/366d70ea2b6dec316cd3e8b06624d0a6

--- src/lib/util/functions/booleans.ts
+++ src/lib/util/functions/booleans.ts
@@ -3,7 +3,5 @@ import { resolveT, TResolvable } from '#lib/i18n/translate';
 
 export function formatBoolean(t: TResolvable, value: boolean): string {
 	const tFunction = resolveT(t);
-
-	if (value) return tFunction(LanguageKeys.Globals.Yes);
-	return tFunction(LanguageKeys.Globals.No);
+	return tFunction(value ? LanguageKeys.Globals.Yes : LanguageKeys.Globals.No);
 }

2fb5fde20a30f6813cd9d92cbf731069f31f89ad
Jeroen Claassens <support@favware.tech>
2021-09-23 21:07:39 +0200

    refactor: remove `subcommand` analytics tag

--- src/listeners/analytics/commandUsageAnalytics.ts
+++ src/listeners/analytics/commandUsageAnalytics.ts
@@ -6,11 +6,10 @@ import { ApplyOptions } from '@sapphire/decorators';
 
 @ApplyOptions<AnalyticsListener.Options>({ event: Events.CommandUsageAnalytics })
 export class UserAnalyticsEvent extends AnalyticsListener {
-	public run(commandName: string, category: string, subCategory: string) {
+	public run(commandName: string, category: string) {
 		const command = new Point(Points.Commands)
 			.tag(Tags.Action, Actions.Addition)
 			.tag(CommandCategoryTypes.Category, category)
-			.tag(CommandCategoryTypes.SubCategory, subCategory)
 			.intField(commandName.replace(/^time$/, 'case-time'), 1);
 
 		return this.writePoint(command);

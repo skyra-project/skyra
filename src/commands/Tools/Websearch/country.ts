9e214df225c7d1a6abcc7603b7d34802933bb351
Jeroen Claassens <support@favware.tech>
2021-10-01 02:32:37 +0200

    chore: lol!
    permalink: http://whatthecommit.com/eb46f473d426fbf019df4a495c476697

--- src/commands/Tools/Websearch/country.ts
+++ src/commands/Tools/Websearch/country.ts
@@ -16,10 +16,7 @@ const mapCurrency = (currency: CurrencyData) => `${currency.name} (${currency.sy
 @ApplyOptions<PaginatedMessageCommand.Options>({
 	description: LanguageKeys.Commands.Tools.CountryDescription,
 	detailedDescription: LanguageKeys.Commands.Tools.CountryExtended,
-	enabled: false,
-	hidden: true,
-	guarded: true,
-	permissionLevel: PermissionLevels.BotOwner
+	enabled: false
 })
 export class UserPaginatedMessageCommand extends PaginatedMessageCommand {
 	public async run(message: GuildMessage, args: PaginatedMessageCommand.Args) {

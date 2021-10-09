99f390766dafaac25bcc54d6b08ec30ac9b452e1
Jeroen Claassens <support@favware.tech>
2021-10-09 23:58:10 +0200

    chore: /sigh
    permalink: http://whatthecommit.com/3f874760c0ae34ebeff1fee8f2f60e6f

--- tests/lib/util/functions/emojis/isValidSerializedEmoji.test.ts
+++ tests/lib/util/functions/emojis/isValidSerializedEmoji.test.ts
@@ -1,4 +1,4 @@
-import { encodedBunnyTwemoji, serializedAnimatedSkyraGlasses, serializedStaticSkyra } from '#mocks/constants';
+import { animatedSkyraGlasses, encodedBunnyTwemoji, serializedAnimatedSkyraGlasses, serializedStaticSkyra, staticSkyra } from '#mocks/constants';
 import { isValidSerializedEmoji } from '#utils/functions';
 
 describe('isValidSerializedEmoji', () => {
@@ -12,5 +12,15 @@ describe('isValidSerializedEmoji', () => {
 
 	test('GIVEN custom serialized animated emoji THEN returns true', () => {
 		expect(isValidSerializedEmoji(serializedAnimatedSkyraGlasses)).toBe(true);
+	});
+
+	test('GIVEN custom static emoji THEN returns false', () => {
+		// @ts-expect-error intentionally incorrect type
+		expect(isValidSerializedEmoji(staticSkyra)).toBe(false);
+	});
+
+	test('GIVEN custom animated emoji THEN returns false', () => {
+		// @ts-expect-error intentionally incorrect type
+		expect(isValidSerializedEmoji(animatedSkyraGlasses)).toBe(false);
 	});
 });

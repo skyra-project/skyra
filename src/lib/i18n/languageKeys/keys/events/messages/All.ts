e064ed5b2f6195b2f0c46955c81982f4f361326c
Antonio Román <kyradiscord@gmail.com>
2022-01-03 14:37:29 +0100

    fix: resolved message-update logs not showing channel

--- src/lib/i18n/languageKeys/keys/events/messages/All.ts
+++ src/lib/i18n/languageKeys/keys/events/messages/All.ts
@@ -1,7 +1,7 @@
 import { FT } from '#lib/types';
-import type { Message, TextChannel } from 'discord.js';
+import type { GuildTextBasedChannel } from 'discord.js';
 
 export const AfkRemove = FT<{ user: string }>('events/messages:afkRemove');
 export const AfkStatus = FT<{ user: string; duration: number; content: string }>('events/messages:afkStatus');
-export const MessageUpdate = FT<{ message: Message }>('events/messages:messageUpdate');
-export const MessageDelete = FT<{ channel: TextChannel }>('events/messages:messageDelete');
+export const MessageUpdate = FT<{ channel: GuildTextBasedChannel }>('events/messages:messageUpdate');
+export const MessageDelete = FT<{ channel: GuildTextBasedChannel }>('events/messages:messageDelete');

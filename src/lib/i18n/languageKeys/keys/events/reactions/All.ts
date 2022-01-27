c708f73b5b2fc14c474fb692c16b0b215ce16ac6
Nytelife26 <xtylerjrx@gmail.com>
2022-01-27 19:12:52 +0000

    fix: warn users of incorrect reaction role hierarchy

--- src/lib/i18n/languageKeys/keys/events/reactions/All.ts
+++ src/lib/i18n/languageKeys/keys/events/reactions/All.ts
@@ -3,3 +3,4 @@ import { FT, T } from '#lib/types';
 export const Reaction = T<string>('events/reactions:reaction');
 export const Filter = FT<{ user: string }, string>('events/reactions:filter');
 export const FilterFooter = T<string>('events/reactions:filterFooter');
+export const SelfRoleHierarchy = T<string>('events/reactions:selfRoleHierarchy');

0b9675ce252919283237dea148d53e4d73abe7d9
Nytelife26 <xtylerjrx@gmail.com>
2022-02-03 00:22:33 +0000

    fix(listeners): log manual bans

--- src/listeners/guilds/bans/guildBanRemove.ts
+++ src/listeners/guilds/bans/guildBanRemove.ts
@@ -5,7 +5,7 @@ import { Listener } from '@sapphire/framework';
 import type { Guild, User } from 'discord.js';
 
 export class UserListener extends Listener {
-	public async run(guild: Guild, user: User) {
+	public async run({ guild, user }: { guild: Guild; user: User }) {
 		if (!guild.available || !(await readSettings(guild, GuildSettings.Events.BanRemove))) return;
 
 		const moderation = getModeration(guild);

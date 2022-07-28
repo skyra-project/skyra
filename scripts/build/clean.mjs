fea8e01f4832bedb2c029bfcfaa4764dc28737e6
Jeroen Claassens <support@favware.tech>
2022-07-28 21:17:46 +0200

    chore: i guess

--- scripts/build/clean.mjs
+++
@@ -1,8 +0,0 @@
-import { rm } from 'node:fs/promises';
-
-const rootFolder = new URL('../../', import.meta.url);
-const distFolder = new URL('dist/', rootFolder);
-
-const options = { recursive: true, force: true };
-
-await rm(distFolder, options);

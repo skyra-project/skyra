2f7cb1c26c8c3f4fa7a393559743f1e6cdafaa49
Jeroen Claassens <support@favware.tech>
2022-07-31 01:36:25 +0200

    refactor: remove waifu

--- src/lib/i18n/languageKeys/keys/commands/Animation.ts
+++
@@ -1,6 +0,0 @@
-import type { LanguageHelpDisplayOptions } from '#lib/i18n/LanguageHelp';
-import { T } from '#lib/types';
-
-export const WaifuDescription = T('commands/animation:waifuDescription');
-export const WaifuExtended = T<LanguageHelpDisplayOptions>('commands/animation:waifuExtended');
-export const WaifuFooter = T('commands/animation:waifuFooter');

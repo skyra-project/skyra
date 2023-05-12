509df2555485b36119b64eca36b9a8f709e57a7b
Enes Genç <61084101+enxg@users.noreply.github.com>
2023-05-12 21:28:42 +0300

    refactor: change invite links to use Discord URLs instead (#2460)
    Co-authored-by: Aura Román <kyradiscord@gmail.com>
    Co-authored-by: Jeroen Claassens <jeroen.claassens@live.nl>

--- src/commands/General/invite.ts
+++ src/commands/General/invite.ts
@@ -1,9 +1,10 @@
 import { LanguageKeys } from '#lib/i18n/languageKeys';
 import { SkyraCommand } from '#lib/structures';
 import { BrandingColors } from '#utils/constants';
+import { hyperlink } from '@discordjs/builders';
 import { ApplyOptions } from '@sapphire/decorators';
 import { send } from '@sapphire/plugin-editable-commands';
-import { PermissionFlagsBits } from 'discord-api-types/v9';
+import { OAuth2Scopes, PermissionFlagsBits } from 'discord-api-types/v9';
 import { Message, MessageEmbed } from 'discord.js';
 import type { TFunction } from 'i18next';
 
@@ -26,20 +27,53 @@ export class UserCommand extends SkyraCommand {
 	}
 
 	private getEmbed(t: TFunction, shouldNotAddPermissions: boolean): MessageEmbed {
+		const embeddedInviteLink = hyperlink(
+			t(LanguageKeys.Commands.General.InvitePermissionInviteText),
+			this.generateInviteLink(shouldNotAddPermissions)
+		);
+		const embeddedJoinLink = hyperlink(t(LanguageKeys.Commands.General.InvitePermissionSupportServerText), 'https://discord.com/invite/6gakFR2');
+
 		return new MessageEmbed() //
 			.setColor(BrandingColors.Primary)
 			.setDescription(
 				[
-					[
-						`[${t(LanguageKeys.Commands.General.InvitePermissionInviteText)}](https://invite.skyra.pw${
-							shouldNotAddPermissions ? '/no-permissions' : ''
-						})`,
-						`[${t(LanguageKeys.Commands.General.InvitePermissionSupportServerText)}](https://join.skyra.pw)`
-					].join(' | '),
+					[embeddedInviteLink, embeddedJoinLink].join(' | '),
 					shouldNotAddPermissions ? undefined : t(LanguageKeys.Commands.General.InvitePermissionsDescription)
 				]
 					.filter(Boolean)
 					.join('\n')
 			);
+	}
+
+	private generateInviteLink(shouldNotAddPermissions: boolean) {
+		return this.container.client.generateInvite({
+			scopes: [OAuth2Scopes.ApplicationsCommands, OAuth2Scopes.Bot],
+			permissions: shouldNotAddPermissions
+				? 0n
+				: PermissionFlagsBits.AddReactions |
+				  PermissionFlagsBits.AttachFiles |
+				  PermissionFlagsBits.BanMembers |
+				  PermissionFlagsBits.ChangeNickname |
+				  PermissionFlagsBits.CreatePrivateThreads |
+				  PermissionFlagsBits.CreatePublicThreads |
+				  PermissionFlagsBits.DeafenMembers |
+				  PermissionFlagsBits.EmbedLinks |
+				  PermissionFlagsBits.KickMembers |
+				  PermissionFlagsBits.ManageChannels |
+				  PermissionFlagsBits.ManageEmojisAndStickers |
+				  PermissionFlagsBits.ManageGuild |
+				  PermissionFlagsBits.ManageMessages |
+				  PermissionFlagsBits.ManageNicknames |
+				  PermissionFlagsBits.ManageRoles |
+				  PermissionFlagsBits.ManageThreads |
+				  PermissionFlagsBits.MoveMembers |
+				  PermissionFlagsBits.MuteMembers |
+				  PermissionFlagsBits.ReadMessageHistory |
+				  PermissionFlagsBits.SendMessages |
+				  PermissionFlagsBits.SendMessagesInThreads |
+				  PermissionFlagsBits.UseExternalEmojis |
+				  PermissionFlagsBits.UseExternalStickers |
+				  PermissionFlagsBits.ViewChannel
+		});
 	}
 }

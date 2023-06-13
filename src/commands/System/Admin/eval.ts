6ba26444b40be916faa6b577a6d51dd5a5a5216b
Aura Román <kyradiscord@gmail.com>
2023-06-13 19:25:09 +0200

    refactor: remove `ExceededLength` handler and simplify eval
    Also made the character length count check count codepoints instead of characters for extra accuracy.

--- src/commands/System/Admin/eval.ts
+++ src/commands/System/Admin/eval.ts
@@ -2,12 +2,12 @@ import { LanguageKeys } from '#lib/i18n/languageKeys';
 import { SkyraCommand } from '#lib/structures';
 import { PermissionLevels } from '#lib/types/Enums';
 import { seconds } from '#utils/common';
-import { EvalExtraData, handleMessage } from '#utils/Parsers/ExceededLength';
 import { clean } from '#utils/Sanitizer/clean';
 import { cast } from '#utils/util';
+import { bold } from '@discordjs/builders';
 import { ApplyOptions } from '@sapphire/decorators';
+import { send } from '@sapphire/plugin-editable-commands';
 import { Stopwatch } from '@sapphire/stopwatch';
-import { Type } from '@sapphire/type';
 import { codeBlock, isThenable } from '@sapphire/utilities';
 import type { Message } from 'discord.js';
 import { setTimeout as sleep } from 'node:timers/promises';
@@ -17,8 +17,8 @@ import { inspect } from 'node:util';
 	aliases: ['ev'],
 	description: LanguageKeys.Commands.System.EvalDescription,
 	detailedDescription: LanguageKeys.Commands.System.EvalExtended,
-	flags: ['async', 'no-timeout', 'json', 'silent', 'log', 'showHidden', 'hidden', 'sql'],
-	options: ['wait', 'lang', 'language', 'output', 'output-to', 'depth'],
+	flags: ['async', 'no-timeout', 'json', 'silent', 'showHidden', 'hidden', 'sql'],
+	options: ['wait', 'lang', 'language', 'depth'],
 	permissionLevel: PermissionLevels.BotOwner,
 	quotes: []
 })
@@ -30,29 +30,24 @@ export class UserCommand extends SkyraCommand {
 
 		const wait = args.getOption('wait');
 		const flagTime = args.getFlags('no-timeout') ? (wait === null ? this.kTimeout : Number(wait)) : Infinity;
-		const language = args.getOption('lang', 'language') ?? (args.getFlags('json') ? 'json' : 'js');
 		const executeSql = args.getFlags('sql');
-		const { success, result, time, type } = executeSql ? await this.sql(code) : await this.timedEval(message, args, code, flagTime);
+		const language = args.getOption('lang', 'language') ?? (executeSql || args.getFlags('json') ? 'json' : 'js');
+		const { success, result, time } = executeSql ? await this.sql(code) : await this.timedEval(message, args, code, flagTime);
 
 		if (args.getFlags('silent')) {
 			if (!success && result && cast<Error>(result).stack) this.container.logger.fatal(cast<Error>(result).stack);
 			return null;
 		}
 
-		const footer = executeSql ? undefined : codeBlock('ts', type as string);
-		const sendAs = args.getOption('output', 'output-to') ?? (args.getFlags('log') ? 'log' : null);
+		const header = `${bold(success ? 'Output' : 'Error')}: ${time}`;
+		// If the sum of the length between the header, codeblock (```\n...\n```), and result exceed 2000 characters, send as file:
+		if ([...header, ...language, ...result].length + 8 > 2000) {
+			const file = { attachment: Buffer.from(result, 'utf8'), name: `output.${language}` } as const;
+			return send(message, { content: header, files: [file] });
+		}
 
-		return handleMessage<Partial<EvalExtraData>>(message, {
-			sendAs,
-			hastebinUnavailable: false,
-			url: null,
-			canLogToConsole: true,
-			success,
-			result,
-			time,
-			footer,
-			language
-		});
+		// Otherwise send as a single message:
+		return send(message, `${header}${codeBlock(language, result)}`);
 	}
 
 	private async timedEval(message: Message, args: SkyraCommand.Args, code: string, flagTime: number) {
@@ -76,7 +71,6 @@ export class UserCommand extends SkyraCommand {
 		let asyncTime = '';
 		let result: unknown;
 		let thenable = false;
-		let type: Type;
 
 		try {
 			if (args.getFlags('async')) code = `(async () => {\n${code}\n})();`;
@@ -87,7 +81,6 @@ export class UserCommand extends SkyraCommand {
 			// eslint-disable-next-line no-eval
 			result = eval(code);
 			syncTime = stopwatch.toString();
-			type = new Type(result);
 			if (isThenable(result)) {
 				thenable = true;
 				stopwatch.restart();
@@ -98,7 +91,6 @@ export class UserCommand extends SkyraCommand {
 		} catch (error) {
 			if (!syncTime.length) syncTime = stopwatch.toString();
 			if (thenable && !asyncTime.length) asyncTime = stopwatch.toString();
-			if (!type!) type = new Type(error);
 			result = error;
 			success = false;
 		}
@@ -117,7 +109,6 @@ export class UserCommand extends SkyraCommand {
 		}
 		return {
 			success,
-			type: type!,
 			time: this.formatTime(syncTime, asyncTime ?? ''),
 			result: clean(result as string)
 		};
@@ -128,16 +119,13 @@ export class UserCommand extends SkyraCommand {
 		let success: boolean;
 		let time: string;
 		let result: unknown;
-		let type: Type;
 
 		try {
 			result = await this.container.db.connection.query(sql);
 			time = stopwatch.toString();
-			type = new Type(result);
 			success = true;
 		} catch (error) {
 			time = stopwatch.toString();
-			type = new Type(error);
 			result = error;
 			success = false;
 		}
@@ -146,7 +134,6 @@ export class UserCommand extends SkyraCommand {
 
 		return {
 			success,
-			type: type!,
 			time: this.formatTime(time),
 			result: JSON.stringify(result, null, 2)
 		};
--- src/lib/i18n/languageKeys/keys/System.ts
+++ src/lib/i18n/languageKeys/keys/System.ts
@@ -3,13 +3,6 @@ import { FT, T } from '#lib/types';
 export const FetchBansFail = T('system:fetchBansFail');
 export const Loading = T<readonly string[]>('system:loading');
 export const DiscordAbortError = T('system:discordAbortError');
-export const ExceededLengthOutput = FT<{ output: string }>('system:exceededLengthOutput');
-export const ExceededLengthOutputConsole = T('system:exceededLengthOutputConsole');
-export const ExceededLengthOutputFile = T('system:exceededLengthOutputFile');
-export const ExceededLengthOutputHastebin = FT<{ url: string }>('system:exceededLengthOutputHastebin');
-export const ExceededLengthChooseOutput = FT<{ output: string[] }>('system:exceededLengthChooseOutput');
-export const ExceededLengthOutputType = FT<{ type: string }>('system:exceededLengthOutputType');
-export const ExceededLengthOutputTime = FT<{ time: string }>('system:exceededLengthOutputTime');
 export const HelpTitles = T<{
 	aliases: string;
 	usages: string;

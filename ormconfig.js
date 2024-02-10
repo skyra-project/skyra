2e9b9bc40d5d44563399157d117c0838006347c8
Jeroen Claassens <support@favware.tech>
2024-02-10 18:51:41 +0100

    build: better docker and typeorm config (#2577)

--- ormconfig.js
+++ ormconfig.js
@@ -1,1 +1,8 @@
-export default (await import('./dist/lib/database/database.config.js')).AppDataConfig;
+import { AppDataConfig } from '#lib/database/database.config';
+import { rootFolder } from '#utils/constants';
+import { setup } from '@skyra/env-utilities';
+import { join } from 'node:path';
+
+setup(join(rootFolder, 'src', '.env'));
+
+export default AppDataConfig;

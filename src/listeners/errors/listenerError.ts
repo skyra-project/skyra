473ccf5c80328efb7dd84702757a1804961b2513
Aura <kyradiscord@gmail.com>
2024-11-02 22:33:22 +0100

    fix(listeners): ignore `UserError` (#2691)

--- src/listeners/errors/listenerError.ts
+++ src/listeners/errors/listenerError.ts
@@ -1,7 +1,10 @@
-import { Events, Listener, type ListenerErrorPayload } from '@sapphire/framework';
+import { Events, Listener, UserError, type ListenerErrorPayload } from '@sapphire/framework';
 
 export class UserListener extends Listener<typeof Events.ListenerError> {
 	public run(error: Error, context: ListenerErrorPayload) {
+		// We generally do not care about `UserError`s in listeners since they're not bugs.
+		if (error instanceof UserError) return;
+
 		this.container.logger.fatal(`[EVENT] ${context.piece.name}\n${error.stack || error.message}`);
 	}
 }

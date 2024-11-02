3f6bfb9bf76bb9fa9a494b3b82871c2d5823ab09
Aura Román <kyradiscord@gmail.com>
2024-11-02 18:06:40 +0100

    fix(listeners): ignore `UserError`

--- src/listeners/errors/listenerErrorSentry.ts
+++ src/listeners/errors/listenerErrorSentry.ts
@@ -1,11 +1,14 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Events, Listener, type ListenerErrorPayload } from '@sapphire/framework';
+import { Events, Listener, UserError, type ListenerErrorPayload } from '@sapphire/framework';
 import { captureException } from '@sentry/node';
 import { envIsDefined } from '@skyra/env-utilities';
 
 @ApplyOptions({ enabled: envIsDefined('SENTRY_URL') })
 export class UserListener extends Listener<typeof Events.ListenerError> {
 	public run(error: Error, context: ListenerErrorPayload) {
+		// We generally do not care about `UserError`s in listeners since they're not bugs.
+		if (error instanceof UserError) return;
+
 		captureException(error, { tags: { name: context.piece.name } });
 	}
 }

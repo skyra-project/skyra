d76c54d9a263ad84b7d20aa4017f58259fdaa913
Aura <kyradiscord@gmail.com>
2025-02-26 22:15:39 +0100

    feat: add `enforceNonce: true` (#2733)

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -4,16 +4,16 @@ import { ExtendedHandler as EnGbHandler } from '#root/languages/en-GB/constants'
 import { ExtendedHandler as EnUsHandler } from '#root/languages/en-US/constants';
 import { ExtendedHandler as EsEsHandler } from '#root/languages/es-ES/constants';
 import { ExtendedHandler as NlHandler } from '#root/languages/nl/constants';
-import type { LocaleString } from 'discord.js';
+import { Locale } from 'discord.js';
 
-export const handlers = new Map<LocaleString, Handler>([
-	['de', new DeHandler()],
-	['en-US', new EnUsHandler()],
-	['en-GB', new EnGbHandler()],
-	['es-ES', new EsEsHandler()],
-	['nl', new NlHandler()]
+export const handlers = new Map<Locale, Handler>([
+	[Locale.German, new DeHandler()],
+	[Locale.EnglishUS, new EnUsHandler()],
+	[Locale.EnglishGB, new EnGbHandler()],
+	[Locale.SpanishES, new EsEsHandler()],
+	[Locale.Dutch, new NlHandler()]
 ]);
 
-export function getHandler(name: LocaleString): Handler {
-	return handlers.get(name) ?? handlers.get('en-US')!;
+export function getHandler(name: Locale): Handler {
+	return handlers.get(name) ?? handlers.get(Locale.EnglishUS)!;
 }
